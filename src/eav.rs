c145622a259ab003b16c74e7d50289cac6f032a9
Corey Montella <cmontella@live.com>
2018-02-09 13:46:49 -0500

    Implement Entity and Attribute

--- src/eav.rs
+++ src/eav.rs
@@ -3,14 +3,86 @@
 // ## Prelude
 
 use core::fmt;
+use indexes::Hasher;
 
 // ## Entity
 
-pub struct Entity(u64);
+pub struct Entity {
+  pub id: u64,
+  pub pairs: Vec<(Attribute, Value)>,
+}
+
+impl Entity {
+
+  pub fn new() -> Entity {
+    Entity {
+      id: 0,
+      pairs: Vec::new(),
+    }
+  }
+
+  pub fn from_raw(pairs: Vec<(&str, Value)>) -> Entity {
+    let mut entity = Entity::new();
+    let mut entity_id = Hasher::new();
+    let mut attribute_id = Hasher::new();
+    for (attribute, value) in pairs {
+      entity_id.write(attribute);
+      entity_id.write_value(&value);
+      let attribute = Attribute::from_str(attribute);
+      entity.pairs.push((attribute, value));
+    } 
+    entity.id = entity_id.finish();
+    entity
+  }
+}
+
+impl fmt::Debug for Entity {
+    #[inline]
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+      write!(f,"{} -> [ ", self.id);
+      for &(ref attribute, ref value) in &self.pairs {
+        write!(f,"{:?}: {:?} ", attribute, value);
+      }
+      write!(f,"]")
+    }
+}
 
 // ## Attribute
 
-pub struct Attribute(u64);
+pub struct Attribute {
+  pub id: u64,
+  pub display: String,
+}
+
+impl Attribute {
+
+  pub fn new() -> Attribute {
+    Attribute {
+      id: 0,
+      display: String::from(""),
+    }
+  }
+
+  pub fn from_str(string: &str) -> Attribute {
+    let mut attribute_id = Hasher::new();
+    attribute_id.write(string);
+    let mut attribute = Attribute::new();
+    attribute.id = attribute_id.finish();
+    attribute.display = String::from(string);
+    attribute
+  }
+
+
+}
+
+impl fmt::Debug for Attribute {
+    #[inline]
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+      write!(f,"{}", self.display)
+    }
+}
+
+
 
 // ## Value
 
@@ -42,7 +114,7 @@ impl fmt::Debug for Value {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
       match self {
         &Value::Number(ref x) => write!(f, "{}", x),
-        &Value::String(ref x) => write!(f, "{}", x),
+        &Value::String(ref x) => write!(f, "\"{}\"", x),
         &Value::Any => write!(f, "Any"),
       }
     }
