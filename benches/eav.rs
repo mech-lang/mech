46f1165e460be1bd76706d0879571c736ba01788
Corey Montella <cmontella@live.com>
2018-02-09 13:54:37 -0500

    Add eav bench file, with entity bench

---
+++ benches/eav.rs
@@ -0,0 +1,17 @@
+#![feature(test)]
+
+extern crate test;
+extern crate mech;
+
+use test::Bencher;
+use mech::eav::{Entity, Value};
+
+#[bench]
+fn make_entity(b: &mut Bencher) {
+    b.iter(|| {
+         let raw = vec![("tag", Value::from_str("#keyboard/event/keydown")),
+                   ("key", Value::from_str("A")),
+                   ("code", Value::from_int(42))];
+        Entity::from_raw(raw)
+    });
+}
