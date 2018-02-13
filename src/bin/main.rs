50c27e1a7c9300543c220bc9ec5b15e7ead90ed4
cmontella <cmontella@live.com>
2018-02-12 22:22:41 -0800

    test database

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -1,13 +1,38 @@
-#![feature(sip_hash_13)]
-
 extern crate mech;
 extern crate core;
 
+
 use std::collections::{BTreeSet, BTreeMap};
 use mech::runtime::{Database, Transaction, Change, ChangeType};
-use mech::eav::Value;
+use mech::eav::{Entity, Attribute, Value};
 use mech::indexes::Hasher;
 
 fn main() {
+  let mut db = Database::new(1000,1000);
+
+  let mut raw = vec![("tag", Value::from_str("keyboard/event/keydown")),
+                 ("key", Value::from_str("A")),
+                 ("code", Value::from_u64(42))];
+  let mut key = Entity::from_raw(raw);
+  let mut changes = key.make_changeset(ChangeType::Add);
+  let mut txn = Transaction::from_changeset(changes);
+
+  db.register_transaction(txn);
+
+println!("{:?}", db.entity_index);
+
+  //raw = vec![("tag", Value::from_str("keyboard/event/keydown")),
+  //               ("key", Value::from_str("A")),
+  //               ("code", Value::from_u64(42))];
+  //key = Entity::from_raw(raw);
+  changes = key.make_changeset(ChangeType::Remove);
+  txn = Transaction::from_changeset(changes);
+
+
+
+  db.register_transaction(txn);
+
+
+  println!("{:?}", db.entity_index);
 
 }
