4b0bdf7d84bb9ed9dcfc55e860de6861e92a15a5
cmontella <cmontella@live.com>
2018-02-22 15:06:17 -0800

    test more in main

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -4,10 +4,29 @@ extern crate core;
 
 use std::collections::{BTreeSet, BTreeMap};
 use mech::database::{Database, Transaction, Change, ChangeType};
-use mech::eav::{Entity, Attribute, Value};
+use mech::eav::{Entity, Attribute, Value, Table};
 use mech::indexes::Hasher;
 
 fn main() {
+
+  let mut vec: Vec<Vec<Value>> = Vec::new();
+  vec.push(vec![Value::Any, Value::from_str("Hello")]);
+  println!("{:?}", vec[0][0]);
+  vec[0][0] = Value::from_str("Set");
+  println!("{:?}", vec[0][0]);
+
+  let mut table = Table::new("keyboard/event/keydown", 16, 16);
+  
+  table.add_value(&0, &0, Value::from_u64(10));
+  table.add_value(&0, &1, Value::from_u64(11));
+  table.add_value(&1, &0, Value::from_u64(31));
+  table.add_value(&1, &2, Value::from_u64(99));
+  table.add_value(&2, &1, Value::from_u64(75));
+  table.add_value(&1, &1, Value::from_u64(83));
+  table.add_value(&1, &1, Value::from_u64(29));
+
+  println!("{:?}", table);
+
   let mut db = Database::new(1000, 1000);
 
   let mut raw = vec![("tag", Value::from_str("keyboard/event/keydown")),
@@ -19,7 +38,7 @@ fn main() {
 
   db.register_transaction(txn);
 
-println!("{:?}", db.entity_index);
+  println!("{:?}", db.entity_index);
 
   //raw = vec![("tag", Value::from_str("keyboard/event/keydown")),
   //               ("key", Value::from_str("A")),
