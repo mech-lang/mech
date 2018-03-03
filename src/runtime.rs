05722236e7a5027ae4ef2933418a190c655aface
cmontella <cmontella@live.com>
2018-03-03 15:51:51 -0800

    Some notes

--- src/runtime.rs
+++ src/runtime.rs
@@ -1,9 +1,11 @@
 // # Mech Runtime
 
-// The Mech Runtime is the engine that drives computations in Mech. The runtime
-// is comprised of "Blocks", interconnected by "Pipes" of data they query and 
-// publish. Blocks can interact with the database, by Scanning for records that 
-// match a pattern, or by Projecting compted records into the database.
+/* 
+ The Mech Runtime is the engine that drives computations in Mech. The runtime
+ is comprised of "Blocks", interconnected by "Pipes" of records.
+ Blocks can interact with the database, by Scanning for records that 
+ match a pattern, or by Projecting computed records into the database.
+*/
 
 // ## Prelude
 
@@ -19,7 +21,6 @@ pub struct Address {
 }
 
 pub struct Register {
-
   data: Vec<(Entity, Attribute, Value)>,
 }
 
@@ -30,7 +31,6 @@ pub struct Block {
   pub output_registers: Vec<Register>,
   pub constraints: Vec<Constraint>,
 }
-
 
 impl Block {
   
@@ -48,6 +48,8 @@ impl Block {
 
 // ## Pipe
 
+// Pipes are conduits of records between blocks.
+
 pub struct Pipe {
   input: Address,
   output: Address,
@@ -61,5 +63,6 @@ pub struct Pipe {
 
 
 pub enum Constraint {
-  Scan { entity: u64, Attribute: u64, value: Value, register: Register },
+  // A Scan searches for a given 
+  Scan { entity: u64, attribute: u64, value: Value, register: Register },
 }
