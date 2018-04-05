996e7690eebc471b60fac9fc8a8741dbfd05a93a
cmontella <cmontella@live.com>
2018-04-04 18:42:05 -0700

    Clear out main binary for now

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -7,28 +7,6 @@ use mech::indexes::Hasher;
 
 fn main() {
 
-  let mut table = Table::new("keyboard/event/keydown", 16, 16);
-  
-  let me: u64 = Hasher::hash_str("This is me");
-  let wife: u64 = Hasher::hash_str("This is my wife");
-  let age: u64 = Hasher::hash_str("age");
-  let first: u64 = Hasher::hash_str("first name");
-  let last: u64 = Hasher::hash_str("last name");
-  let gender: u64 = Hasher::hash_str("gender");
 
-  table.add_value(me, first, Value::from_str("Corey"));
-  table.add_value(wife, first, Value::from_str("Rachel"));
-  table.add_value(me, last, Value::from_str("Montella"));
-  table.add_value(me, age, Value::from_u64(31));
-  table.add_value(wife, age, Value::from_u64(29));
-  table.add_value(wife, last, Value::from_str("Montella"));
-  table.add_value(wife, gender, Value::from_str("female"));
-  table.add_value(me, gender, Value::from_str("male"));
-
-  println!("{:?}", table);
-
-  println!("{:?}", table.get_rows(wife));
-
-  let mut db = Database::new(1000, 1000);
 
 }
