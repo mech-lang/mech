31de7ac480e35a65b9946e8a6edfc8a1d5e6e397
cmontella <cmontella@live.com>
2018-04-06 23:19:21 -0700

    Fix main

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -16,27 +16,34 @@ fn main() {
   let test1: u64 = Hasher::hash_str("test1");
   let test2: u64 = Hasher::hash_str("test2");
 
-  table.add_value(student1, first, Value::from_str("Mark"));
-  table.add_value(student1, last, Value::from_str("Laughlin"));
-  table.add_value(student1, test1, Value::from_u64(83));
-  table.add_value(student1, test2, Value::from_u64(76));
+  table.set(student1, first, Value::from_str("Mark"));
+  table.set(student1, last, Value::from_str("Laughlin"));
+  table.set(student1, test1, Value::from_u64(83));
+  table.set(student1, test2, Value::from_u64(76));
 
-  table.add_value(student2, first, Value::from_str("Sabra"));
-  table.add_value(student2, last, Value::from_str("Kindar"));
-  table.add_value(student2, test1, Value::from_u64(99));
-  table.add_value(student2, test2, Value::from_u64(95));
+  table.set(student2, first, Value::from_str("Sabra"));
+  table.set(student2, last, Value::from_str("Kindar"));
+  table.set(student2, test1, Value::from_u64(99));
+  table.set(student2, test2, Value::from_u64(95));
+  table.set(student2, first, Value::from_u64(100));
 
   println!("{:?}", table);
 
-  println!("{:?}", table.get_rows(vec![student1]));
+    println!("{:?}", table.get_rows(vec![student1]));
   println!("{:?}", table.get_cols(vec![first, test1, last, 3]));
 
-  let mut foo = table.index(student1, test1).unwrap();
+  table.index(student1, test1);
+
+  table.clear(student1, last);
+  table.clear(student2, test2);
+
+  println!("{:?}", table);
+
+
   //println!("{:?}", foo);
   //let mut my_value = Value::from_u64(100);
   //foo = &mut my_value;
   
-  println!("{:?}", Value::from_vec(vec![Value::from_u64(100), Value::from_u64(200)]));
 
   let mut db = Database::new(1000, 1000);
 
