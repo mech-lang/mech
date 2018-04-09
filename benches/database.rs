07f832919b8afeace2b5576a4d1c0ece41b04db2
cmontella <cmontella@live.com>
2018-04-08 23:33:52 -0700

    fix benches

--- benches/database.rs
+++ benches/database.rs
@@ -30,43 +30,23 @@ fn db_register_table(b: &mut Bencher) {
     let test1: u64 = Hasher::hash_str("test1");
     b.iter(|| {
         let t1 = NewTableChange::new(String::from("students"), vec![], vec![], 10, 10);
-        let c1 = AddChange::new(students, student1, test1, Value::from_u64(100));
         let txn = Transaction::from_changeset(vec![
-            Change::Add(c1), 
             Change::NewTable(t1)]);
         db.register_transaction(txn);
     });
 }
 
 #[bench]
-fn db_register_transaction(b: &mut Bencher) {
+fn db_register_add(b: &mut Bencher) {
     let mut db = Database::new(1, 1, 1);
     let students: u64 = Hasher::hash_str("students");  
     let student1: u64 = Hasher::hash_str("Mark");
     let test1: u64 = Hasher::hash_str("test1");
+    let t1 = NewTableChange::new(String::from("students"), vec![], vec![], 10, 10);
+    let txn = Transaction::from_change(Change::NewTable(t1));
     b.iter(|| {
-        let t1 = NewTableChange::new(String::from("students"), vec![], vec![], 10, 10);
         let c1 = AddChange::new(students, student1, test1, Value::from_u64(100));
-        let txn = Transaction::from_changeset(vec![
-            Change::Add(c1), 
-            Change::NewTable(t1)]);
+        let txn = Transaction::from_change(Change::Add(c1));
         db.register_transaction(txn);
-    });
-}
-
-
-#[bench]
-// Register 1000 txns
-fn db_register_txn_1000(b: &mut Bencher) {
-    let mut db = Database::new(1, 1, 1);
-    b.iter(|| {
-        /*for _ in 1..1000 {
-            let raw = vec![("tag", Value::from_str("keyboard/event/keydown")),
-                            ("key", Value::from_str("A")),
-                            ("code", Value::from_u64(14))];
-            let key = Entity::from_raw(raw);
-            let changes = key.make_changeset(ChangeType::Add);
-            let txn = Transaction::from_changeset(changes);    
-        }*/
     });
 }
