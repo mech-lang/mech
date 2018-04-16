e9dcbe4458fc91caa5a15f90d80ad86a77527217
cmontella <cmontella@live.com>
2018-04-15 23:38:11 -0700

    update main with runtime testing

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -4,9 +4,40 @@ extern crate core;
 use mech::database::{Database, Transaction, Change, AddChange, NewTableChange};
 use mech::table::{Value, Table};
 use mech::indexes::Hasher;
-use std::thread::{self};
 use mech::runtime::{Runtime, Block, Constraint, Register};
 
 fn main() {
+
+  let mut db = Database::new(1000, 1000, 1000);
+  let students: u64 = Hasher::hash_str("students");  
+  let student1: u64 = Hasher::hash_str("Mark");
+  let student2: u64 = Hasher::hash_str("Sabra");
+  let test1: u64 = Hasher::hash_str("test1");
+  let test2: u64 = Hasher::hash_str("test2");
+  let result: u64 = Hasher::hash_str("result");
+
+  let c1 = AddChange::new(students, student1, test1, Value::from_u64(83));
+  let c2 = AddChange::new(students, student1, test2, Value::from_u64(76));
+  let c3 = AddChange::new(students, student2, test1, Value::from_u64(100));
+  let c4 = AddChange::new(students, student2, test2, Value::from_u64(88));
+  let t1= NewTableChange::new(String::from("students"), vec![], vec![], 10, 10);
+  let txn = Transaction::from_changeset(vec![
+    Change::Add(c1), 
+    Change::NewTable(t1), 
+    Change::Add(c2),
+    Change::Add(c3),
+    Change::Add(c4)]);
+  
+  
+  let mut block = Block::new();
+  block.constraints.push(Constraint::Scan {table: students, attribute: test1});
+  block.constraints.push(Constraint::Scan {table: students, attribute: test2});
+  block.constraints.push(Constraint::Insert {table: students, attribute: result});
+  db.runtime.register_block(block.clone(), &db.store);
+
+  println!("{:?}", db);
+  println!("{:?}", db.runtime);
+
+  db.register_transaction(txn);
 
 }
