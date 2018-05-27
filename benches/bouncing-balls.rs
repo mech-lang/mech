0d9025495a6edf0e5ff2c5b3b92ab85dfe92366d
cmontella <cmontella@live.com>
2018-05-26 18:14:04 -0700

    update bouncing balls bench

--- benches/bouncing-balls.rs
+++ benches/bouncing-balls.rs
@@ -40,24 +40,29 @@ fn position_update() -> Block {
   block.add_constraint(Constraint::Scan {table: ball, column: 2, input: 3});
   block.add_constraint(Constraint::Scan {table: ball, column: 3, input: 4});
   block.add_constraint(Constraint::Scan {table: ball, column: 4, input: 5});  
-  block.add_constraint(Constraint::Function {operation: Function::Add, parameters: vec![5, 6], output: 1}); 
-  block.add_constraint(Constraint::Function {operation: Function::Add, parameters: vec![7, 8], output: 2});
-  block.add_constraint(Constraint::Function {operation: Function::Add, parameters: vec![8, 4], output: 3});
-  block.add_constraint(Constraint::Constant {value: 16, input: 4});
-  block.add_constraint(Constraint::Identity {source: 2, sink: 5});
-  block.add_constraint(Constraint::Identity {source: 4, sink: 6});
-  block.add_constraint(Constraint::Identity {source: 3, sink: 7});
-  block.add_constraint(Constraint::Identity {source: 5, sink: 8});
-  block.add_constraint(Constraint::Insert {output: 1, table: ball, column: 1});
-  block.add_constraint(Constraint::Insert {output: 2, table: ball, column: 2});
-  block.add_constraint(Constraint::Insert {output: 3, table: ball, column: 4});
+  block.add_constraint(Constraint::Identity {source: 2, sink: 1});
+  block.add_constraint(Constraint::Identity {source: 4, sink: 2});
+  block.add_constraint(Constraint::Identity {source: 3, sink: 3});
+  block.add_constraint(Constraint::Identity {source: 5, sink: 4});
+  block.add_constraint(Constraint::Function {operation: Function::Add, parameters: vec![1, 2], output: 5}); 
+  block.add_constraint(Constraint::Function {operation: Function::Add, parameters: vec![3, 4], output: 6});
+  block.add_constraint(Constraint::Constant {value: 10, input: 7});
+  block.add_constraint(Constraint::Function {operation: Function::Add, parameters: vec![4, 7], output: 8});
+  block.add_constraint(Constraint::Insert {output: 5, table: ball, column: 1});
+  block.add_constraint(Constraint::Insert {output: 6, table: ball, column: 2});
+  block.add_constraint(Constraint::Insert {output: 7, table: ball, column: 4});
   let plan = vec![
-    Constraint::Function {operation: Function::Add, parameters: vec![5, 6], output: 1},
-    Constraint::Function {operation: Function::Add, parameters: vec![7, 8], output: 2},
-    Constraint::Function {operation: Function::Add, parameters: vec![8, 4], output: 3},
-    Constraint::Insert {output: 1, table: ball, column: 1},
-    Constraint::Insert {output: 2, table: ball, column: 2},
-    Constraint::Insert {output: 3, table: ball, column: 4},
+    Constraint::Identity {source: 2, sink: 1},
+    Constraint::Identity {source: 4, sink: 2},
+    Constraint::Identity {source: 3, sink: 3},
+    Constraint::Identity {source: 5, sink: 4},
+    Constraint::Constant {value: 10, input: 7},
+    Constraint::Function {operation: Function::Add, parameters: vec![1, 2], output: 5},
+    Constraint::Function {operation: Function::Add, parameters: vec![3, 4], output: 6},
+    Constraint::Function {operation: Function::Add, parameters: vec![4, 7], output: 8},
+    Constraint::Insert {output: 5, table: ball, column: 1},
+    Constraint::Insert {output: 6, table: ball, column: 2},
+    Constraint::Insert {output: 8, table: ball, column: 4},
   ];
   block.plan = plan;
   block
