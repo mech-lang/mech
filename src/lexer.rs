359818a4f1d4358c2d6330030a53280828e994b9
cmontella <cmontella@live.com>
2018-06-12 16:30:44 -0700

    Move table parsing to parser stage, recognize characters instead here

--- src/lexer.rs
+++ src/lexer.rs
@@ -8,14 +8,16 @@
 
 #[derive(Clone, Debug, PartialEq)]
 pub enum Token {
-  Table { name: Vec<u8> },
   Digit { value: u8 },
+  Identifier{ name: Vec<u8> },
+  HashTag,
   LeftBracket,
   RightBracket,
   Comma,
   Space,
   Plus,
   Equal,
+  Period,
 }
 
 
@@ -56,19 +58,20 @@ impl Lexer {
   pub fn get_tokens(&mut self) -> &Vec<Token> {
     let bytes = self.string.clone().into_bytes();
     while self.position < self.string.len() {
-      if match_table(&bytes, self) {
-        self.last_token += 1;
-        let extracted = extract_bytes(&bytes, self);
-        self.push_token(Token::Table{name: extracted});
-      } else if match_digit(&bytes, self) {
+      if match_digit(&bytes, self) {
         let extracted = extract_bytes(&bytes, self)[0];
         self.push_token(Token::Digit{value: extracted})
+      } else if match_identifier(&bytes, self) { 
+        let extracted = extract_bytes(&bytes, self);
+        self.push_token(Token::Identifier{name: extracted});
+      } else if match_char(&bytes, '#', self) { self.push_token(Token::HashTag);
       } else if match_char(&bytes, '[', self) { self.push_token(Token::LeftBracket);
       } else if match_char(&bytes, ']', self) { self.push_token(Token::RightBracket);
       } else if match_char(&bytes, ',', self) { self.push_token(Token::Comma); 
       } else if match_char(&bytes, ' ', self) { self.push_token(Token::Space); 
       } else if match_char(&bytes, '+', self) { self.push_token(Token::Plus); 
       } else if match_char(&bytes, '=', self) { self.push_token(Token::Equal); 
+      } else if match_char(&bytes, '.', self) { self.push_token(Token::Period);      
       } else {
         println!("Unknown Byte {:?} {:?}", bytes[self.position] as char, bytes[self.position]);
         break;
@@ -81,16 +84,22 @@ impl Lexer {
 
 pub fn match_table(bytes: &Vec<u8>, lexer: &mut Lexer) -> bool {
   let byte = bytes[lexer.position];
-  if test_match(bytes[lexer.position] == '#' as u8, lexer) {
-    while match_alpha(&bytes, lexer){
-      if lexer.position >= bytes.len() {
-        break;
-      }
-    }
-    true
+  if test_match(byte == '#' as u8, lexer) {
+    match_identifier(&bytes, lexer)
   } else {
     false
   }
+}
+
+pub fn match_identifier(bytes: &Vec<u8>, lexer: &mut Lexer) -> bool {
+  let mut matched = false;
+  while match_alpha(&bytes, lexer) {
+    matched = true;
+    if lexer.position >= bytes.len() {
+      break;
+    }
+  }
+  matched
 }
 
 pub fn match_alpha(bytes: &Vec<u8>, lexer: &mut Lexer) -> bool {
--- tests/lexer.rs
+++ tests/lexer.rs
@@ -1,14 +1,14 @@
 extern crate mech_syntax;
 
 use mech_syntax::lexer::{Lexer, Token};
-use mech_syntax::lexer::Token::{Table, LeftBracket, RightBracket, Digit, Space, Comma, Plus, Equal};
+use mech_syntax::lexer::Token::{HashTag, Identifier, LeftBracket, RightBracket, Digit, Space, Comma, Plus, Equal};
 
 #[test]
 fn lex_table() {
     let mut lexer = Lexer::new();
     lexer.add_string(String::from("#abc"));
     let tokens = lexer.get_tokens();
-    assert_eq!(tokens, &vec![Table{name: vec![97, 98, 99]}]);
+    assert_eq!(tokens, &vec![HashTag, Identifier{name: vec![97, 98, 99]}]);
 }
 
 #[test]
@@ -32,7 +32,7 @@ fn lex_table_with_brackets() {
     let mut lexer = Lexer::new();
     lexer.add_string(String::from("#abc[]"));
     let tokens = lexer.get_tokens();
-    assert_eq!(tokens, &vec![Table{name: vec![97, 98, 99]}, LeftBracket, RightBracket]);
+    assert_eq!(tokens, &vec![HashTag, Identifier{name: vec![97, 98, 99]}, LeftBracket, RightBracket]);
 }
 
 #[test]
@@ -40,7 +40,7 @@ fn lex_table_full() {
     let mut lexer = Lexer::new();
     lexer.add_string(String::from("#abc[1, 2, 3]"));
     let tokens = lexer.get_tokens();
-    assert_eq!(tokens, &vec![Table { name: vec![97, 98, 99] }, LeftBracket, Digit { value: 49 }, Comma, Space, Digit { value: 50 }, Comma, Space, Digit { value: 51 }, RightBracket]);
+    assert_eq!(tokens, &vec![HashTag, Identifier { name: vec![97, 98, 99] }, LeftBracket, Digit { value: 49 }, Comma, Space, Digit { value: 50 }, Comma, Space, Digit { value: 51 }, RightBracket]);
 }
 
 #[test]
@@ -48,7 +48,7 @@ fn lex_table_add() {
     let mut lexer = Lexer::new();
     lexer.add_string(String::from("#abc += [1 2]"));
     let tokens = lexer.get_tokens();
-    assert_eq!(tokens, &vec![Table { name: vec![97, 98, 99] }, Space, Plus, Equal, Space, LeftBracket, Digit { value: 49 }, Space, Digit { value: 50 }, RightBracket]);
+    assert_eq!(tokens, &vec![HashTag, Identifier { name: vec![97, 98, 99] }, Space, Plus, Equal, Space, LeftBracket, Digit { value: 49 }, Space, Digit { value: 50 }, RightBracket]);
 }
 
 #[test]
@@ -56,5 +56,5 @@ fn lex_table_relation() {
     let mut lexer = Lexer::new();
     lexer.add_string(String::from("#abc[3] = #abc[1] + #abc[2]"));
     let tokens = lexer.get_tokens();
-    assert_eq!(tokens, &vec![Table { name: vec![97, 98, 99] }, LeftBracket, Digit { value: 51 }, RightBracket, Space, Equal, Space, Table { name: vec![97, 98, 99] }, LeftBracket, Digit { value: 49 }, RightBracket, Space, Plus, Space, Table { name: vec![97, 98, 99] }, LeftBracket, Digit { value: 50 }, RightBracket]);
+    assert_eq!(tokens, &vec![HashTag, Identifier { name: vec![97, 98, 99] }, LeftBracket, Digit { value: 51 }, RightBracket, Space, Equal, Space, HashTag, Identifier { name: vec![97, 98, 99] }, LeftBracket, Digit { value: 49 }, RightBracket, Space, Plus, Space, HashTag, Identifier { name: vec![97, 98, 99] }, LeftBracket, Digit { value: 50 }, RightBracket]);
 }
