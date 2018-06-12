c8cbcd1219d20590f696d52c34e028c901a032c0
cmontella <cmontella@live.com>
2018-06-12 03:10:38 -0700

    Add some more tests to lexer

--- tests/lexer.rs
+++ tests/lexer.rs
@@ -1,13 +1,14 @@
 extern crate mech_syntax;
 
 use mech_syntax::lexer::{Lexer, Token};
+use mech_syntax::lexer::Token::{Table, LeftBracket, RightBracket, Digit, Space, Comma};
 
 #[test]
 fn lex_table() {
     let mut lexer = Lexer::new();
     lexer.add_string(String::from("#abc"));
     let tokens = lexer.get_tokens();
-    assert_eq!(tokens, vec![Token::Table{name: vec![97, 98, 99]}]);
+    assert_eq!(tokens, &vec![Table{name: vec![97, 98, 99]}]);
 }
 
 #[test]
@@ -15,7 +16,7 @@ fn lex_left_bracket() {
     let mut lexer = Lexer::new();
     lexer.add_string(String::from("["));
     let tokens = lexer.get_tokens();
-    assert_eq!(tokens, vec![Token::LeftBracket]);
+    assert_eq!(tokens, &vec![LeftBracket]);
 }
 
 #[test]
@@ -23,7 +24,7 @@ fn lex_right_bracket() {
     let mut lexer = Lexer::new();
     lexer.add_string(String::from("]"));
     let tokens = lexer.get_tokens();
-    assert_eq!(tokens, vec![Token::RightBracket]);
+    assert_eq!(tokens, &vec![RightBracket]);
 }
 
 #[test]
@@ -31,5 +32,13 @@ fn lex_table_with_brackets() {
     let mut lexer = Lexer::new();
     lexer.add_string(String::from("#abc[]"));
     let tokens = lexer.get_tokens();
-    assert_eq!(tokens, vec![Token::Table{name: vec![97, 98, 99]}, Token::LeftBracket, Token::RightBracket]);
+    assert_eq!(tokens, &vec![Table{name: vec![97, 98, 99]}, LeftBracket, RightBracket]);
+}
+
+#[test]
+fn lex_table_full() {
+    let mut lexer = Lexer::new();
+    lexer.add_string(String::from("#abc[1, 2, 3]"));
+    let tokens = lexer.get_tokens();
+    assert_eq!(tokens, &vec![Table { name: vec![97, 98, 99] }, LeftBracket, Digit { value: 49 }, Comma, Space, Digit { value: 50 }, Comma, Space, Digit { value: 51 }, RightBracket]);
 }
