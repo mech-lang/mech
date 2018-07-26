fcf9f1d11b0e463b79e53e33acb94148c29e2a88
cmontella <cmontella@live.com>
2018-07-26 17:43:15 -0400

    test table define and some math

--- tests/compiler.rs
+++ tests/compiler.rs
@@ -1,9 +1,11 @@
 #[macro_use]
 extern crate mech_syntax;
+extern crate mech;
 
 use mech_syntax::lexer::Lexer;
 use mech_syntax::parser::{Parser, ParseStatus, Node};
 use mech_syntax::compiler::Compiler;
+use mech::{Hasher, Core};
 
 macro_rules! compile_string {
   ($func:ident, $test:tt) => (
@@ -26,6 +28,24 @@ macro_rules! compile_string {
   )
 }
 
+macro_rules! test_math {
+  ($func:ident, $input:tt, $test:tt) => (
+    #[test]
+    fn $func() {
+      let mut compiler = Compiler::new();
+      let mut core = Core::new(10, 10);
+      let input = String::from($input);
+      compiler.compile_string(input);
+      core.register_blocks(compiler.blocks);
+      core.step();
+      let table = Hasher::hash_str("test");
+      let row = 1;
+      let column = 1;
+      let test = $test;
+      assert_eq!(core.index(table,row,column).unwrap().as_u64().unwrap(),test);
+    }
+  )
+}
 
 compile_string!(empty, "");
 
@@ -41,8 +61,13 @@ compile_string!(table, "#table");
 //compile_string!(table_index_dot_index_name, "#table.field");
 
 
-compile_string!(column_define, "# A Working Program
+compile_string!(table_define, "# A Working Program
 
 ## Section Two
 
   #gravity = 9");
+
+test_math!(math_constant,"  #test = 10", 10);
+test_math!(math_add,"  #test = 1 + 1", 2);
+test_math!(math_multiply,"  #test = 2 * 2", 4);
+test_math!(math_divide,"  #test = 4 / 2", 2);
