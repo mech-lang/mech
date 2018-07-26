9637da032ee05874f0b59fb7d73b3a47e07a48b0
cmontella <cmontella@live.com>
2018-07-25 21:48:53 -0400

    clear main

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -14,29 +14,5 @@ use mech::{Runtime, Block, Constraint, Register};
 use rand::{Rng};
 
 fn main() {
-    let ball = Hasher::hash_str("math");
-    let db = make_db();
-    let col = db.store.get_column(ball, 3);
-    let val = vec![Value::from_u64(3)];
-    assert_eq!(col, Some(&val));
-}
-
-fn make_db() -> Core {
-  
-  let math = Hasher::hash_str("math");
-  let mut db = Core::new(1,1);
-
-  let txn = Transaction::from_changeset(vec![
-    Change::NewTable{tag: math, rows: 1, columns: 3}, 
-    Change::Add{table: math, row: 1, column: 1, value: Value::from_u64(1)},
-    Change::Add{table: math, row: 1, column: 2, value: Value::from_u64(2)},
-  ]); 
- 
-  // Make a block
-  let block = 
-
-  db.runtime.register_block(block.clone(), &mut db.store);
-  db.process_transaction(&txn);
-  db
 
 }
