a0a07f6d053f2e92691338d7cf6ab03b4fa350ed
cmontella <cmontella@live.com>
2018-07-26 22:33:55 -0400

    start a new test

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -20,19 +20,11 @@ use mech::Core;
 fn main() {
   let mut compiler = Compiler::new();
   let mut core = Core::new(10, 10);
-  let input = String::from("# Program
-  
-This is the first block
-  #x = 4 + 5
-
-This is a second block
-  #test = 2 * #x");
+  let input = String::from("#ball = [hello]");
   compiler.compile_string(input);
   core.register_blocks(compiler.blocks);
   core.step();
-  let table = Hasher::hash_str("test");
-  let row = 1;
-  let column = 1;
-  let test = 18;
-  assert_eq!(core.index(table,row,column).unwrap().as_u64().unwrap(),test);
+  println!("{:?}", compiler.syntax_tree);
+  println!("{:?}", core);
+  println!("{:?}", core.runtime);
 }
