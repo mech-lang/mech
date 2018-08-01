a8e9962b53e38cc643f5f3b5494a0127c38a1bb1
cmontella <cmontella@live.com>
2018-08-01 00:19:15 -0400

    rix runtime error

--- src/runtime.rs
+++ src/runtime.rs
@@ -262,7 +262,7 @@ impl Block {
           self.column_lengths.resize(memory as usize, 0);
         }
         self.memory.grow_to_fit(1, memory as usize);
-        self.column_lengths[memory as usize - 1] = 0;
+        self.column_lengths.push(0);
       },
       Constraint::Filter{ref comparator, lhs, rhs, memory} => {
         self.memory_registers.push(Register::memory(memory));
@@ -344,12 +344,14 @@ impl Block {
   }
 
   pub fn solve(&mut self, store: &mut Interner) {
+    println!("SOLVING {:?}", self);
     for step in &self.plan {
       match step {
         Constraint::ChangeScan{table, column, input} => {
           self.ready = clear_bit(self.ready, *input as usize - 1);
         }
         Constraint::Function{operation, parameters, memory} => {
+          println!("Function");
           // Pass the parameters to the appropriate function
           let op_fun = match operation {
             Function::Add => operations::math_add,
@@ -364,10 +366,13 @@ impl Block {
           operations::compare(comparator, *lhs as usize, *rhs as usize, *memory as usize, &mut self.memory, &mut self.column_lengths);
         },
         Constraint::CopyInput{input, memory} => {
+          println!("Copy Input");
           let register = &self.input_registers[*input as usize - 1];
           match store.get_column(register.table, register.column as usize) {
             Some(column) => {
+              println!("HERE1");
               self.column_lengths[*memory as usize - 1] = column.len() as u64;
+              println!("HERE2");
               operations::identity(column, *memory, &mut self.memory);
             },
             None => (),
@@ -408,6 +413,7 @@ impl Block {
           self.column_lengths[memory_ix - 1] = source_length as u64;
         },
         Constraint::Insert{memory, output, table, column} => {
+          println!("Insert");
           let output_memory_ix = self.output_registers[*output as usize - 1].column;
           match &mut self.memory.get_column_by_ix(output_memory_ix as usize) {
             Some(column_data) => {
