6c08a26085fa39a3f167f5142c20a4504d551c7c
cmontella <cmontella@live.com>
2018-08-04 14:49:57 -0400

    update test program

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -18,14 +18,26 @@ use mech::Hasher;
 use mech::Core;
 
 fn main() {
+  println!("system/timer ({:#x})", Hasher::hash_str("system/timer"));
   let mut compiler = Compiler::new();
   let mut core = Core::new(10, 100);
-  let input = String::from("#x = 2 + 1");
+  let input = String::from("# Bouncing Balls
+Define the environment
+  #ball = [x: 15 y: 9 vx: 18 vy: 9]
+  #system/timer = [tick: 1]
+  #gravity = 98
+
+Now update the block positions
+  ~ #system/timer.tick
+  #ball.x := #ball.x + #ball.vx
+  #ball.y := #ball.y + #ball.vy
+  #ball.vy := #ball.vy + #gravity");
   compiler.compile_string(input);
   core.register_blocks(compiler.blocks);
-  core.step();
   println!("{:?}", compiler.parse_tree);
   println!("{:?}", compiler.syntax_tree);
+  core.step();
   println!("{:?}", core);
   println!("{:?}", core.runtime);
+  
 }   
