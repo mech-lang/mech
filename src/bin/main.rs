ae173df94a72786a81e75faeed35d1852f44045d
cmontella <cmontella@live.com>
2018-09-25 12:41:55 -0400

    remove plan reference

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -9,7 +9,7 @@ use test::Bencher;
 use mech_core::{Core, Transaction, Change};
 use mech_core::{Value, Table};
 use mech_core::Hasher;
-use mech_core::{Function, Plan, Comparator};
+use mech_core::{Function, Comparator};
 use mech_core::{Runtime, Block, Constraint, Register};
 use rand::{Rng};
 
