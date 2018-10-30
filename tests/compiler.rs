b81c417ceb94eddcf28f2ea3b8fdadd52e9ff9fc
cmontella <cmontella@live.com>
2018-10-30 18:00:37 -0400

    modernize compile string test

--- tests/compiler.rs
+++ tests/compiler.rs
@@ -11,19 +11,10 @@ macro_rules! compile_string {
   ($func:ident, $test:tt) => (
     #[test]
     fn $func() {
-      let mut lexer = Lexer::new();
-      let mut parser = Parser::new();
       let mut compiler = Compiler::new();
-      lexer.add_string(String::from($test));
-      let tokens = lexer.get_tokens();
-      parser.text = String::from($test);
-      parser.add_tokens(&mut tokens.clone());
-      parser.build_parse_tree();
-      assert_eq!(parser.status, ParseStatus::Ready);
-      compiler.build_syntax_tree(parser.parse_tree);
-      let ast = compiler.syntax_tree.clone();
-      compiler.compile_blocks(ast);
-      assert_eq!(parser.status, ParseStatus::Ready);
+      let input = String::from($test);
+      compiler.compile_string(input);
+      assert_eq!(compiler.errors.is_empty(), true);
     }
   )
 }
