33e800860426c52589f6b8c63d2b01f2fc9115ae
cmontella <cmontella@live.com>
2018-11-06 17:31:29 -0500

    adding comparators back

--- src/operations.rs
+++ src/operations.rs
@@ -120,7 +120,8 @@ pub enum Comparator {
   LessThanOrEqual,
   GreaterThanOrEqual,
   Equal,
-  NotEqual
+  NotEqual,
+  Undefined
 }
 
 impl fmt::Debug for Comparator {
@@ -133,72 +134,47 @@ impl fmt::Debug for Comparator {
       Comparator::GreaterThanOrEqual => write!(f, ">="),
       Comparator::Equal => write!(f, "="),
       Comparator::NotEqual => write!(f, "!="),
+      Comparator::Undefined => write!(f, "Undefined Comparator"),
     }
   }
 }
 
-/*
-pub fn compare(comparator: &Comparator, lhs: usize, rhs: usize, output: usize, store: &mut Table, lengths: &mut Vec<u64>) {
-  let lhs_length = lengths[lhs - 1] as usize;
-  let rhs_length = lengths[rhs - 1] as usize;
-  let out = output as usize;
-  let mut out_length = 0;
-  if lhs_length == rhs_length {
-    for i in 1 .. lhs_length + 1 {     
-      match (store.index(i, lhs), store.index(i, rhs)) {
-        (Some(&Value::Number(lhs_val)), Some(&Value::Number(rhs_val))) => {
-          let truth = match comparator {
-            Comparator::LessThan           => Value::Bool(lhs_val < rhs_val),
-            Comparator::GreaterThan        => Value::Bool(lhs_val > rhs_val),
-            Comparator::LessThanOrEqual    => Value::Bool(lhs_val <= rhs_val),
-            Comparator::GreaterThanOrEqual => Value::Bool(lhs_val >= rhs_val),
-            Comparator::Equal              => Value::Bool(lhs_val == rhs_val),
-            Comparator::NotEqual           => Value::Bool(lhs_val != rhs_val),
-          };
-          store.set_cell(i, output, truth);
-        }, 
-        _ => {store.set_cell(i, out, Value::Empty);},
-      } 
-      out_length = lhs_length;
-    }
-  // Add vector to scalar  
-  } else if lhs_length == 1 && rhs_length > 1 {
-    for i in 1 .. rhs_length + 1 {     
-      match (store.index(1, lhs), store.index(i, rhs)) {
-        (Some(&Value::Number(lhs_val)), Some(&Value::Number(rhs_val))) => {
-          let truth = match comparator {
-            Comparator::LessThan           => Value::Bool(lhs_val < rhs_val),
-            Comparator::GreaterThan        => Value::Bool(lhs_val > rhs_val),
-            Comparator::LessThanOrEqual    => Value::Bool(lhs_val <= rhs_val),
-            Comparator::GreaterThanOrEqual => Value::Bool(lhs_val >= rhs_val),
-            Comparator::Equal              => Value::Bool(lhs_val == rhs_val),
-            Comparator::NotEqual           => Value::Bool(lhs_val != rhs_val),
-          };
-          store.set_cell(i, output, truth);
-        }, 
-        _ => {store.set_cell(i, out, Value::Empty);},
-      } 
-      out_length = rhs_length;
-    }
-  } else if lhs_length > 1 && rhs_length == 1 {
-    for i in 1 .. lhs_length + 1 {     
-      match (store.index(i, lhs), store.index(1, rhs)) {
-        (Some(&Value::Number(lhs_val)), Some(&Value::Number(rhs_val))) => {
-          let truth = match comparator {
-            Comparator::LessThan           => Value::Bool(lhs_val < rhs_val),
-            Comparator::GreaterThan        => Value::Bool(lhs_val > rhs_val),
-            Comparator::LessThanOrEqual    => Value::Bool(lhs_val <= rhs_val),
-            Comparator::GreaterThanOrEqual => Value::Bool(lhs_val >= rhs_val),
-            Comparator::Equal              => Value::Bool(lhs_val == rhs_val),
-            Comparator::NotEqual           => Value::Bool(lhs_val != rhs_val),
-          };
-          store.set_cell(i, output, truth);
-        }, 
-        _ => {store.set_cell(i, out, Value::Empty);},
+#[macro_export]
+macro_rules! comparator {
+  ($func_name:ident, $op:tt) => (
+    pub fn $func_name(lhs: &Table, lhs_rows: &Vec<Index>, lhs_columns: &Vec<Index>, 
+                      rhs: &Table, rhs_rows: &Vec<Index>, rhs_columns: &Vec<Index>,
+                      out: &mut Table) {
+                              // Get the math dimensions
+      let lhs_height = if lhs_rows.is_empty() { lhs.rows }
+                       else { lhs_rows.len() as u64 };
+      let lhs_width  = if lhs_columns.is_empty() { lhs.columns }
+                       else { lhs_columns.len() as u64 };
+      let rhs_height = if rhs_rows.is_empty() { rhs.rows }
+                       else { rhs_rows.len() as u64 };
+      let rhs_width  = if rhs_columns.is_empty() { rhs.columns }
+                       else { rhs_columns.len() as u64 }; 
+      let lhs_is_scalar = lhs_columns.is_empty() && lhs_width == 1 && lhs_rows.is_empty() && lhs_height == 1;
+      let rhs_is_scalar = rhs_columns.is_empty() && rhs_width == 1 && rhs_rows.is_empty() && rhs_height == 1;
+
+      // The tables are the same size, and we're operating over the whole of both
+      if lhs_columns.is_empty() && rhs_columns.is_empty() {
+        out.grow_to_fit(lhs_height, lhs_width);
+        for i in 0..lhs_width as usize {
+          for j in 0..lhs_height as usize {
+            match (&lhs.data[i][j], &rhs.data[i][j]) {
+              (Value::Number(x), Value::Number(y)) => {
+                out.data[i][j] = Value::Bool(x $op y);
+              },
+              _ => (),
+            }
+          }
+        }
       }
-      out_length = lhs_length; 
     }
-  }
-  lengths[out - 1] = out_length as u64;
+  )
 }
-*/
+
+comparator!{compare_greater_than, >}
+comparator!{compare_less_than, <}
+comparator!{compare_undefined, >}
