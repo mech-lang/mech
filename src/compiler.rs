682e80cfba595544d5fb6be66b469ed81fac67c7
Corey Montella <cmontella@live.com>
2018-11-19 23:19:09 -0500

    compile logic expression

--- src/compiler.rs
+++ src/compiler.rs
@@ -3,7 +3,7 @@
 // ## Preamble
 
 use mech_core::{Block, Constraint, Index, TableId};
-use mech_core::{Function, Comparator};
+use mech_core::{Function, Comparator, Logic};
 use mech_core::Hasher;
 use parser;
 use lexer::Lexer;
@@ -29,6 +29,7 @@ pub enum Node {
   Expression{ children: Vec<Node> },
   MathExpression{ children: Vec<Node> },
   FilterExpression{ name: String, children: Vec<Node> },
+  LogicExpression{ name: String, children: Vec<Node> },
   SelectExpression{ children: Vec<Node> },
   Data{ children: Vec<Node> },
   DataWatch{ children: Vec<Node> },
@@ -99,6 +100,7 @@ pub fn print_recurse(node: &Node, level: usize) {
     Node::MathExpression{children} => {print!("MathExpression\n"); Some(children)},
     Node::SelectExpression{children} => {print!("SelectExpression\n"); Some(children)},
     Node::FilterExpression{name, children} => {print!("FilterExpression({:?})\n", name); Some(children)},
+    Node::LogicExpression{name, children} => {print!("LogicExpression({:?})\n", name); Some(children)},
     Node::Constraint{children, ..} => {print!("Constraint\n"); Some(children)},
     Node::Identifier{name, id} => {print!("Identifier({}({:#x}))\n", name, id); None},
     Node::String{text} => {print!("String({:?})\n", text); None},
@@ -475,6 +477,43 @@ impl Compiler {
           constraints.append(&mut p.clone());
         }  
       },
+      Node::LogicExpression{name, children} => {
+        /*
+        let comparator = match name.as_ref() {
+          "&" => Comparator::And,
+          "|" => Comparator::Or,
+          _ => Comparator::Undefined,
+        };
+        self.table = Hasher::hash_string(format!("LogicExpression{:?},{:?}-{:?}", self.section, self.block, self.expression));
+        let mut output = TableId::Local(self.table);
+        let mut parameters: Vec<Vec<Constraint>> = vec![];
+        for child in children {
+          self.column += 1;
+          parameters.push(self.compile_constraint(child));
+        }
+        let mut parameter_registers: Vec<(TableId, Vec<Index>, Vec<Index>)> = vec![];
+        for parameter in &parameters {
+          match &parameter[0] {
+            Constraint::NewTable{id, rows, columns} => {
+              parameter_registers.push((id.clone(), vec![], vec![]));
+            },
+            Constraint::Scan{table, rows, columns} => {
+              parameter_registers.push((table.clone(), rows.clone(), columns.clone()));
+            },
+            Constraint::Function{operation, parameters, output} => {
+              for o in output {
+                parameter_registers.push((o.clone(), vec![], vec![]));
+              }
+            },
+            _ => (),
+          };
+        }
+        constraints.push(Constraint::NewTable{id: output.clone(), rows: 0, columns: 0});
+        constraints.push(Constraint::Filter{comparator, lhs: parameter_registers[0].clone(), rhs: parameter_registers[1].clone(), output: output.clone()});
+        for mut p in &parameters {
+          constraints.append(&mut p.clone());
+        }*/
+      },
       Node::MathExpression{children} => {
         let store_row = self.row;
         let store_col = self.column;
@@ -768,6 +807,19 @@ impl Compiler {
         }
         compiled.push(Node::FilterExpression{name, children});
       },
+      parser::Node::LogicExpression{children} => {
+        let result = self.compile_nodes(children);
+        let mut children: Vec<Node> = Vec::new();
+        let mut name = String::new();
+        for node in result {
+          match node {
+            Node::Token{token: Token::Space, ..} => (), 
+            Node::Token{token, byte} => name = byte_to_char(byte).unwrap().to_string(),
+            _ => children.push(node),
+          }
+        }
+        compiled.push(Node::LogicExpression{name, children});
+      },
       parser::Node::InlineTable{children} => {
         let result = self.compile_nodes(children);
         let mut children: Vec<Node> = Vec::new();
@@ -1051,6 +1103,7 @@ impl Compiler {
         compiled.push(Node::Function{name, children: vec![input.clone()]});
       },
       // Pass through nodes. These will just be omitted
+      parser::Node::LogicOperator{children} |
       parser::Node::Subscript{children} |
       parser::Node::DataOrConstant{children} |
       parser::Node::SpaceOrTab{children} |
@@ -1127,6 +1180,7 @@ fn byte_to_char(byte: u8) -> Option<char> {
     32 => Some(' '),
     33 => Some('!'),
     35 => Some('#'),
+    38 => Some('&'),
     42 => Some('*'),
     43 => Some('+'),
     45 => Some('-'),
@@ -1170,6 +1224,7 @@ fn byte_to_char(byte: u8) -> Option<char> {
     120 => Some('x'),
     121 => Some('y'),    
     122 => Some('z'),
+    124 => Some('|'),
     126 => Some('~'),
     65 => Some('A'),
     66 => Some('B'),
