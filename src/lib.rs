64c75852b0674babf73aa6479205c2d245bf4239
Corey Montella <cmontella@live.com>
2018-11-19 23:18:56 -0500

    export logic operator

--- src/lib.rs
+++ src/lib.rs
@@ -37,7 +37,7 @@ mod operations;
 pub use self::database::{Transaction, Change, Interner};
 pub use self::table::{Value, Index, TableId, Table};
 pub use self::indexes::{TableIndex, Hasher};
-pub use self::operations::{Function, Comparator};
+pub use self::operations::{Function, Comparator, Logic};
 pub use self::runtime::{Runtime, Block, Constraint, Register};
 
 // ## Core
