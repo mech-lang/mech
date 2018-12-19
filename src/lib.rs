745a49ce76c713a79d0134d414e373ceef830302
cmontella <cmontella@live.com>
2018-12-19 15:01:09 -0500

    re-export some functions

--- src/lib.rs
+++ src/lib.rs
@@ -5,3 +5,8 @@
 extern crate mech_core;
 extern crate mech_syntax;
 extern crate mech_server;
+
+pub use mech_core::Core;
+pub use mech_syntax::compiler::Compiler;
+pub use mech_server::program::{ProgramRunner, RunLoop, RunLoopMessage};
+pub use mech_server::client::{ClientHandler};
