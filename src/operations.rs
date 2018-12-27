2f6e966b54bbad145922ab67d051eb38be4cdedd
Corey Montella <cmontella@live.com>
2018-12-26 19:45:21 -0500

    fix comparator

--- src/operations.rs
+++ src/operations.rs
@@ -164,25 +164,34 @@ macro_rules! comparator {
     pub fn $func_name(lhs: &Table, lhs_rows: &Vec<Value>, lhs_columns: &Vec<Value>, 
                       rhs: &Table, rhs_rows: &Vec<Value>, rhs_columns: &Vec<Value>,
                       out: &mut Table) {
-                       /* 
+
       // Get the math dimensions
-      let lhs_height = if lhs_rows.is_empty() { lhs.rows }
-                       else { lhs_rows.len() as u64 };
       let lhs_width  = if lhs_columns.is_empty() { lhs.columns }
                        else { lhs_columns.len() as u64 };
+      let rhs_width  = if rhs_columns.is_empty() { rhs.columns }
+                       else { rhs_columns.len() as u64 };      
+      let lhs_height = if lhs_rows.is_empty() { lhs.rows }
+                       else { lhs_rows.len() as u64 };
       let rhs_height = if rhs_rows.is_empty() { rhs.rows }
                        else { rhs_rows.len() as u64 };
-      let rhs_width  = if rhs_columns.is_empty() { rhs.columns }
-                       else { rhs_columns.len() as u64 }; 
-      let lhs_is_scalar = lhs_columns.is_empty() && lhs_width == 1 && lhs_rows.is_empty() && lhs_height == 1;
-      let rhs_is_scalar = rhs_columns.is_empty() && rhs_width == 1 && rhs_rows.is_empty() && rhs_height == 1;
 
-      // The tables are the same size, and we're operating over the whole of both
-      if lhs_columns.is_empty() && rhs_columns.is_empty() {
-        out.grow_to_fit(lhs_height, lhs_width);
+      let lhs_is_scalar = lhs_width == 1 && lhs_height == 1;
+      let rhs_is_scalar = rhs_width == 1 && rhs_height == 1;
+
+      // The tables are the same size
+      if lhs_width == rhs_width && lhs_height == rhs_height {
+        out.grow_to_fit(lhs_height, lhs_width);        
         for i in 0..lhs_width as usize {
+          let lcix = if lhs_columns.is_empty() { i }
+                    else { lhs_columns[i].as_u64().unwrap() as usize - 1 };
+          let rcix = if rhs_columns.is_empty() { i }
+                    else { rhs_columns[i].as_u64().unwrap() as usize - 1 };
           for j in 0..lhs_height as usize {
-            match (&lhs.data[i][j], &rhs.data[i][j]) {
+            let lrix = if lhs_rows.is_empty() { j }
+                       else { lhs_rows[j].as_u64().unwrap() as usize - 1 };
+            let rrix = if rhs_rows.is_empty() { j }
+                       else { rhs_rows[j].as_u64().unwrap() as usize - 1 };
+            match (&lhs.data[lcix][lrix], &rhs.data[rcix][rrix]) {
               (Value::Number(x), Value::Number(y)) => {
                 out.data[i][j] = Value::Bool(x $op y);
               },
@@ -192,13 +201,20 @@ macro_rules! comparator {
         }
       // Operate with scalar on the left
       } else if lhs_is_scalar {
-        out.grow_to_fit(rhs_height, rhs_width);
-        for column in rhs_columns {
-          for j in 1..rhs_height + 1 {
-            match (&lhs.data[0][0], rhs.index(&Index::Index(j), column).unwrap()) {
+        out.grow_to_fit(rhs_height, rhs_width);        
+        for i in 0..rhs_width as usize {
+          let lcix = if lhs_columns.is_empty() { 0 }
+                    else { lhs_columns[0].as_u64().unwrap() as usize - 1 };
+          let rcix = if rhs_columns.is_empty() { i }
+                    else { rhs_columns[i].as_u64().unwrap() as usize - 1 };
+          for j in 0..rhs_height as usize {
+            let lrix = if lhs_rows.is_empty() { 0 }
+                       else { lhs_rows[0].as_u64().unwrap() as usize - 1 };
+            let rrix = if rhs_rows.is_empty() { j }
+                       else { rhs_rows[j].as_u64().unwrap() as usize - 1 };
+            match (&lhs.data[lcix][lrix], &rhs.data[rcix][rrix]) {
               (Value::Number(x), Value::Number(y)) => {
-                
-                out.data[0][j as usize - 1] = Value::Bool(x $op y);
+                out.data[i][j] = Value::Bool(x $op y);
               },
               _ => (),
             }
@@ -206,31 +222,26 @@ macro_rules! comparator {
         }
       // Operate with scalar on the right
       } else if rhs_is_scalar {
-        out.grow_to_fit(lhs_height, lhs_width);
-        for column in lhs_columns {
-          for j in 1..lhs_height + 1 {
-            match (lhs.index(&Index::Index(j), column).unwrap(), &rhs.data[0][0]) {
+        out.grow_to_fit(lhs_height, lhs_width);        
+        for i in 0..rhs_width as usize {
+          let lcix = if lhs_columns.is_empty() { i }
+                    else { lhs_columns[i].as_u64().unwrap() as usize - 1 };
+          let rcix = if rhs_columns.is_empty() { 0 }
+                    else { rhs_columns[0].as_u64().unwrap() as usize - 1 };
+          for j in 0..lhs_height as usize {
+            let lrix = if lhs_rows.is_empty() { j }
+                       else { lhs_rows[j].as_u64().unwrap() as usize - 1 };
+            let rrix = if rhs_rows.is_empty() { 0 }
+                       else { rhs_rows[0].as_u64().unwrap() as usize - 1 };
+            match (&lhs.data[lcix][lrix], &rhs.data[rcix][rrix]) {
               (Value::Number(x), Value::Number(y)) => {
-                out.data[0][j as usize - 1] = Value::Bool(x $op y);
+                out.data[i][j] = Value::Bool(x $op y);
               },
               _ => (),
             }
           }
         }
-      // Operate on a selection of columns
-      } else if rhs_width == lhs_width && lhs_height == rhs_height &&!lhs_columns.is_empty() && !lhs_columns.is_empty()  {
-        out.grow_to_fit(lhs_height, lhs_width);
-        for (i, (lhs_column, rhs_column)) in lhs_columns.iter().zip(rhs_columns).enumerate() {
-          for j in 1..lhs_height + 1 {
-            match (lhs.index(&Index::Index(j), lhs_column).unwrap(), rhs.index(&Index::Index(j), rhs_column).unwrap()) {
-              (Value::Number(x), Value::Number(y)) => {
-                out.data[i][j as usize - 1] = Value::Bool(x $op y);
-              },
-              _ => (),
-            }
-          }
-        }
-      }*/
+      }
     }
   )
 }
