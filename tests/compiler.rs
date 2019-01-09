e6ee28ed50462022e4d10d793b740b7512bde847
cmontella <cmontella@live.com>
2019-01-09 14:40:11 -0500

    add test

--- tests/compiler.rs
+++ tests/compiler.rs
@@ -279,3 +279,21 @@ block
 
 block
   #test = #ball.y", 11);
+
+test_math!(set_rhs_math_filters_logic,"
+block
+  #ball = [x y vx vy
+           1 2 3 4
+           5 6 7 8
+           9 10 11 12]
+  #system/timer = [resolution: 15 tick: 0]
+  #gravity = 2
+
+block
+  ix = #ball.vy > 10
+  iy = #ball.vy < 5
+  ixx = ix | iy
+  #ball.y{ixx} := #ball.vy * 9099
+
+block
+  #test = #ball{1,2} + #ball{3,2}", 145584);
