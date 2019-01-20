1240da6b9690db0b480e177f5bf92b774c122d08
Corey Montella <cmontella@live.com>
2019-01-20 16:38:59 -0500

    new test

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -20,39 +20,10 @@ use mech_core::Core;
 fn main() {
   let mut core = Core::new(100, 100);
   let mut compiler = Compiler::new();
-  let input = String::from("# Bouncing Balls
-
-Define the environment
-  #html/event/click = [x: 0 y: 0]
-  #ball = [x  y vx vy
-           15 9 40 9]
-  #system/timer = [resolution: 15, tick: 0]
-  #gravity = 2
-  #boundary = 30
-
-Now update the block positions
-  ~ #system/timer.tick
-  #ball.x := #ball.x + #ball.vx
-  #ball.y := #ball.y + #ball.vy
-  #ball.vy := #ball.vy + #gravity
-
-Keep the balls within the y boundary
-  ~ #ball.y
-  iy = #ball.y > #boundary
-  #ball.y{iy} := #boundary
-  #ball.vy{iy} := -#ball.vy * 80 / 100
-
-Keep the balls within the x boundary
-  ~ #ball.x
-  ix = #ball.x > #boundary
-  ixx = #ball.x < 0
-  #ball.x{ix} := #boundary
-  #ball.x{ixx} := 0
-  #ball.vx{ix | ixx} := -#ball.vx * 80 / 100
-
-Create ball on click
-  ~ #html/event/click.x
-  #ball += [x: 10 y: 10 vx: 40 vy: 0]");
+  let input = String::from("
+block
+  x = 1:100000
+  z = x + x");
 
   compiler.compile_string(input);
   core.register_blocks(compiler.blocks.clone());
