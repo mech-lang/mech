1e36980726401a17f5f908b9fad79067a728cf1e
Corey Montella <cmontella@live.com>
2019-01-24 12:57:28 -0500

    fix program extern

--- src/lib.rs
+++ src/lib.rs
@@ -5,8 +5,9 @@
 extern crate mech_core;
 extern crate mech_syntax;
 extern crate mech_server;
+extern crate mech_program;
 
 pub use mech_core::Core;
 pub use mech_syntax::compiler::Compiler;
-pub use mech_server::program::{ProgramRunner, RunLoop, RunLoopMessage};
+pub use mech_program::{ProgramRunner, RunLoop, RunLoopMessage};
 pub use mech_server::client::{ClientHandler};
