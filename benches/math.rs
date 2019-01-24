a719e5aa7d221151a045382cf65ae688591a282e
Corey Montella <cmontella@live.com>
2019-01-24 18:23:46 -0500

    add more benches

--- benches/math.rs
+++ benches/math.rs
@@ -7,6 +7,38 @@ use mech::{Compiler, Core};
 use test::Bencher;
 
 #[bench]
+fn add_two_vectors_10(b:&mut Bencher) {
+  let mut core = Core::new(100, 100);
+  let mut compiler = Compiler::new();
+  let input = String::from("
+block
+  x = 1:10
+  z = x + x");
+  compiler.compile_string(input);
+  b.iter(|| {
+    core.register_blocks(compiler.blocks.clone());
+    core.step();
+    core.clear();
+  });
+}
+
+#[bench]
+fn add_two_vectors_10e2(b:&mut Bencher) {
+  let mut core = Core::new(100, 100);
+  let mut compiler = Compiler::new();
+  let input = String::from("
+block
+  x = 1:100
+  z = x + x");
+  compiler.compile_string(input);
+  b.iter(|| {
+    core.register_blocks(compiler.blocks.clone());
+    core.step();
+    core.clear();
+  });
+}
+
+#[bench]
 fn add_two_vectors_10e3(b:&mut Bencher) {
   let mut core = Core::new(100, 100);
   let mut compiler = Compiler::new();
@@ -23,12 +55,44 @@ block
 }
 
 #[bench]
+fn add_two_vectors_10e4(b:&mut Bencher) {
+  let mut core = Core::new(100, 100);
+  let mut compiler = Compiler::new();
+  let input = String::from("
+block
+  x = 1:10000
+  z = x + x");
+  compiler.compile_string(input);
+  b.iter(|| {
+    core.register_blocks(compiler.blocks.clone());
+    core.step();
+    core.clear();
+  });
+}
+
+#[bench]
 fn add_two_vectors_10e5(b:&mut Bencher) {
   let mut core = Core::new(100, 100);
   let mut compiler = Compiler::new();
   let input = String::from("
 block
   x = 1:100000
+  z = x + x");
+  compiler.compile_string(input);
+  b.iter(|| {
+    core.register_blocks(compiler.blocks.clone());
+    core.step();
+    core.clear();
+  });
+}
+
+#[bench]
+fn add_two_vectors_10e6(b:&mut Bencher) {
+  let mut core = Core::new(100, 100);
+  let mut compiler = Compiler::new();
+  let input = String::from("
+block
+  x = 1:1000000
   z = x + x");
   compiler.compile_string(input);
   b.iter(|| {
