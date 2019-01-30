d31f311c862ae914ea0e2edc3c6c381254484a51
Corey Montella <cmontella@live.com>
2019-01-30 17:01:30 -0500

    new main test

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -21,13 +21,29 @@ fn main() {
   let mut core = Core::new(100, 100);
   let mut compiler = Compiler::new();
   let input = String::from("
+
 block
-  #test = [ x y | 5 6]");
+  #start = [x: 3]
+
+block
+  ~ #start.x
+  x = 1:10
+  xp = x * 0 + 3
+  y = 11:20
+  #z = [xp y]
+  #qrs += [y: 33]
+  
+block
+  #test = #z{1,1} + #z{1,2} + #z{2,1} + #z{1,1}
+
+block
+  #qrs = [y|1]
+");
 
   compiler.compile_string(input);
   core.register_blocks(compiler.blocks.clone());
-  println!("{:?}", compiler.parse_tree);
-  println!("{:?}", compiler.syntax_tree);
+  //println!("{:?}", compiler.parse_tree);
+  //println!("{:?}", compiler.syntax_tree);
   core.step();
   println!("{:?}", core);
   println!("{:?}", core.runtime); 
