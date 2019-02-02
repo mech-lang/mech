428f21ddbc01f18b89f68c542749c2267d8b44c0
Corey Montella <cmontella@live.com>
2019-02-02 13:23:06 -0500

    update

--- www/index.js
+++ www/index.js
@@ -1,3 +1,25 @@
-import * as wasm from "hello-wasm-pack";
+import * as mech from "wasmtest";
 
-wasm.greet();
+let code = document.createElement("textarea");
+code.setAttribute("class", "code");
+code.setAttribute("id", "code");
+code.innerHTML =  "# Program";
+
+let compile = document.createElement("button");
+compile.setAttribute("id", "compile");
+compile.innerHTML =  "Compile";
+
+let container = document.createElement("div");
+container.setAttribute("class","container");
+
+container.appendChild(code);
+container.appendChild(compile);
+
+document.body.appendChild(container);
+
+
+
+document.getElementById("compile").addEventListener("click", function() {
+  let code = document.getElementById("code");
+  mech.compile(code.value);
+});
