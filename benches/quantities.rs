079c3b68ec5362b6b8e105e98d72c2e8f07e559d
Corey Montella <cmontella@live.com>
2019-02-07 20:55:30 -0500

    add more benches

--- benches/quantities.rs
+++ benches/quantities.rs
@@ -4,7 +4,7 @@ extern crate test;
 extern crate mech_core;
 
 use test::Bencher;
-use mech_core::{Quantity, QuantityMath, make_quantity};
+use mech_core::{Quantity, ToQuantity, QuantityMath, make_quantity};
 
 #[bench]
 fn quantity_add(b:&mut Bencher) {
@@ -40,4 +40,58 @@ fn quantity_divide(b:&mut Bencher) {
     b.iter(|| {
         let sub = x.divide(y);
     });
+}
+
+#[bench]
+fn bench_quantity_add_100000(b:&mut Bencher) {
+    let y:i32 = -1;
+    let y_quantity = y.to_quantity();
+    b.iter(|| {
+        for x in (0..100_000).map(|x: i32| x.to_quantity()) {
+            test::black_box(x.add(y_quantity));
+        }
+    });
+}
+
+#[bench]
+fn bench_quantity_subtract_100000(b:&mut Bencher) {
+    let y:i32 = -1;
+    let y_quantity = y.to_quantity();
+    b.iter(|| {
+        for x in (0..100_000).map(|x: i32| x.to_quantity()) {
+            test::black_box(x.sub(y_quantity));
+        }
+    });
+}
+
+#[bench]
+fn bench_quantity_divide_100000(b:&mut Bencher) {
+    let y:i32 = -1;
+    let y_quantity = y.to_quantity();
+    b.iter(|| {
+        for x in (0..100_000).map(|x: i32| x.to_quantity()) {
+            test::black_box(x.divide(y_quantity));
+        }
+    });
+}
+
+#[bench]
+fn bench_quantity_multiply_100000(b:&mut Bencher) {
+    let y:i32 = -1;
+    let y_quantity = y.to_quantity();
+    b.iter(|| {
+        for x in (0..100_000).map(|x: i32| x.to_quantity()) {
+            test::black_box(x.multiply(y_quantity));
+        }
+    });
+}
+
+#[bench]
+fn bench_quantity_normal_add_100000(b:&mut Bencher) {
+    let y:i32 = -1;
+    b.iter(|| {
+        for x in 0..100_000 {
+            test::black_box(x * y);
+        }
+     });
 }
