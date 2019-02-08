9f7f1a4e7923db298a2323d8bd0bf491c6974080
Corey Montella <cmontella@live.com>
2019-02-08 16:37:58 -0500

    quantities example

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -1,9 +1,18 @@
 extern crate mech_core;
 
-use mech_core::Core;
+use mech_core::{Core, Quantity, ToQuantity, QuantityMath, make_quantity};
 
 fn main() {
 
-  Core::new(100,100);
+  let x = make_quantity(1,-1,1);
+  let y = make_quantity(2,-1,2);
+  let z = make_quantity(3,-1,0);
+  println!("x {:#066b}", x);
+  println!("y {:#066b}", y);
+  println!("z {:#066b}", z);
+  let q = 0.1 * 0.2;
+  println!("{}", q);
+  let r = x.add(y);
+  println!("Result: {}", r.to_string());
 
 }
