d8789d87db963f41cfbe0b0aed7beff544d6bb6a
Corey Montella <cmontella@live.com>
2019-02-13 11:17:38 -0500

    trigger

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -4,15 +4,18 @@ use mech_core::{Core, Quantity, ToQuantity, QuantityMath, make_quantity};
 
 fn main() {
 
-  let x = make_quantity(1,-1,1);
-  let y = make_quantity(2,-1,2);
-  let z = make_quantity(3,-1,0);
+  let w = make_quantity(0,0,0);
+  let x = make_quantity(14336512000000,-12,0);
+  let y = make_quantity(8,-1,0);
+  let z = make_quantity(1,0,0);
   println!("x {:#066b}", x);
   println!("y {:#066b}", y);
   println!("z {:#066b}", z);
   let q = 0.1 * 0.2;
   println!("{}", q);
-  let r = x.add(y);
-  println!("Result: {}", r.to_string());
+  //let p = x.add(z);
+  let q = w.sub(x);
+  let r = q.multiply(y);
+  println!("Result: {} {}", r.mantissa(), r.range());
 
 }
