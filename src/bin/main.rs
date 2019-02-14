2e6c934d4a4db2fde5919172b9c798aebc605dd7
Corey Montella <cmontella@live.com>
2019-02-14 17:06:06 -0500

    new example

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -6,7 +6,7 @@ use mech_syntax::parser::{Parser, ParseStatus, Node};
 use mech_syntax::compiler::Compiler;
 use mech_core::Block;
 use mech_core::{Change, Transaction};
-use mech_core::{Value};
+use mech_core::{Value, Index};
 use mech_core::Hasher;
 use mech_core::Core;
 
@@ -16,37 +16,27 @@ fn main() {
   let input = String::from("# Bouncing Balls
 
 Define the environment
-  #html/event/click = [x: 123 y: 456]
-  x = 1:5
+  #html/event/click = [|x y|]
+  range = 1:5
+  x = range * 30
   v = x * 0
   #ball = [|x y vx vy| x x v v]
   #system/timer = [resolution: 15, tick: 0]
-  #gravity = 2
-  #boundary = 420
+  #gravity = 1
+  #boundary-y = 820
+  #boundary-x = 500
 
-## Update condition
-
-Now update the block positions
+Update the block positions on each tick of the timer
   ~ #system/timer.tick
   #ball.x := #ball.x + #ball.vx
   #ball.y := #ball.y + #ball.vy
   #ball.vy := #ball.vy + #gravity
 
-## Boundary Condition
-
 Keep the balls within the y boundary
   ~ #system/timer.tick
-  iy = #ball.y > #boundary
-  #ball.y{iy} := #boundary
-  #ball.vy{iy} := -#ball.vy
-
-## Create More Balls
-
-Create ball on click
-  ~ #html/event/click.x
-  x = #html/event/click.x
-  y = #html/event/click.y
-  #ball += [x: x, y: y, vx: 0, vy: 0]");
+  iy = #ball.y > #boundary-y
+  #ball.y{iy} := #boundary-y
+  #ball.vy{iy} := -#ball.vy * 0.80");
 
   compiler.compile_string(input);
   core.register_blocks(compiler.blocks.clone());
@@ -54,5 +44,21 @@ Create ball on click
   //println!("{:?}", compiler.syntax_tree);
   core.step();
   println!("{:?}", core);
-  println!("{:?}", core.runtime); 
+  //println!("{:?}", core.runtime); 
+
+  for i in 0..296 {
+    println!("--------------------------------------------");
+    let table_id = Hasher::hash_str("system/timer");
+    let change = Change::Set{table: table_id, 
+                              row: Index::Index(1 as u64), 
+                              column: Index::Index(2 as u64),
+                              value: Value::from_u64(i as u64),
+                            };
+    let txn = Transaction::from_change(change.clone());
+    core.process_transaction(&txn);
+  }
+  println!("{:?}", core);
+  println!("{:?}", core.runtime);
+
+
 }
