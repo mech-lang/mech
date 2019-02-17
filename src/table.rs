8d90b8180cf35e1b743eb73bb0634c9f0e7d2710
Corey Montella <cmontella@live.com>
2019-02-16 22:45:20 -0500

    fix constant collision

--- src/table.rs
+++ src/table.rs
@@ -51,7 +51,7 @@ impl Value {
 
   pub fn as_u64(&self) -> Option<u64> {
     match self {
-      Value::Number(n) => Some(n.mantissa() as u64),
+      Value::Number(n) => Some(n.to_float() as u64),
       _ => None,
     }
   }
