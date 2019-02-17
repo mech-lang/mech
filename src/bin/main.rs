5c6910686ab827145b83f065f24c34a07d3ed397
Corey Montella <cmontella@live.com>
2019-02-17 17:45:15 -0500

    new example

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -9,45 +9,28 @@ use mech_core::{Change, Transaction};
 use mech_core::{Value, Index};
 use mech_core::Hasher;
 use mech_core::Core;
+use std::time::{Duration, SystemTime};
 
 fn main() {
-  let mut core = Core::new(100, 100);
+  let mut core = Core::new(10000000, 100);
   let mut compiler = Compiler::new();
   let input = String::from("# Bouncing Balls
 
 Define the environment
-  #html/event/click = [|x y|]
-  range = 1:5
-  x = range * 30
-  v = x * 0
-  #ball = [|x y vx vy| x x v v]
-  #system/timer = [resolution: 15, tick: 0]
-  #gravity = 1
-  #boundary-y = 820
-  #boundary-x = 500
-
-Update the block positions on each tick of the timer
-  ~ #system/timer.tick
-  #ball.x := #ball.x + #ball.vx
-  #ball.y := #ball.y + #ball.vy
-  #ball.vy := #ball.vy + #gravity
-
-Keep the balls within the y boundary
-  ~ #system/timer.tick
-  iy = #ball.y > #boundary-y
-  #ball.y{iy} := #boundary-y
-  #ball.vy{iy} := -#ball.vy * 0.80");
+  #test = 10000 + 1");
 
   compiler.compile_string(input);
   core.register_blocks(compiler.blocks.clone());
   //println!("{:?}", compiler.parse_tree);
-  //println!("{:?}", compiler.syntax_tree);
+  println!("{:?}", compiler.syntax_tree);
   core.step();
   println!("{:?}", core);
-  //println!("{:?}", core.runtime); 
+  println!("{:?}", core.runtime); 
 
-  for i in 0..296 {
-    println!("--------------------------------------------");
+  /*
+  let now = SystemTime::now();
+  let n = 100;
+  for i in 0..n {
     let table_id = Hasher::hash_str("system/timer");
     let change = Change::Set{table: table_id, 
                               row: Index::Index(1 as u64), 
@@ -57,8 +40,18 @@ Keep the balls within the y boundary
     let txn = Transaction::from_change(change.clone());
     core.process_transaction(&txn);
   }
-  println!("{:?}", core);
-  println!("{:?}", core.runtime);
-
+  //println!("{:?}", core);
+  match now.elapsed() {
+       Ok(elapsed) => {
+           // it prints '2'
+           let time: f32 = elapsed.as_millis() as f32;
+           println!("{}ms", time / n as f32);
+       }
+       Err(e) => {
+           // an error occurred!
+           println!("Error: {:?}", e);
+       }
+   }
+  println!("{:?}", core);*/
 
 }
