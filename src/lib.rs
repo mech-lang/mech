fb947928745fbaacec430deba7cfb09fb2ac9c5f
Corey Montella <cmontella@live.com>
2019-02-18 17:05:06 -0500

    create a canvas, list tables

--- src/lib.rs
+++ src/lib.rs
@@ -142,12 +142,29 @@ impl Core {
       output
   }
 
+  pub fn add_canvas(&self) -> Result<(), JsValue> {
+    let window = web_sys::window().expect("no global `window` exists");
+    let document = window.document().expect("should have a document on window");
+    let body = document.body().expect("document should have a body");
+    let drawing_area = document.get_element_by_id("drawing").unwrap();
+    let canvas = document.create_element("canvas")?;
+    canvas.set_attribute("id","canvas");
+    canvas.set_attribute("width", "500");
+    canvas.set_attribute("height", "820");
+    canvas.set_attribute("style", "background-color: rgb(226, 79, 94)");
+    drawing_area.append_child(&canvas)?;
+
+    self.render_balls();
+
+    Ok(())
+  }
+
   pub fn render_balls(&self) -> Result<(), JsValue> {
     let window = web_sys::window().expect("no global `window` exists");
     let document = window.document().expect("should have a document on window");
     let body = document.body().expect("document should have a body");
 
-    let canvas = document.get_element_by_id("drawing area").unwrap();
+    let canvas = document.get_element_by_id("canvas").unwrap();
     let canvas: web_sys::HtmlCanvasElement = canvas
                 .dyn_into::<web_sys::HtmlCanvasElement>()
                 .map_err(|_| ())
@@ -173,6 +190,22 @@ impl Core {
 
     Ok(())
   } 
+
+  pub fn list_global_tables(&self) -> Result<(), JsValue> {
+    let window = web_sys::window().expect("no global `window` exists");
+    let document = window.document().expect("should have a document on window");
+    let body = document.body().expect("document should have a body");
+    let table_list_div = document.create_element("div")?;
+    let table_list = document.create_element("ul")?;
+    for (table_name, table) in self.core.store.tables.map.iter() {
+      let table_list_item = document.create_element("li")?;
+      table_list_item.set_inner_html(self.core.store.names.get(table_name).unwrap());
+      table_list.append_child(&table_list_item)?;
+    }
+    table_list_div.append_child(&table_list)?;
+    body.append_child(&table_list_div)?;
+    Ok(())
+  }
 
 }
 
