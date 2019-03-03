ceac5c1c506648f91a158f1b60fc5dc33cc2bf6b
Corey Montella <cmontella@live.com>
2019-03-03 00:46:13 -0500

    rename test function

--- tests/compiler.rs
+++ tests/compiler.rs
@@ -19,7 +19,7 @@ macro_rules! compile_string {
   )
 }
 
-macro_rules! test_math {
+macro_rules! test_mech {
   ($func:ident, $input:tt, $test:expr) => (
     #[test]
     fn $func() {
@@ -62,7 +62,7 @@ compile_string!(table_define_data, "#table =  [x y z | 1 2 3]");
 compile_string!(table_define_data_math, "#table = [x      y          z|
                                                    1 * 2, 4 + 7 * 9, 3]");
 
-test_math!(table_define_program, "# A Working Program
+test_mech!(table_define_program, "# A Working Program
 
 ## Section Two
 
@@ -70,13 +70,13 @@ test_math!(table_define_program, "# A Working Program
 
 // ## Select
 
-test_math!(select_table,"  
+test_mech!(select_table,"  
 block
   #x = 500
 block
   #test = #x", Value::from_i64(500));
 
-test_math!(select_table_reverse_ordering,"  
+test_mech!(select_table_reverse_ordering,"  
 block
   #test = #x
 block
@@ -84,61 +84,61 @@ block
 
 // ## Math
 
-test_math!(math_constant,"#test = 10", Value::from_i64(10));
+test_mech!(math_constant,"#test = 10", Value::from_i64(10));
 
-test_math!(math_add,"#test = 1 + 1", Value::from_i64(2));
+test_mech!(math_add,"#test = 1 + 1", Value::from_i64(2));
 
-test_math!(math_multiply,"#test = 2 * 2", Value::from_i64(4));
+test_mech!(math_multiply,"#test = 2 * 2", Value::from_i64(4));
 
-test_math!(math_divide,"#test = 4 / 2", Value::Number(make_quantity(20000,-4,0)));
+test_mech!(math_divide,"#test = 4 / 2", Value::Number(make_quantity(20000,-4,0)));
 
-test_math!(math_two_terms,"#test = 1 + 2 * 9", Value::from_i64(19));
+test_mech!(math_two_terms,"#test = 1 + 2 * 9", Value::from_i64(19));
 
-test_math!(math_constant_collision,"#test = 10000 + 1", Value::from_i64(10001));
+test_mech!(math_constant_collision,"#test = 10000 + 1", Value::from_i64(10001));
 
-test_math!(math_multiple_variable_graph,"block
+test_mech!(math_multiple_variable_graph,"block
   a = z * 5
   #test = d * z + a
   d = 9 * z
   z = 5", Value::from_i64(250));
 
-test_math!(math_multiple_variable_graph_new_ordering,"block
+test_mech!(math_multiple_variable_graph_new_ordering,"block
   #test = d * z + a
   a = z * 5
   z = 5
   d = 9 * z", Value::from_i64(250));
 
-test_math!(math_on_whole_table,"
+test_mech!(math_on_whole_table,"
 block
   #x = 500
 block
   #test = #x + 5", Value::from_i64(505));
 
-test_math!(select_column_by_id,"  
+test_mech!(select_column_by_id,"  
 block
   #ball = [x: 56 y: 2 vx: 3 vy: 4]
 block
   #test = #ball.x", Value::from_i64(56));
 
-test_math!(math_multiple_rows_select,"
+test_mech!(math_multiple_rows_select,"
 block
   #ball = [x: 15 y: 9 vx: 18 vy: 0]
 block
   #test = #ball.x + #ball.y * #ball.vx", Value::from_i64(177));
 
-test_math!(math_const_and_select,"
+test_mech!(math_const_and_select,"
 block
   #ball = [x: 15 y: 9 vx: 18 vy: 0]
 block
   #test = 9 + #ball.x", Value::from_i64(24));
 
-test_math!(math_select_and_const,"
+test_mech!(math_select_and_const,"
 block
   #ball = [x: 15 y: 9 vx: 18 vy: 0]
 block
   #test = #ball.x + 9", Value::from_i64(24));
 
-test_math!(partial_bouncing_ball,"# Bouncing Balls
+test_mech!(partial_bouncing_ball,"# Bouncing Balls
 Define the environment
   #ball = [x: 15 y: 9 vx: 18 vy: 9]
   #system/timer = [resolution: 1000]
@@ -150,7 +150,7 @@ Now update the block positions
   dt = #system/timer.resolution
   #test = x + y * dt", Value::from_i64(18033));
 
-test_math!(math_add_columns,"
+test_mech!(math_add_columns,"
 block
   #ball = [|x y|
             1 2
@@ -159,7 +159,7 @@ block
 block
   #test = #ball.x + #ball.y", Value::from_i64(3));
 
-test_math!(math_add_matrices,"
+test_mech!(math_add_matrices,"
 block
   x = [1 2 3
        4 5 6
@@ -169,50 +169,50 @@ block
        16 17 18]
   #test = x + y", Value::from_i64(11));
 
-test_math!(math_scalar_plus_vector,"
+test_mech!(math_scalar_plus_vector,"
 block
   x = 3:6
   #test = 5 + x", Value::from_i64(8));
 
-test_math!(math_vector_plus_scalar,"
+test_mech!(math_vector_plus_scalar,"
 block
   x = 3:6
   #test = x + 5", Value::from_i64(8));
 
-test_math!(math_negation_double_negative,"
+test_mech!(math_negation_double_negative,"
 block
   y = -13
   #test = -y", Value::from_i64(13));
 
-test_math!(math_parenthetical_expression_constants,"
+test_mech!(math_parenthetical_expression_constants,"
 block
   #test = (1 + 2) * 3", Value::from_i64(9));
 
 // ## Ranges
 
-test_math!(range_basic,"#test = 5 : 14", Value::from_i64(5));
+test_mech!(range_basic,"#test = 5 : 14", Value::from_i64(5));
 
 // ## Subscripts
 
-test_math!(subscript_scalar_math,"
+test_mech!(subscript_scalar_math,"
 block
   x = 3:6
   y = 10:12
   #test = x{1,1} + y{3,1}", Value::from_i64(15));
 
-test_math!(subscript_scan,"
+test_mech!(subscript_scan,"
 block
   x = 10:20
   z = 3:5
   #test = x{z, :}", Value::from_i64(12));
 
-test_math!(subscript_logical_greater,"
+test_mech!(subscript_logical_greater,"
   block
   x = 10:20
   z = x > 15
   #test = x{z, :}", Value::from_i64(16));
 
-test_math!(subscript_logical_less,"
+test_mech!(subscript_logical_less,"
   block
   x = 10:20
   z = x < 15
@@ -220,7 +220,7 @@ test_math!(subscript_logical_less,"
 
 // ## Set
 
-test_math!(set_column_logical,"
+test_mech!(set_column_logical,"
 block
   ix = x > 0
   x = #test.x
@@ -232,7 +232,7 @@ block
             4 5 6
             7 8 9]", Value::from_i64(3));
 
-test_math!(set_second_column_logical,"
+test_mech!(set_second_column_logical,"
 block
   #test = #ball.y
 
@@ -247,7 +247,7 @@ block
             4 5 6
             7 8 9]", Value::from_i64(3));
 
-test_math!(set_second_omit_row_subscript,"
+test_mech!(set_second_omit_row_subscript,"
 block
   #ball = [x: 15 y: 9 vx: 40 vy: 9]
   #system/timer = [resolution: 15 tick: 0]
@@ -260,7 +260,7 @@ block
 block
   #test = #ball.y", Value::from_i64(11));
 
-test_math!(set_rhs_math_filters_logic,"
+test_mech!(set_rhs_math_filters_logic,"
 block
   #ball = [|x y vx vy|
             1 2 3 4
@@ -278,7 +278,7 @@ block
 block
   #test = #ball{1,2} + #ball{3,2}", Value::from_i64(145584));
 
-test_math!(set_implicit_logic,"
+test_mech!(set_implicit_logic,"
 block
   #ball = [|x y vx vy|
             1 2 3 4
@@ -297,7 +297,7 @@ block
 
 // ## Concat
 
-test_math!(concat_horzcat_data,"
+test_mech!(concat_horzcat_data,"
 block
   x = 1:10
   y = 11:20
@@ -308,7 +308,7 @@ block
 
 // ## Append
 
-test_math!(append_row_inline,"
+test_mech!(append_row_inline,"
 block
   ix = #foo.x > 50
   #test = #foo{ix, :}
@@ -332,7 +332,7 @@ block
 
 // ## Logic
 
-test_math!(logic_and,"
+test_mech!(logic_and,"
 block
   ix1 = #foo.x > 5
   ix2 = #foo.x < 11
@@ -345,7 +345,7 @@ block
            8 9 10
            11 12 13]", Value::from_i64(8));
 
-test_math!(logic_or,"
+test_mech!(logic_or,"
 block
   ix1 = #foo.x < 7
   ix2 = #foo.x > 9
@@ -360,7 +360,7 @@ block
 
 // ## Change scan
 
-test_math!(change_scan_simple,"
+test_mech!(change_scan_simple,"
 block
   #ball = [|x y z| 
             1 2 3
@@ -380,7 +380,7 @@ block
 
 // ## Full programs
 
-test_math!(program_bouncing_balls,"# Bouncing Balls
+test_mech!(program_bouncing_balls,"# Bouncing Balls
 
 Define the environment
   #html/event/click = [|x y|]
@@ -424,19 +424,19 @@ block
 
 // ## Strings
 
-test_math!(string_basic,r#"
+test_mech!(string_basic,r#"
 block
   #test = "Hello World""#, Value::from_str("Hello World"));
 
-test_math!(string_table,r#"
+test_mech!(string_table,r#"
 block
   #test = ["Hello" "World"]"#, Value::from_str("Hello"));
 
-test_math!(string_named_attributes, r#"#test = [type: "h1" text: "An App"]"#, Value::from_str("h1"));
+test_mech!(string_named_attributes, r#"#test = [type: "h1" text: "An App"]"#, Value::from_str("h1"));
 
 // ## Nesting
 
-test_math!(nesting_basic,r#"
+test_mech!(nesting_basic,r#"
 block
   x = [#app{1,2}{1,1}]
   y = [#app{1,2}{2,1}]
