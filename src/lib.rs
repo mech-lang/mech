d1448e0e77815d060a581fe18e1c41e4ced2a577
Corey Montella <cmontella@live.com>
2019-03-23 11:52:42 -0400

    reexpor parser,

--- src/lib.rs
+++ src/lib.rs
@@ -9,5 +9,6 @@ extern crate mech_program;
 
 pub use mech_core::{Core, Hasher};
 pub use mech_syntax::compiler::Compiler;
+pub use mech_syntax::parser::Parser;
 pub use mech_program::{ProgramRunner, RunLoop, RunLoopMessage, ClientMessage};
 pub use mech_server::client::{ClientHandler};
