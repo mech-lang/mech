c0b03bbca51fe783d140f89891f200d2d812d995
Corey Montella <cmontella@live.com>
2019-03-23 21:00:56 -0400

    reexport quantity math

--- src/lib.rs
+++ src/lib.rs
@@ -7,7 +7,8 @@ extern crate mech_syntax;
 extern crate mech_server;
 extern crate mech_program;
 
-pub use mech_core::{Core, Hasher};
+pub use mech_core::{Core, Table, Value, Hasher};
+pub use mech_core::QuantityMath;
 pub use mech_syntax::compiler::Compiler;
 pub use mech_syntax::parser::Parser;
 pub use mech_program::{ProgramRunner, RunLoop, RunLoopMessage, ClientMessage};
