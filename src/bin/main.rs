2033c32dcf9a1f7ba87bfc672e5a339b846f0c58
Corey Montella <cmontella@live.com>
2019-04-27 17:57:23 -0400

    new main

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -32,11 +32,35 @@ fn compile_test(input: String, test: Value) {
 
 
 fn main() {
-  let input = String::from(r#"# Hello World
+  let input = String::from(r#"# Pong Game Client (Client)
 
-This is a paragraph
+Start a timer
+  #time/timer = [period: 5000 tick: 0 hours: 0 minutes: 0 seconds: 0]
 
-Great deal."#);
+Draw the game
+  #app/main = [root: "drawing" direction: _ contains: [#game]]
+
+controller slider
+  #paddle-control = [type: "slider" class: _ contains: _ parameters:  [min: 0 max: 300 value: 0]]
+
+game area
+  #game = [|type     class contains    parameters|
+            #paddle-control
+            "canvas" _     [#elements] [width: 400 height: 400]]
+
+define the controller
+  #pong/paddle = #paddle-control{1,4}{1,3}
+
+draw the game area
+  pos = #pong/paddle
+  start = pos
+  end = pos + 100
+  #elements = [|shape    parameters|
+                "circle" [cx: #pong/ball.x cy: #pong/ball.y radius: 10 fill: "000000"]
+                "line"   [x1: start y1: 350 x2: end y2: 350 stroke: "000000"]]
+
+Stub the ball
+  #pong/ball = [x: 0 y: 0]"#);
   
   //let value = Value::Number(make_quantity(780000,-4,0));
   //compile_test(input.clone(), value);
@@ -50,7 +74,7 @@ Great deal."#);
  
 
   core.register_blocks(compiler.blocks.clone());
-  println!("{:?}", compiler.parse_tree);
+  //println!("{:?}", compiler.parse_tree);
   println!("{:?}", compiler.unparsed);
   println!("{:?}", compiler.syntax_tree);
   //println!("{:?}", core.runtime);
