9869af370e3b5997ac92334bad787a72a90268f1
Corey Montella <cmontella@live.com>
2019-04-29 12:40:51 -0400

    change from systemtimer to time/timer

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -4,7 +4,7 @@ use mech_program::{ProgramRunner, RunLoop, RunLoopMessage};
 fn main() {
 
   let mut runner = ProgramRunner::new("p1", 1000);
-  runner.load_program("#system/timer = [resolution: 1000]".to_string());
+  runner.load_program("#time/timer = [period: 1000]".to_string());
   runner.run();
 
   let mut runner = ProgramRunner::new("p2", 1000);
