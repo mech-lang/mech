dd8a2991e98750d5201cc0f5d29c7c5bb4ae7c79
Corey Montella <cmontella@live.com>
2019-05-08 21:45:07 -0400

    update error type

--- src/errors.rs
+++ src/errors.rs
@@ -2,18 +2,23 @@
 
 // Defines a struct for errors and an enum which enumerates the error types
 
+// ## Prelude
+
+use table::{Index};
+use runtime::Constraint;
+
+// ## The Error Struct
+
 #[derive(Clone, Debug, PartialEq)]
 pub struct Error { 
   pub block: u64,
-  pub constraint: usize,
-  pub line: usize,
-  pub column: usize,
+  pub constraint: Constraint,
   pub error_id: ErrorType,
 }
 
 
 #[derive(Clone, Debug, PartialEq)]
 pub enum ErrorType {
-  MissingAttribute(u64),
+  MissingAttribute(Index),
   DuplicateAlias(u64),
 }
