64bf2800f4c97898bc3f2ae9246f0e1f1ac579d4
Corey Montella <cmontella@live.com>
2019-07-31 23:06:14 -0400

    fix tests

--- tests/quantities.rs
+++ tests/quantities.rs
@@ -6,14 +6,14 @@ use mech_core::{Quantity, ToQuantity, QuantityMath, make_quantity};
 fn quantities_base() {
     let x = make_quantity(1, 3, 1);
     let y = make_quantity(1, -3, 1);
-    let added = x.add(y);
+    let added = x.add(y).unwrap();
     assert_eq!(x.mantissa(), 1);
     assert_eq!(y.mantissa(), 1);
     assert_eq!(x.range(), 3);
     assert_eq!(y.range(), -3);
     assert_eq!(added.mantissa(), 1000001);
     assert_eq!(added.range(), -3);
-    let added_reverse = y.add(x);
+    let added_reverse = y.add(x).unwrap();
     assert_eq!(added_reverse.mantissa(), 1000001);
     assert_eq!(added_reverse.range(), -3);
 }
@@ -22,7 +22,7 @@ fn quantities_base() {
 fn quantities_base_sub() {
     let x = make_quantity(1, 3, 1);
     let y = make_quantity(1, -3, 1);
-    let sub = x.sub(y);
+    let sub = x.sub(y).unwrap();
     assert_eq!(sub.mantissa(), 999999);
     assert_eq!(sub.range(), -3);
 }
@@ -31,7 +31,7 @@ fn quantities_base_sub() {
 fn quantities_base_multiply() {
     let x = make_quantity(1, 3, 1);
     let y = make_quantity(1, -3, 1);
-    let sub = x.multiply(y);
+    let sub = x.multiply(y).unwrap();
     assert_eq!(sub.mantissa(), 1);
     assert_eq!(sub.range(), 0);
 }
@@ -40,7 +40,7 @@ fn quantities_base_multiply() {
 fn quantities_base_add_float() {
     let x = make_quantity(1, -1, 0);
     let y = make_quantity(2, -1, 0);
-    assert_eq!(x.add(y), make_quantity(3, -1, 0));
+    assert_eq!(x.add(y).unwrap(), make_quantity(3, -1, 0));
 }
 
 #[test]
@@ -48,7 +48,7 @@ fn quantities_base_add_01_02_03() {
     let x = make_quantity(1, -1, 0);
     let y = make_quantity(2, -1, 0);
     let z = make_quantity(3, -1, 0);
-    assert_eq!(x.add(y.add(z)), make_quantity(6, -1, 0));
+    assert_eq!(x.add(y.add(z).unwrap()).unwrap(), make_quantity(6, -1, 0));
 }
 
 #[test]
@@ -56,7 +56,7 @@ fn quantities_base_associativity() {
     let x = make_quantity(1, -1, 0);
     let y = make_quantity(2, -1, 0);
     let z = make_quantity(3, -1, 0);
-    assert_eq!(z.add(x.add(y)), make_quantity(6, -1, 0));
+    assert_eq!(z.add(x.add(y).unwrap()).unwrap(), make_quantity(6, -1, 0));
 }
 
 #[test]
@@ -64,14 +64,14 @@ fn quantities_base_add_subtract() {
     let x = make_quantity(1, -1, 0);
     let y = make_quantity(2, -1, 0);
     let z = make_quantity(3, -1, 0);
-    assert_eq!((z.add(x.add(y))).sub(z).sub(y), make_quantity(1, -1, 0));
+    assert_eq!((z.add(x.add(y).unwrap()).unwrap()).sub(z).unwrap().sub(y).unwrap(), make_quantity(1, -1, 0));
 }
 
 #[test]
 fn quantities_base_add_big_little() {
   let x = make_quantity(275251200000000,-12,0);
   let y = make_quantity(7864320000000,-12,0);
-    assert_eq!(x.add(y), make_quantity(28311552000000, -11, 0));
+    assert_eq!(x.add(y).unwrap(), make_quantity(28311552000000, -11, 0));
 }
 
 #[test]
@@ -80,8 +80,8 @@ fn quantities_base_multiply_small() {
   let x = make_quantity(14336512000000,-12,0);
   let y = make_quantity(8,-1,0);
   let z = make_quantity(1,0,0);
-  let q = w.sub(x);
-  let r = q.multiply(y);
+  let q = w.sub(x).unwrap();
+  let r = q.multiply(y).unwrap();
   assert_eq!(r, make_quantity(-114692096000000, -13, 0));
 }
 
@@ -99,20 +99,20 @@ fn quantities_base_float() {
 fn quantities_add_large_neg_small() {
     let x = make_quantity(30292178951320,-11,0);
     let y = make_quantity(30,0,0);
-    assert_eq!(x.sub(y), make_quantity(27292178951320,-11,0));
+    assert_eq!(x.sub(y).unwrap(), make_quantity(27292178951320,-11,0));
 }
 
 #[test]
 fn quantities_adding_zero() {
     let zero = make_quantity(0,0,0);
     let offset = make_quantity(49825176195110, -11, 0);
-    assert_eq!(offset.add(zero), offset);
-    assert_eq!(zero.add(offset), offset);
+    assert_eq!(offset.add(zero).unwrap(), offset);
+    assert_eq!(zero.add(offset).unwrap(), offset);
 }
 
 #[test]
 fn quantities_division_different_ranges() {
     let x = make_quantity(282743338860,-9,0);
     let y = make_quantity(180,0,0);
-    assert_eq!(x.divide(y), make_quantity(15707963270000,-13,0));
+    assert_eq!(x.divide(y).unwrap(), make_quantity(15707963270000,-13,0));
 }
