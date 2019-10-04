9b0e5ef7dabef7b215296a5c3615873535f50541
Corey Montella <cmontella@live.com>
2019-10-04 16:47:57 -0400

    test vert single index

--- tests/compiler.rs
+++ tests/compiler.rs
@@ -231,6 +231,11 @@ block
   x = [1 2 3]
   #test = x{2}", Value::from_i64(2));
 
+test_mech!(subscript_single_vert,"
+block
+  x = [1; 2; 3]
+  #test = x{2}", Value::from_i64(2));
+
 // ## Comparators
 
 test_mech!(comparator_greater,"
