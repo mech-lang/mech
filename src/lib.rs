d7a3dca4077b13f944f360bc6584d8778128693e
cmontella <cmontella@live.com>
2019-10-31 16:55:46 -0400

    move runloopmessage to utilities to keep it light

--- src/lib.rs
+++ src/lib.rs
@@ -3,7 +3,7 @@ extern crate serde_derive;
 extern crate serde;
 extern crate mech_core;
 
-use mech_core::Transaction;
+use mech_core::{Transaction, Interner};
 
 // ## Client Message
 
@@ -15,4 +15,31 @@ pub enum WebsocketClientMessage {
   Table(usize),
   RemoveBlock(usize),
   Transaction(Transaction),
+}
+
+// Run loop messages are sent to the run loop from the client
+
+#[derive(Debug, Clone)]
+pub enum RunLoopMessage {
+  Stop,
+  StepBack,
+  StepForward,
+  Pause,
+  Resume,
+  Clear,
+  PrintCore,
+  PrintRuntime,
+  Listening(Vec<u64>),
+  Table(u64),
+  Transaction(Transaction),
+  Code(String),
+}
+
+// ## Watchers
+
+pub trait Watcher {
+  fn get_name(& self) -> String;
+  fn get_columns(& self) -> usize;
+  fn set_name(&mut self, &str);
+  fn on_change(&mut self, store: &mut Interner, diff: Transaction);
 }
