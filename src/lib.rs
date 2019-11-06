30e9cd113bdfbdef4a111bc7eb72a5c9f72a68d7
cmontella <cmontella@live.com>
2019-11-06 16:32:01 -0500

    fix reexport

--- src/lib.rs
+++ src/lib.rs
@@ -6,6 +6,7 @@ extern crate mech_core;
 extern crate mech_syntax;
 extern crate mech_server;
 extern crate mech_program;
+extern crate mech_utilities;
 
 mod repl;
 
@@ -13,5 +14,6 @@ pub use mech_core::{Core, Table, Value, Hasher};
 pub use mech_core::QuantityMath;
 pub use mech_syntax::compiler::Compiler;
 pub use mech_syntax::parser::Parser;
-pub use mech_program::{ProgramRunner, RunLoop, RunLoopMessage, ClientMessage};
+pub use mech_program::{ProgramRunner, RunLoop, ClientMessage};
+pub use mech_utilities::RunLoopMessage;
 pub use mech_server::client::{ClientHandler};
