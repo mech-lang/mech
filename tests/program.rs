b1292e1c7aa73f18411d4edc886b0ea39ef9d63a
cmontella <cmontella@live.com>
2019-11-07 17:46:38 -0500

    test boxed result

--- tests/program.rs
+++ tests/program.rs
@@ -1,6 +1,8 @@
 extern crate mech_program;
+extern crate mech_utilities;
 extern crate libloading;
 use mech_program::{ProgramRunner, RunLoop, ClientMessage};
+use mech_utilities::Watcher;
 
 #[test]
 fn program_test() {
@@ -8,9 +10,10 @@ fn program_test() {
   let system_mechanism: Library = Library::new("../mechanisms/system/target/debug/mech_system.dll").expect("Can't load library");
   let mut runner = ProgramRunner::new("test", 1000);
   let native_rust = unsafe {
-    system_mechanism.get::<fn()>(b"foo\0").expect("Symbol not present")
+    system_mechanism.get::<fn(u32)->Box<Watcher>>(b"boxed_shared_trait\0").expect("Symbol not present")
   };
-  native_rust();
+  let result = native_rust(3);
+  println!("{:?}", result.get_name());
   let outgoing = runner.program.outgoing.clone();
   runner.load_program("
   #test = 1 + 1
