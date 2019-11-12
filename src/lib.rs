d3c893942efc9f3cc2151d8070967c5cd2b9fca9
cmontella <cmontella@live.com>
2019-11-12 14:56:04 -0500

    fix build

--- src/lib.rs
+++ src/lib.rs
@@ -7,7 +7,7 @@ use mech_utilities::Watcher;
 use mech_core::{Quantity, ToQuantity, QuantityMath, make_quantity};
 use libm::{sin, cos, fmod, round, floor};
 
-static pi: f64 = 3.141592653589793238462643383279502884197169399375105820974944592307816406286;
+static PI: f64 = 3.141592653589793238462643383279502884197169399375105820974944592307816406286;
 
 #[no_mangle]
 pub extern "C" fn math_sin(x: Value) -> Value {
@@ -16,7 +16,7 @@ pub extern "C" fn math_sin(x: Value) -> Value {
     90.0 => 1.0,
     180.0 => 0.0,
     270.0 => -1.0,
-    _ => sin(x.as_float().unwrap() * pi / 180.0),
+    _ => sin(x.as_float().unwrap() * PI / 180.0),
   };
   Value::from_quantity(result.to_quantity())
 }
@@ -28,23 +28,23 @@ pub extern "C" fn math_cos(x: Value) -> Value {
     90.0 => 0.0,
     180.0 => -1.0,
     270.0 => 0.0,
-    _ => cos(x.as_float().unwrap() * pi / 180.0),
+    _ => cos(x.as_float().unwrap() * PI / 180.0),
   };
   Value::from_quantity(result.to_quantity())
 }
 
 #[no_mangle]
 pub extern "C" fn math_round(x: Value) -> Value {
-  let result = match x {
-    Value::Number(n) => Value::from_quantity(round(x.to_float()).to_quantity()),
+  match x {
+    Value::Number(n) => Value::from_quantity(round(n.to_float()).to_quantity()),
     _ => Value::Empty
   }
 }
 
 #[no_mangle]
 pub extern "C" fn math_floor(x: Value) -> Value {
-  let result = match x {
-    Value::Number(n) => Value::from_quantity(floor(x.to_float()).to_quantity()),
+  match x {
+    Value::Number(n) => Value::from_quantity(floor(n.to_float()).to_quantity()),
     _ => Value::Empty
   }
 }
