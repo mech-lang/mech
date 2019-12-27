41fbb58330b645d719c1f04720fd8d441662ac33
Corey Montella <cmontella@live.com>
2019-12-26 23:57:49 -0500

    reimplement math/sin and math/cos

--- src/lib.rs
+++ src/lib.rs
@@ -2,39 +2,65 @@ extern crate mech_core;
 extern crate mech_utilities;
 extern crate libm;
 use mech_core::{Interner, Transaction};
-use mech_core::Value;
+use mech_core::{Value, Table};
 use mech_utilities::Watcher;
 use mech_core::{Quantity, ToQuantity, QuantityMath, make_quantity};
 use libm::{sin, cos, fmod, round, floor};
 
 static PI: f64 = 3.141592653589793238462643383279502884197169399375105820974944592307816406286;
 
+
 #[no_mangle]
-pub extern "C" fn math_sin(x: Value) -> Value {
-  let result = match fmod(x.as_float().unwrap(), 360.0) {
-    0.0 => 0.0,
-    90.0 => 1.0,
-    180.0 => 0.0,
-    270.0 => -1.0,
-    _ => sin(x.as_float().unwrap() * PI / 180.0),
-  };
-  Value::from_quantity(result.to_quantity())
+pub extern "C" fn math_sin(input: Vec<(String, Table)>) -> Table {
+  let (argument, table_ref) = &input[0];
+  let mut out = Table::new(0,table_ref.rows,table_ref.columns);
+  if argument == "radians" {
+
+  } else if argument == "degrees" {
+    for i in 0..table_ref.columns as usize {
+      for j in 0..table_ref.rows as usize {
+        let x = &table_ref.data[i][j];
+        let result = match fmod(x.as_float().unwrap(), 360.0) {
+          0.0 => 0.0,
+          90.0 => 1.0,
+          180.0 => 0.0,
+          270.0 => -1.0,
+          _ => sin(x.as_float().unwrap() * PI / 180.0),
+        };
+        out.data[i][j] = Value::from_quantity(result.to_quantity());
+      }
+    }
+  }
+  out 
 }
 
 #[no_mangle]
-pub extern "C" fn math_cos(x: Value) -> Value {
-  let result = match fmod(x.as_float().unwrap(), 360.0) {
-    0.0 => 1.0,
-    90.0 => 0.0,
-    180.0 => -1.0,
-    270.0 => 0.0,
-    _ => cos(x.as_float().unwrap() * PI / 180.0),
-  };
-  Value::from_quantity(result.to_quantity())
+pub extern "C" fn math_cos(input: Vec<(String, Table)>) -> Table {
+  let (argument, table_ref) = &input[0];
+  let mut out = Table::new(0,table_ref.rows,table_ref.columns);
+  if argument == "radians" {
+
+  } else if argument == "degrees" {
+    for i in 0..table_ref.columns as usize {
+      for j in 0..table_ref.rows as usize {
+        let x = &table_ref.data[i][j];
+        let result = match fmod(x.as_float().unwrap(), 360.0) {
+          0.0 => 1.0,
+          90.0 => 0.0,
+          180.0 => -1.0,
+          270.0 => 0.0,
+          _ => cos(x.as_float().unwrap() * PI / 180.0),
+        };
+        out.data[i][j] = Value::from_quantity(result.to_quantity());
+      }
+    }
+  }
+  out 
 }
 
+/*
 #[no_mangle]
-pub extern "C" fn math_round(x: Value) -> Value {
+pub extern "C" fn math_round(input: Vec<(String, Table)>) -> Table {
   match x {
     Value::Number(n) => Value::from_quantity(round(n.to_float()).to_quantity()),
     _ => Value::Empty
@@ -42,9 +68,9 @@ pub extern "C" fn math_round(x: Value) -> Value {
 }
 
 #[no_mangle]
-pub extern "C" fn math_floor(x: Value) -> Value {
+pub extern "C" fn math_floor(input: Vec<(String, Table)>) -> Table {
   match x {
     Value::Number(n) => Value::from_quantity(floor(n.to_float()).to_quantity()),
     _ => Value::Empty
   }
-}
+}*/
