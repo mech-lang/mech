951a869ed9f294959882e8186d96a43a07be547d
cmontella <cmontella@live.com>
2020-04-17 19:56:04 -0400

    better test for downloading dependencies

--- tests/program.rs
+++ tests/program.rs
@@ -1,7 +1,9 @@
 extern crate mech_program;
 extern crate mech_utilities;
+extern crate mech_core;
 use mech_program::{ProgramRunner, RunLoop, ClientMessage};
-use mech_utilities::Watcher;
+use mech_utilities::RunLoopMessage;
+use mech_core::{Hasher, Index, Value};
 
 #[test]
 fn program_test() {
@@ -13,6 +15,17 @@ fn program_test() {
 #[test]
 fn load_module_with_program() {
   let mut runner = ProgramRunner::new("test", 1000);
-  runner.load_program("#test = math/sin(degrees: 90)".to_string());
+  runner.load_program("#test = math/sin(degrees: 0)".to_string());
   let running = runner.run();
+  running.send(RunLoopMessage::Table(Hasher::hash_str("test")));
+  loop {
+    match running.receive() {
+      (Ok(ClientMessage::Table(table))) => {
+          assert_eq!(table.unwrap().index(&Index::Index(1),&Index::Index(1)),
+                     Some(&Value::from_u64(0)));
+          break;
+      },
+      message => assert_eq!(1,2),
+    }
+  }
 }
