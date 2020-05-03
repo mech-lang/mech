0248faf8ca848194567dcca5249aed92e6eee42e
cmontella <cmontella@live.com>
2020-05-02 23:42:06 -0400

    Add a table for sending over network

--- src/lib.rs
+++ src/lib.rs
@@ -2,17 +2,19 @@
 extern crate serde_derive;
 extern crate serde;
 extern crate mech_core;
+extern crate hashbrown;
 
-use mech_core::{Transaction, Interner, TableId, Core, Constraint};
+use hashbrown::HashMap;
+use mech_core::{Table, Value, Aliases, Transaction, Interner, TableId, Core, Constraint, Register};
 
 // ## Client Message
 
 #[derive(Serialize, Deserialize, Debug)]
-pub enum WebsocketClientMessage {
-  Listening(Vec<TableId>),
+pub enum WebsocketMessage {
+  Listening(Register),
   Control(u8),
   Code(MechCode),
-  Table(usize),
+  Table(NetworkTable),
   RemoveBlock(usize),
   Transaction(Transaction),
 }
@@ -29,8 +31,8 @@ pub enum RunLoopMessage {
   Clear,
   PrintCore(Option<u64>),
   PrintRuntime,
-  Listening(Vec<TableId>),
-  Table(u64),
+  Listening(Register),
+  Table(NetworkTable),
   Transaction(Transaction),
   Code((u64,MechCode)),
   EchoCode(String),
@@ -56,5 +58,62 @@ impl MiniBlock {
 #[derive(Debug, Clone, Serialize, Deserialize)]
 pub enum MechCode {
   String(String),
-  MiniBlock(String),
+  MiniBlock(Vec<MiniBlock>),
+}
+
+// TODO This is a kludge to get around having to write a serialize method for
+// hashmaps.... fix this!
+#[derive(Debug, Clone, PartialEq, Serialize, Deserialize)]
+pub struct NetworkTable {
+  pub id: u64,
+  pub rows: u64,
+  pub columns: u64,
+  pub column_aliases: Vec<(u64, u64)>,
+  pub column_index_to_alias: Vec<Option<u64>>, 
+  pub row_aliases: Vec<(u64, u64)>,
+  pub data: Vec<Vec<Value>>,
+}
+
+impl NetworkTable {
+  pub fn new(table: &Table) -> NetworkTable {
+    let mut column_aliases: Vec<(u64,u64)> = Vec::new(); 
+    for (k,v) in table.column_aliases.iter() {
+      column_aliases.push((k.clone(),v.clone()));
+    };
+    let mut row_aliases: Vec<(u64,u64)> = Vec::new(); 
+    for (k,v) in table.row_aliases.iter() {
+      row_aliases.push((k.clone(),v.clone()));
+    };
+    NetworkTable {
+      id: table.id.clone(),
+      rows: table.rows.clone(),
+      columns: table.columns.clone(),
+      column_aliases,
+      column_index_to_alias: table.column_index_to_alias.clone(),
+      row_aliases,
+      data: table.data.clone(),
+    }
+  }
+
+  pub fn to_table(&mut self) -> Table {
+    let mut column_aliases = Aliases::new(); 
+    for (k,v) in self.column_aliases.iter() {
+      column_aliases.insert(k.clone(),v.clone());
+    };
+    let mut row_aliases: HashMap<u64,u64> = HashMap::new(); 
+    for (k,v) in self.row_aliases.iter() {
+      row_aliases.insert(k.clone(),v.clone());
+    };
+    Table {
+      id: self.id.clone(),
+      rows: self.rows.clone(),
+      columns: self.columns.clone(),
+      column_aliases,
+      column_index_to_alias: self.column_index_to_alias.clone(),
+      row_aliases: row_aliases,
+      data: self.data.clone(),
+    }
+  }
+
+
 }
