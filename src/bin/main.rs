8051db425d3b006ba1b576133a8d297f15285b8b
cmontella <cmontella@live.com>
2020-05-04 11:53:28 -0400

    remove main code

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -4,7 +4,7 @@ extern crate mech_core;
 extern crate crossbeam_channel;
 extern crate mech_syntax;
 use mech_program::{ProgramRunner, RunLoop, ClientMessage};
-use mech_utilities::{Watcher, RunLoopMessage};
+use mech_utilities::{RunLoopMessage};
 use mech_core::{Core, Value, Hasher, Index, Table};
 use mech_syntax::compiler::Compiler;
 
@@ -12,43 +12,5 @@ use crossbeam_channel::Sender;
 use crossbeam_channel::Receiver;
 
 fn main() {
-  
-  let mut runner = ProgramRunner::new("test", 1000);
-  //let outgoing = runner.program.outgoing.clone();
-  /*runner.load_program("
-block
-  #data = [10 20
-           30 40]".to_string());
 
-  let mut compiler = Compiler::new();
-  let mut core = Core::new(100,100);
-  compiler.compile_string(r#"
-block
-  #ans = #data"#.to_string());
-  core.register_blocks(compiler.blocks);
-  runner.load_core(core);*/
-
-
-  let running = runner.run();
-  running.send(RunLoopMessage::Code((0,"#data = 1 + 1".to_string())));
-  //running.send(RunLoopMessage::Table(Hasher::hash_str("test")));
-  //running.send(RunLoopMessage::PrintRuntime);
-  loop {
-    loop {
-      println!("Receiving");
-      match running.receive() {
-        (Ok(ClientMessage::Table(table))) => {
-            println!("{:?}", table);
-        },
-        (Ok(ClientMessage::Done)) => {
-          println!("Got a done");
-          break;
-        },
-        (Ok(ClientMessage::String(message))) => {
-          println!("{}", message);
-        }
-        message => println!("{:?}", message),
-      }
-    }
-  }
 }
