979c989052f34e4ebde8defdc8a0f4744fbaeb90
cmontella <cmontella@live.com>
2020-05-07 18:05:19 -0400

    format wait and until

--- src/formatter.rs
+++ src/formatter.rs
@@ -348,6 +348,14 @@ impl Formatter {
           code = format!("|~ {}", table);
         };
       }
+      Node::Until{children} => {
+        let table = self.write_node(&children[0]);
+        if self.html {
+          code = format!("<span class=\"highlight-watch\">~|</span> {}", table);
+        } else {
+          code = format!("~| {}", table);
+        };
+      }
       Node::TableHeader{children} => {
         self.rows += 1;
         node_type = "parameter";
