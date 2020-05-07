efe58607f5a7238d5b1cf777d3974273a0e18138
cmontella <cmontella@live.com>
2020-05-07 18:05:12 -0400

    compile table define instead of copy table

--- src/compiler.rs
+++ src/compiler.rs
@@ -846,7 +846,7 @@ impl Compiler {
             },
             _ => 0,
           };
-          constraints.push(Constraint::CopyTable{from_table, to_table});
+          constraints.push(Constraint::DefineTable{from_table, to_table});
         } else {
           // TODO error if there are no children
         }
