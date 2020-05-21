689621accd07816eda5031da83f67f8e2c48aaf9
cmontella <cmontella@live.com>
2020-05-21 18:21:35 -0400

    closer...

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -2,19 +2,57 @@ extern crate mech_core;
 
 
 use mech_core::{Core, TableId, Index, Value, Change, Transaction, Transformation, Block};
-
+use std::hash::Hasher;
+extern crate ahash;
+use ahash::AHasher;
 use std::time::{Duration, SystemTime};
 use std::io;
 use std::io::prelude::*;
 
-fn main() {
+fn hash_string(input: &str) -> u64 {
+  let mut hasher = AHasher::new_with_keys(329458495230, 245372983457);
+  hasher.write(input.to_string().as_bytes());
+  hasher.finish()
+}
 
+fn main() {
 
   let balls = 4000;
 
   print!("Allocating memory...");
   let mut core = Core::new(balls * 4 * 4);
   println!("Done!");
+
+  let mut block = Block::new(6);
+  block.register_transformation(Transformation::NewTable{table_id: TableId::Local(0x01), rows: 1, columns: 1});
+  block.register_transformation(Transformation::Constant{table_id: TableId::Local(0x01), value: Value::from_u64(0)});
+  block.register_transformation(Transformation::NewTable{table_id: TableId::Local(0x02), rows: 1, columns: 1});
+  block.register_transformation(Transformation::Constant{table_id: TableId::Local(0x02), value: Value::from_u64(balls as u64)});
+  block.register_transformation(Transformation::NewTable{table_id: TableId::Local(0x78), rows: balls, columns: 1});
+  block.register_transformation(Transformation::NewTable{table_id: TableId::Local(0x79), rows: balls, columns: 1});
+  block.register_transformation(Transformation::NewTable{table_id: TableId::Global(0x123), rows: balls, columns: 4});
+  block.register_transformation(Transformation::NewTable{table_id: TableId::Global(0x456), rows: 1, columns: 1});
+  block.register_transformation(Transformation::Set{table_id: TableId::Global(0x456), row: Index::Index(1), column: Index::Index(1), value: Value::from_u64(9)});
+  block.register_transformation(Transformation::NewTable{table_id: TableId::Global(0x789), rows: 1, columns: 2});
+  block.register_transformation(Transformation::Set{table_id: TableId::Global(0x789), row: Index::Index(1), column: Index::Index(1), value: Value::from_u64(16)});
+  block.register_transformation(Transformation::Function{
+    name: 0x285a4efbfcdc2ef4, 
+    arguments: vec![
+      (TableId::Local(0x01), Index::All, Index::All), 
+      (TableId::Local(0x02), Index::All, Index::All),
+    ],
+    out: (TableId::Local(0x78), Index::All, Index::All)
+  });
+  block.register_transformation(Transformation::Function{
+    name: 0x285a4efbfcdc2ef4, 
+    arguments: vec![
+      (TableId::Local(0x01), Index::All, Index::All), 
+      (TableId::Local(0x02), Index::All, Index::All),
+    ],
+    out: (TableId::Local(0x79), Index::All, Index::All)
+  });
+  block.gen_id();
+  core.runtime.register_block(block);
 
 /*
   x = 1:4000
@@ -24,7 +62,7 @@ fn main() {
   #gravity = 1
   #time/timer = [period: 1, ticks: 0]
 */
-
+/*
   let mut txn = Transaction{
     changes: vec![
       Change::NewTable{table_id: 123, rows: balls, columns: 4},
@@ -55,7 +93,7 @@ fn main() {
     ]
   };
 
-  core.process_transaction(&txn);
+  core.process_transaction(&txn);*/
 
 /*
   ~ #time/timer.ticks
@@ -64,27 +102,33 @@ fn main() {
   #ball.vy := #ball.vy + #gravity
 */
 
-  let mut block = Block::new(1000);
+  let mut block = Block::new(8500);
   block.register_transformation(Transformation::Whenever{table_id: 789, row: Index::All, column: Index::Index(2)});
   block.register_transformation(Transformation::Function{
     name: 0x13166E07A8EF9CC3, 
-    lhs: (TableId::Global(123), Index::All, Index::Index(1)), 
-    rhs: (TableId::Global(123), Index::All, Index::Index(3)),
+    arguments: vec![
+      (TableId::Global(123), Index::All, Index::Index(1)), 
+      (TableId::Global(123), Index::All, Index::Index(3))
+    ],
     out: (TableId::Global(123), Index::All, Index::Index(1))
   });
   block.register_transformation(Transformation::Function{
     name: 0x13166E07A8EF9CC3, 
-    lhs: (TableId::Global(123), Index::All, Index::Index(2)), 
-    rhs: (TableId::Global(123), Index::All, Index::Index(4)),
+    arguments: vec![
+      (TableId::Global(123), Index::All, Index::Index(2)), 
+      (TableId::Global(123), Index::All, Index::Index(4)),
+    ],
     out: (TableId::Global(123), Index::All, Index::Index(2))
   });
   block.register_transformation(Transformation::Function{
     name: 0x13166E07A8EF9CC3, 
-    lhs: (TableId::Global(123), Index::All, Index::Index(4)), 
-    rhs: (TableId::Global(456), Index::All, Index::All),
+    arguments: vec![
+      (TableId::Global(123), Index::All, Index::Index(4)), 
+      (TableId::Global(456), Index::All, Index::All),
+    ],
     out: (TableId::Global(123), Index::All, Index::Index(4))
   });
-
+  block.gen_id();
 
   core.runtime.register_block(block);
  
@@ -98,7 +142,7 @@ fn main() {
 
   print!("Running computation...");
   io::stdout().flush().unwrap();
-  let rounds = 1000.0;
+  let rounds = 0.0;
   let start_ns = time::precise_time_ns(); 
   for j in 0..rounds as usize {
     let txn = Transaction{
