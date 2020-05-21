6af47dcd23af19dc4cca61374d2d0900411642ed
cmontella <cmontella@live.com>
2020-05-21 18:20:31 -0400

    add table/range function, handle new transforms

--- src/block.rs
+++ src/block.rs
@@ -59,12 +59,61 @@ impl Block {
 
   pub fn register_transformation(&mut self, tfm: Transformation) {
     match tfm {
+      Transformation::NewTable{table_id, rows, columns} => {
+        match table_id {
+          TableId::Global(id) => {
+            self.changes.push(
+              Change::NewTable{
+                table_id: id,
+                rows,
+                columns,
+              }
+            );
+            self.output.insert(id);
+          }
+          TableId::Local(id) => {
+            self.tables.insert(id, Rc::new(RefCell::new(Table::new(id, rows, columns, self.store.clone()))));
+          }
+        }
+      }
+      Transformation::Constant{table_id, ref value} => {
+        match table_id {
+          TableId::Local(id) => {
+            let mut table = self.tables.get(&id).unwrap().borrow_mut();
+            table.set(&Index::Index(1), &Index::Index(1), &value);
+          }
+          _ => (),
+        }
+      }
+      Transformation::Set{table_id, row, column, value} => {
+        match table_id {
+          TableId::Global(id) => {
+            self.changes.push(
+              Change::Set{
+                table_id: id,
+                values: vec![(row, column, value)],
+              }
+            );
+            self.output.insert(id);
+          }
+          _ => (),
+        }        
+      }
       Transformation::Whenever{table_id, row, column} => {
         self.input.insert(Register{table_id, row, column}.hash());
       }
       Transformation::Function{name, ref arguments, out} => {
         let (out_id, _, _) = out;
-        self.output.insert(*out_id.unwrap());
+        match out_id {
+          TableId::Global(id) => {self.output.insert(id);},
+          _ => (),
+        }
+        for (table_id, _, _) in arguments {
+          match table_id {
+            TableId::Global(id) => {self.input.insert(*id);},
+            _ => (),
+          }
+        }
         self.plan.push(tfm.clone());
       }
       _ => (),
@@ -74,84 +123,126 @@ impl Block {
 
   pub fn solve(&mut self, database: Rc<RefCell<Database>>) {
     let mut changes = Vec::with_capacity(4000);
+    changes.append(&mut self.changes);
+    self.changes.clear();
     'step_loop: for step in &self.plan {
       match step {
         Transformation::Function{name, arguments, out} => {
-          let (lhs_table_id, lhs_rows, lhs_columns) = &arguments[0];
-          let (rhs_table_id, rhs_rows, rhs_columns) = &arguments[1];
-          let (out_table_id, out_rows, out_columns) = out;
-          let db = database.borrow_mut();
-          let lhs_table = match lhs_table_id {
-            TableId::Global(id) => db.tables.get(id).unwrap().borrow(),
-            TableId::Local(id) => self.tables.get(id).unwrap().borrow(),
-          };
-          let rhs_table = match rhs_table_id {
-            TableId::Global(id) => db.tables.get(id).unwrap().borrow(),
-            TableId::Local(id) => self.tables.get(id).unwrap().borrow(),
-          };
-          let store = &db.store.borrow();
+          match name {
+            // math/add
+            0x13166E07A8EF9CC3 => {
+              // TODO test argument count is 2
+              let (lhs_table_id, lhs_rows, lhs_columns) = &arguments[0];
+              let (rhs_table_id, rhs_rows, rhs_columns) = &arguments[1];
+              let (out_table_id, out_rows, out_columns) = out;
+              let db = database.borrow_mut();
+              let lhs_table = match lhs_table_id {
+                TableId::Global(id) => db.tables.get(id).unwrap().borrow(),
+                TableId::Local(id) => self.tables.get(id).unwrap().borrow(),
+              };
+              let rhs_table = match rhs_table_id {
+                TableId::Global(id) => db.tables.get(id).unwrap().borrow(),
+                TableId::Local(id) => self.tables.get(id).unwrap().borrow(),
+              };
+              let store = &db.store.borrow();
 
-          // Figure out dimensions
-          let equal_dimensions = if lhs_table.rows == rhs_table.rows
-          { true } else { false };
-          let lhs_scalar = if lhs_table.rows == 1 && lhs_table.columns == 1 
-          { true } else { false };
-          let rhs_scalar = if rhs_table.rows == 1 && rhs_table.columns == 1
-          { true } else { false };
+              // Figure out dimensions
+              let equal_dimensions = if lhs_table.rows == rhs_table.rows
+              { true } else { false };
+              let lhs_scalar = if lhs_table.rows == 1 && lhs_table.columns == 1 
+              { true } else { false };
+              let rhs_scalar = if rhs_table.rows == 1 && rhs_table.columns == 1
+              { true } else { false };
 
-          let iterator_zip = if equal_dimensions {
-            IndexIteratorZip::new(
-              IndexIterator::Range(1..=lhs_table.rows),
-              IndexIterator::Constant(std::iter::repeat(*lhs_columns.unwrap())),
-              IndexIterator::Range(1..=rhs_table.rows),
-              IndexIterator::Constant(std::iter::repeat(*rhs_columns.unwrap())),
-            )
-          } else if rhs_scalar {
-            IndexIteratorZip::new(
-              IndexIterator::Range(1..=lhs_table.rows),
-              IndexIterator::Constant(std::iter::repeat(*lhs_columns.unwrap())),
-              IndexIterator::Constant(std::iter::repeat(1)),
-              IndexIterator::Constant(std::iter::repeat(1)),
-            )
-          } else {
-            IndexIteratorZip::new(
-              IndexIterator::Constant(std::iter::repeat(1)),
-              IndexIterator::Constant(std::iter::repeat(1)),
-              IndexIterator::Range(1..=rhs_table.rows),
-              IndexIterator::Constant(std::iter::repeat(*rhs_columns.unwrap())),
-            )
-          };
+              let iterator_zip = if equal_dimensions {
+                IndexIteratorZip::new(
+                  IndexIterator::Range(1..=lhs_table.rows),
+                  IndexIterator::Constant(std::iter::repeat(*lhs_columns.unwrap())),
+                  IndexIterator::Range(1..=rhs_table.rows),
+                  IndexIterator::Constant(std::iter::repeat(*rhs_columns.unwrap())),
+                )
+              } else if rhs_scalar {
+                IndexIteratorZip::new(
+                  IndexIterator::Range(1..=lhs_table.rows),
+                  IndexIterator::Constant(std::iter::repeat(*lhs_columns.unwrap())),
+                  IndexIterator::Constant(std::iter::repeat(1)),
+                  IndexIterator::Constant(std::iter::repeat(1)),
+                )
+              } else {
+                IndexIteratorZip::new(
+                  IndexIterator::Constant(std::iter::repeat(1)),
+                  IndexIterator::Constant(std::iter::repeat(1)),
+                  IndexIterator::Range(1..=rhs_table.rows),
+                  IndexIterator::Constant(std::iter::repeat(*rhs_columns.unwrap())),
+                )
+              };
 
-          let mut function_result = Value::from_u64(0);
-          let mut values = Vec::with_capacity(lhs_table.rows);
-          for (lrix, lcix, rrix, rcix) in iterator_zip {
-            match (lhs_table.get(&Index::Index(lrix), &Index::Index(lcix)), 
-                   rhs_table.get(&Index::Index(rrix), &Index::Index(rcix))
-                  ) 
-            {
-              (Some(lhs_ix), Some(rhs_ix)) => {
-                let lhs_value = &store.data[lhs_ix];
-                let rhs_value = &store.data[rhs_ix];
-                match (lhs_value, rhs_value) {
-                  (Value::Number(x), Value::Number(y)) => {
-                    match x.add(*y) {
-                      Ok(result) => {
-                        function_result = Value::from_quantity(result);
+              let mut function_result = Value::from_u64(0);
+              let mut values = Vec::with_capacity(lhs_table.rows);
+              for (lrix, lcix, rrix, rcix) in iterator_zip {
+                match (lhs_table.get_address(&Index::Index(lrix), &Index::Index(lcix)), 
+                      rhs_table.get_address(&Index::Index(rrix), &Index::Index(rcix))
+                      ) 
+                {
+                  (Some(lhs_ix), Some(rhs_ix)) => {
+                    let lhs_value = &store.data[lhs_ix];
+                    let rhs_value = &store.data[rhs_ix];
+                    match (lhs_value, rhs_value) {
+                      (Value::Number(x), Value::Number(y)) => {
+                        match x.add(*y) {
+                          Ok(result) => {
+                            function_result = Value::from_quantity(result);
+                          }
+                          Err(_) => (), // TODO Handle error here
+                        }
                       }
-                      Err(_) => (), // TODO Handle error here
+                      _ => (),
                     }
                   }
                   _ => (),
                 }
+                values.push((Index::Index(lrix), *out_columns, function_result.clone()));
               }
-              _ => (),
+              changes.push(Change::Set{
+                table_id: *out_table_id.unwrap(),
+                values,
+              });
+
             }
-            values.push((Index::Index(lrix), *out_columns, function_result.clone()));
+            // table/range
+            0x285A4EFBFCDC2EF4 => {
+              // TODO test argument count is 2 or 3
+              // 2 -> start, end
+              // 3 -> start, increment, end
+              let (start_table_id, start_rows, start_columns) = &arguments[0];
+              let (end_table_id, end_rows, end_columns) = &arguments[1];
+              let (out_table_id, out_rows, out_columns) = out;
+              let db = database.borrow_mut();
+              let start_table = match start_table_id {
+                TableId::Global(id) => db.tables.get(id).unwrap().borrow(),
+                TableId::Local(id) => self.tables.get(id).unwrap().borrow(),
+              };
+              let end_table = match end_table_id {
+                TableId::Global(id) => db.tables.get(id).unwrap().borrow(),
+                TableId::Local(id) => self.tables.get(id).unwrap().borrow(),
+              };
+              let start_value = start_table.get(&Index::Index(1),&Index::Index(1)).unwrap();
+              let end_value = end_table.get(&Index::Index(1),&Index::Index(1)).unwrap();
+              let range = end_value.as_u64().unwrap() - start_value.as_u64().unwrap();
+              match out_table_id {
+                TableId::Local(id) => {
+                  let mut out_table = self.tables.get(id).unwrap().borrow_mut();
+                  for i in 1..=range as usize {
+                    out_table.set(&Index::Index(i), &Index::Index(1), &Value::from_u64(i as u64));
+                  }
+                }
+                TableId::Global(id) => {
+
+                }
+              }
+            }
+            _ => () // TODO Unknown function
           }
-          changes.push(Change::Set{
-            table_id: *out_table_id.unwrap(),
-            values,
-          });
         }
         _ => (),
       }
@@ -184,27 +275,31 @@ impl Block {
 impl fmt::Debug for Block {
   #[inline]
   fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-    write!(f, "id: {:x}\n", self.id)?;
+    write!(f, "id: {}\n", humanize(&self.id))?;
     write!(f, "state: {:?}\n", self.state)?;
     write!(f, "ready: \n")?;
     for input in self.ready.iter() {
-      write!(f, "       {}\n", humanize(input))?;
+      write!(f, "   {}\n", humanize(input))?;
     }
     write!(f, "input: \n")?;
     for input in self.input.iter() {
-      write!(f, "       {}\n", humanize(input))?;
+      write!(f, "   {}\n", humanize(input))?;
     }
     write!(f, "output: \n")?;
     for output in self.output.iter() {
-      write!(f, "       {}\n", humanize(output))?;
+      write!(f, "   {}\n", humanize(output))?;
     }
     write!(f, "transformations: \n")?;
     for tfm in self.transformations.iter() {
-      write!(f, "       {:?}\n", tfm)?;
+      write!(f, "   {:?}\n", tfm)?;
     }
     write!(f, "plan: \n")?;
     for tfm in self.plan.iter() {
-      write!(f, "       {:?}\n", tfm)?;
+      write!(f, "   {:?}\n", tfm)?;
+    }
+    write!(f, "tables: \n")?;
+    for (_, table) in self.tables.iter() {
+      write!(f, "{:?}\n", table.borrow())?;
     }
     
     Ok(())
@@ -228,7 +323,9 @@ pub enum Error {
 #[derive(Debug, Clone, PartialEq, Eq, Serialize, Deserialize)]
 pub enum Transformation {
   NewTable{table_id: TableId, rows: usize, columns: usize },
+  Constant{table_id: TableId, value: Value},
   ColumnAlias{table_id: TableId, column_ix: usize, column_alias: u64},
+  Set{table_id: TableId, row: Index, column: Index, value: Value},
   RowAlias{table_id: TableId, row_ix: usize, row_alias: u64},
   Whenever{table_id: u64, row: Index, column: Index},
   Function{name: u64, arguments: Vec<(TableId, Index, Index)>, out: (TableId, Index, Index)},
