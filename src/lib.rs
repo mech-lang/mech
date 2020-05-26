68dcf34d1e0f176e8d888f3543956c38bde2663f
cmontella <cmontella@live.com>
2020-05-26 17:16:31 -0400

    export make_quantity

--- src/lib.rs
+++ src/lib.rs
@@ -21,7 +21,7 @@ pub use self::database::{Database, Store, Transaction, Change};
 pub use self::block::{Block, Transformation};
 pub use self::table::{Table, TableId, Index, Value};
 pub use self::core::Core;
-pub use self::quantities::{Quantity, QuantityMath, ToQuantity};
+pub use self::quantities::{Quantity, QuantityMath, ToQuantity, make_quantity};
 
 /*
 // # Mech
