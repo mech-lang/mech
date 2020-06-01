5f94e50396db0cd5314d1f2aca013ebd471d5ff0
Corey Montella <cmontella@live.com>
2020-06-01 16:52:10 -0400

    Add value to util

--- src/lib.rs
+++ src/lib.rs
@@ -42,3 +42,157 @@ pub enum RunLoopMessage {
 pub trait Watcher {
   fn get_name(& self) -> String;
 }
+
+// ## Value
+
+#[derive(Clone, Copy, PartialEq, Eq, Serialize, Deserialize)]
+pub enum Value {
+  Number(Quantity),
+  //Bool(bool),
+  //Reference(TableId),
+  //Empty,
+}
+
+impl Value {
+
+  pub fn from_string(string: String) -> Value {
+    Value::Number(0)
+  }
+
+  pub fn from_str(string: &str) -> Value {
+    Value::Number(0)
+  }
+
+  pub fn from_bool(boolean: bool) -> Value {
+    Value::Number(0)
+  }
+
+  pub fn from_u64(num: u64) -> Value {
+    Value::Number(num.to_quantity())
+  }
+
+  pub fn from_quantity(num: Quantity) -> Value {
+    Value::Number(num)
+  }
+
+  pub fn from_i64(num: i64) -> Value {
+    Value::Number(num.to_quantity())
+  }
+
+  pub fn from_f64(num: f64) -> Value {
+    Value::Number(num.to_quantity())
+  }
+
+  pub fn as_quantity(&self) -> Option<Quantity> {
+    match self {
+      Value::Number(n) => Some(*n),
+      //Value::Empty => Some(0.to_quantity()),
+      _ => None,
+    }
+  }
+
+  pub fn as_u64(&self) -> Option<u64> {
+    match self {
+      Value::Number(n) => Some(n.to_float() as u64),
+      //Value::Reference(TableId::Local(n)) => Some(*n),
+      //Value::Reference(TableId::Global(n)) => Some(*n),
+      _ => None,
+    }
+  }
+
+  pub fn as_float(&self) -> Option<f64> {
+    match self {
+      Value::Number(n) => Some(n.to_float()),
+      _ => None,
+    }
+  }
+
+  pub fn as_i64(&self) -> Option<i64> {
+    match self {
+      Value::Number(n) => Some(n.mantissa()),
+      _ => None,
+    }
+  }
+
+  pub fn as_string(&self) -> Option<String> {
+    match self {
+      //Value::String(n) => Some(n.clone()),
+      Value::Number(q) => Some(q.format()),
+      //Value::Reference(TableId::Global(r)) |
+      //Value::Reference(TableId::Local(r)) => {
+      //  Some(format!("{:?}", r))
+      //},
+      //Value::Empty => Some(String::from("")),
+      //Value::Bool(t) => match t {
+      //  true => Some(String::from("true")),
+      //  false => Some(String::from("false")),
+      //},
+      _ => None,
+    }
+  }
+
+  /*pub fn equal(&self, other: &Value) -> Option<bool> {
+    match (self, other) {
+      (Value::String(ref x), Value::String(ref y)) => {
+        Some(x.to_owned() == y.to_owned())
+      }
+      _ => None,
+    }
+  }*/
+
+  /*pub fn not_equal(&self, other: &Value) -> Option<bool> {
+    match (self, other) {
+      (Value::String(ref x), Value::String(ref y)) => {
+        Some(x.to_owned() != y.to_owned())
+      }
+      _ => None,
+    }
+  }*/
+
+  pub fn less_than(&self, other: &Value) -> Option<bool> {
+    None
+  }
+
+  pub fn less_than_equal(&self, other: &Value) -> Option<bool> {
+    None
+  }
+
+  pub fn greater_than(&self, other: &Value) -> Option<bool> {
+    None
+  }
+
+  pub fn greater_than_equal(&self, other: &Value) -> Option<bool> {
+    None
+  }
+
+  pub fn add(&self, other: &Value) -> Option<bool> {
+    None
+  }
+
+  pub fn sub(&self, other: &Value) -> Option<bool> {
+    None
+  }
+
+  pub fn multiply(&self, other: &Value) -> Option<bool> {
+    None
+  }
+
+  pub fn divide(&self, other: &Value) -> Option<bool> {
+    None
+  }
+
+}
+
+impl fmt::Debug for Value {
+  #[inline]
+  fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+    match self {
+      &Value::Number(x) => write!(f, "{}", x.to_string()),
+      //&Value::String(ref x) => write!(f, "{}", x),
+      //&Value::Empty => write!(f, ""),
+      //&Value::Bool(ref b) => write!(f, "{}", b),
+      //&Value::Reference(ref b) => write!(f, "{:?}", b),
+    }
+  }
+}
+
