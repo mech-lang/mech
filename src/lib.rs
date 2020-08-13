8fe793cb38826fff00e1b07d5adc6b7614742c44
cmontella <cmontella@live.com>
2020-08-13 17:23:55 -0400

    fix math/round and math/floor

--- src/lib.rs
+++ src/lib.rs
@@ -89,34 +89,62 @@ pub extern "C" fn math_cos(arguments: &Vec<(u64, ValueIterator)>, out: &mut Valu
 
 #[no_mangle]
 pub extern "C" fn math_round(arguments: &Vec<(u64, ValueIterator)>, out: &mut ValueIterator) { 
-  /*let (argument, table_ref) = &input[0];
-  let mut out = Table::new(0,table_ref.rows,table_ref.columns);
-  for i in 0..table_ref.columns as usize {
-    for j in 0..table_ref.rows as usize {
-      let x = &table_ref.data[i][j];
-      let result = match x {
-        Value::Number(n) => Value::from_quantity(round(n.to_float()).to_quantity()),
-        _ => Value::Empty
-      };
-      out.data[i][j] = result;
+  let (in_arg_name, vi) = &arguments[0];
+  let mut rows = vi.rows();
+  let mut cols = vi.columns();
+  match in_arg_name {
+    &ANGLE => {
+      unsafe {
+        (*out.table).rows = rows;
+        (*out.table).columns = cols;
+        (*out.table).data.resize(rows*cols, 0);
+      }
+      let mut flag: bool = false;
+      for (i,k) in (1..=rows).zip(vi.row_iter.clone()) {
+        for (j,m) in (1..=cols).zip(vi.column_iter.clone()) {
+          let value = unsafe{(*vi.table).get(&k,&m).unwrap()};
+          match value.as_float() {
+            Some(x) => {
+              unsafe {
+                (*out.table).set_unchecked(i, j, Value::from_f64(round(x)));
+              }
+            },
+            _ => (), // TODO Alert user that there was an error
+          }
+        }
+      }  
     }
+    _ => (), // Unknown argument name
   }
-  out*/
 }
 
 #[no_mangle]
 pub extern "C" fn math_floor(arguments: &Vec<(u64, ValueIterator)>, out: &mut ValueIterator) { 
-  /*let (argument, table_ref) = &input[0];
-  let mut out = Table::new(0,table_ref.rows,table_ref.columns);
-  for i in 0..table_ref.columns as usize {
-    for j in 0..table_ref.rows as usize {
-      let x = &table_ref.data[i][j];
-      let result = match x {
-        Value::Number(n) => Value::from_quantity(floor(n.to_float()).to_quantity()),
-        _ => Value::Empty
-      };
-      out.data[i][j] = result;
+  let (in_arg_name, vi) = &arguments[0];
+  let mut rows = vi.rows();
+  let mut cols = vi.columns();
+  match in_arg_name {
+    &ANGLE => {
+      unsafe {
+        (*out.table).rows = rows;
+        (*out.table).columns = cols;
+        (*out.table).data.resize(rows*cols, 0);
+      }
+      let mut flag: bool = false;
+      for (i,k) in (1..=rows).zip(vi.row_iter.clone()) {
+        for (j,m) in (1..=cols).zip(vi.column_iter.clone()) {
+          let value = unsafe{(*vi.table).get(&k,&m).unwrap()};
+          match value.as_float() {
+            Some(x) => {
+              unsafe {
+                (*out.table).set_unchecked(i, j, Value::from_f64(floor(x)));
+              }
+            },
+            _ => (), // TODO Alert user that there was an error
+          }
+        }
+      }  
     }
+    _ => (), // Unknown argument name
   }
-  out*/
 }
