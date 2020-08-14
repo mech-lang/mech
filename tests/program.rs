be0b25196a562e26ca99a0c7ad778c1ea4a28959
cmontella <cmontella@live.com>
2020-08-13 22:39:27 -0400

    fix tests

--- tests/program.rs
+++ tests/program.rs
@@ -3,7 +3,7 @@ extern crate mech_utilities;
 extern crate mech_core;
 use mech_program::{ProgramRunner, RunLoop, ClientMessage};
 use mech_utilities::{RunLoopMessage, MechCode};
-use mech_core::{Hasher, Index, Value};
+use mech_core::{hash_string, Index, Value, ValueMethods};
 
 #[test]
 fn program_test() {
@@ -19,12 +19,12 @@ fn load_module_with_program() {
   let mut runner = ProgramRunner::new("test", 1000);
   let running = runner.run();
   running.send(RunLoopMessage::Code((0,MechCode::String("#test = math/sin(degrees: 0)".to_string()))));
-  running.send(RunLoopMessage::GetTable(Hasher::hash_str("test")));
+  running.send(RunLoopMessage::GetTable(hash_string("test")));
   loop {
     match running.receive() {
       (Ok(ClientMessage::Table(table))) => {
-          assert_eq!(table.unwrap().index(&Index::Index(1),&Index::Index(1)),
-                     Some(&Value::from_u64(0)));
+          assert_eq!(table.unwrap().get(&Index::Index(1),&Index::Index(1)),
+                     Some(Value::from_u64(0)));
           break;
       },
       message => (),
