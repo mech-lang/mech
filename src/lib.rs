963d78f9858f2c0329f804facca30eb169212c6d
cmontella <cmontella@live.com>
2021-05-10 23:56:46 -0400

    make math f32

--- src/lib.rs
+++ src/lib.rs
@@ -5,10 +5,10 @@ extern crate libm;
 extern crate lazy_static;
 use mech_core::{Transaction};
 use mech_core::{Value, ValueMethods, IndexIterator, Table, ValueIterator};
-use mech_core::{Quantity, ToQuantity, QuantityMath, make_quantity, hash_string};
-use libm::{sin, cos, fmod, round, floor};
+use mech_core::{Quantity, ToQuantity, QuantityMath, hash_string};
+use libm::{sinf, cosf, fmodf, roundf, floorf};
 
-static PI: f64 = 3.141592653589793238462643383279502884197169399375105820974944592307816406286;
+static PI: f32 = 3.141592653589793238462643383279502884197169399375105820974944592307816406286;
 
 lazy_static! {
   static ref ANGLE: u64 = hash_string("angle");
@@ -16,7 +16,7 @@ lazy_static! {
 }
 
 #[no_mangle]
-pub extern "C" fn math_sin(arguments: &Vec<(u64, ValueIterator)>) { 
+pub extern "C" fn math_sin(arguments: &Vec<(u64, ValueIterator)>) {
   let (in_arg_name, vi) = &arguments[0];
   let (_, mut out) = arguments.last().unwrap().clone();
   if *in_arg_name == *ANGLE {
@@ -25,14 +25,14 @@ pub extern "C" fn math_sin(arguments: &Vec<(u64, ValueIterator)>) {
     for ((value, changed), out_ix) in vi.clone().zip(out.linear_index_iterator()) {
       match value.as_quantity() {
         Some(x) => {
-          let result = match fmod(x.as_f64().unwrap(), 360.0) {
+          let result = match fmodf(x.as_f32().unwrap(), 360.0) {
             0.0 => 0.0,
             90.0 => 1.0,
             180.0 => 0.0,
             270.0 => -1.0,
-            _ => sin(x.as_f64().unwrap() * PI / 180.0),
+            _ => sinf(x.as_f32().unwrap() * PI / 180.0),
           };
-          out.set_unchecked_linear(out_ix, Value::from_f64(result));
+          out.set_unchecked_linear(out_ix, Value::from_f32(result));
         },
         _ => (), // TODO Alert user that there was an error
       }
@@ -52,14 +52,14 @@ pub extern "C" fn math_cos(arguments: &Vec<(u64, ValueIterator)>) {
     for ((value, changed), out_ix) in vi.clone().zip(out.linear_index_iterator()) {
       match value.as_quantity() {
         Some(x) => {
-          let result = match fmod(x.as_f64().unwrap(), 360.0) {
+          let result = match fmodf(x.as_f32().unwrap(), 360.0) {
             0.0 => 1.0,
             90.0 => 0.0,
             180.0 => -1.0,
             270.0 => 0.0,
-            _ => cos(x.as_f64().unwrap() * PI / 180.0),
+            _ => cosf(x.as_f32().unwrap() * PI / 180.0),
           };
-          out.set_unchecked_linear(out_ix, Value::from_f64(result));
+          out.set_unchecked_linear(out_ix, Value::from_f32(result));
         },
         _ => (), // TODO Alert user that there was an error
       }
@@ -78,9 +78,9 @@ pub extern "C" fn math_round(arguments: &Vec<(u64, ValueIterator)>, out: &mut Va
     out.resize(vi.rows(),vi.columns());
     let mut flag: bool = false;
     for ((value, changed), out_ix) in vi.clone().zip(out.linear_index_iterator()) {
-      match value.as_f64() {
+      match value.as_f32() {
         Some(x) => {
-          out.set_unchecked_linear(out_ix, Value::from_f64(round(x)));
+          out.set_unchecked_linear(out_ix, Value::from_f32(roundf(x)));
         },
         _ => (), // TODO Alert user that there was an error
       }
@@ -98,9 +98,9 @@ pub extern "C" fn math_floor(arguments: &Vec<(u64, ValueIterator)>, out: &mut Va
     out.resize(vi.rows(),vi.columns());
     let mut flag: bool = false;
     for ((value, changed), out_ix) in vi.clone().zip(out.linear_index_iterator()) {
-      match value.as_f64() {
+      match value.as_f32() {
         Some(x) => {
-          out.set_unchecked_linear(out_ix, Value::from_f64(floor(x)));
+          out.set_unchecked_linear(out_ix, Value::from_f32(floorf(x)));
         },
         _ => (), // TODO Alert user that there was an error
       }
