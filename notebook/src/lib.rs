cddb66f73b32b4be5449112a711e8f81bc184752
Corey Montella <cmontella@live.com>
2021-06-06 17:09:46 -0400

    remove dead code

--- notebook/src/lib.rs
+++ notebook/src/lib.rs
@@ -1,47 +1,15 @@
-use wasm_bindgen::prelude::*;
-
 #[macro_use]
 extern crate mech_wasm;
 use mech_wasm::WasmCore;
 
-use web_sys::XmlHttpRequest;
+use wasm_bindgen::prelude::*;
 
 #[wasm_bindgen(start)]
 pub fn main() -> Result<(), JsValue> {
-  /*let mut mech_core = Core::new(100,100);
-
-  log!("Here we go!");
-
-  let window = web_sys::window().unwrap();
-  let document = window.document().unwrap();
-  let body = document.body().unwrap();
-
-  let val = document.create_element("div")?;
-  val.set_attribute("id","mech-root");
-  body.append_child(&val)?;
-
-  let xhr = XmlHttpRequest::new()?;
-  xhr.open_with_async("GET", "./website/index.mec", false);
-  xhr.send();
-  let program = xhr.response_text();
-
-  match program {
-    Ok(Some(program)) => {
-      mech_core.compile_code(program);
-      mech_core.add_application();
-    }, 
-    _ => (),
-  }*/
-
-  
-
-  
-
   Ok(())
 }
 
 #[wasm_bindgen]
 pub fn new_core() -> WasmCore {
-  log!("Creating a new core.");
   WasmCore::new(1000, 1000)
 }
