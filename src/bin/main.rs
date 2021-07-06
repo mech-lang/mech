c52539016b444c6a9cdf0d8381c0c3e75e448100
cmontella <cmontella@live.com>
2021-07-06 19:08:31 -0400

    benchmarks

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -9,9 +9,11 @@ use std::cell::RefCell;
 extern crate seahash;
 extern crate time;
 extern crate nalgebra;
+extern crate rayon;
 
 use nalgebra::base::Matrix2;
 use nalgebra::base::DMatrix;
+use rayon::prelude::*;
 
 fn main() {
 
@@ -29,37 +31,43 @@ fn main() {
   // stack allocated tables
   // matrix library in std
 
-  const n: usize = 10_000_000;
-  let dm = DMatrix::from_element(n,1,1.0);
-  let dx = DMatrix::from_element(n,1,2.0); 
-  
-  //let mut v: Vec<u32> = vec![0;n];
-  //let mut v: [u64;n] = [0;n];
-  let start_ns = time::precise_time_ns();
-  let c = dm + dx;
-  let end_ns = time::precise_time_ns();
-  let time = (end_ns - start_ns) as f64;
-  println!("{:0.9?} ms", time / 1_000_000.0);
+  const n: usize = 1_000_000_000;
 
-/*
-    let n = 10)=000;
+  {
+    let dm = DMatrix::from_element(n,1,0.0);
+    let start_ns = time::precise_time_ns();
+    let c = dm.add_scalar(1.0);
+    let end_ns = time::precise_time_ns();
+    let time = (end_ns - start_ns) as f64;
+    println!("MATRIX {:0.9?} ms", time / 1_000_000.0);
+  }
+  {
     let mut v: Vec<u64> = vec![0;n];
-    //println!("{:?}", v);
     let start_ns = time::precise_time_ns();
     let x: Vec<u64> = v.par_iter().map(|x| x + 1).collect();
     let end_ns = time::precise_time_ns();
     let time = (end_ns - start_ns) as f64;
     println!("PARI {:0.9?} ms", time / 1_000_000.0);
-    //println!("{:?}",x);
-  
+  }
+  {
     let mut v: Vec<u64> = vec![0;n];
     let start_ns = time::precise_time_ns();
     let x: Vec<u64> = v.iter().map(|x| x + 1).collect();
     let end_ns = time::precise_time_ns();
     let time = (end_ns - start_ns) as f64;
-    println!("ITER {:0.9?} ms", time / 1_000_000.0);*/
-
-
+    println!("ITER {:0.9?} ms", time / 1_000_000.0);
+  }
+  {
+    let mut v: Vec<u64> = vec![0;n];
+    let mut x: Vec<u64> = vec![0;n];
+    let start_ns = time::precise_time_ns();
+    for ix in 0..n {
+      x[ix] = v[ix] + 1;
+    }
+    let end_ns = time::precise_time_ns();
+    let time = (end_ns - start_ns) as f64;
+    println!("LOOP {:0.9?} ms", time / 1_000_000.0);
+  }
 
   
 }
