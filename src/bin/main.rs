607e232f744e957fc7272c76ed444e2e32a85c6c
cmontella <cmontella@live.com>
2021-07-11 21:32:06 -0400

    more tests

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -1,32 +1,5 @@
-#![feature(get_mut_unchecked)]
-
-extern crate mech_core;
-
-//use mech_core::{Register, Table, TableId, Database, TableIterator, IndexRepeater, TableIndex, ValueIterator, Value, ValueMethods, Store, IndexIterator, ConstantIterator};
 use std::sync::Arc;
 use std::cell::RefCell;
-
-extern crate seahash;
-extern crate time;
-extern crate nalgebra;
-extern crate rayon;
-
-use nalgebra::base::Matrix2;
-use nalgebra::base::DMatrix;
-use rayon::prelude::*;
-
-use std::thread;
-use std::time::Duration;
-
-fn long_running_computation(x: &u64) -> u64 {
-  let ten_millis = Duration::from_millis(10);
-  let now = time::Instant::now();
-  thread::sleep(ten_millis);
-  x + 1
-}
-
-#[tokio::main]
-pub async fn main() -> Result<(),String> {
 
   // New runtime
   // requirements:
@@ -42,32 +15,50 @@ pub async fn main() -> Result<(),String> {
   // stack allocated tables
   // matrix library in std
 
-  const n: usize = 1000;
+use nalgebra::base::Matrix2;
+use nalgebra::base::DMatrix;
+use rayon::prelude::*;
 
-  /*
+use std::thread;
+use std::time::Duration;
+
+fn long_running_computation(x: &u64) -> u64 {
+  //let ten_millis = Duration::from_micros(1);
+  //thread::sleep(ten_millis);
+  x + 2
+}
+
+#[tokio::main]
+pub async fn main() -> Result<(),String> {
+
+  const n: usize = 1000_000;
   {
-    let dm = DMatrix::from_element(n,1,0.0);
+    let mut v: Vec<u64> = vec![0;n];
     let start_ns = time::precise_time_ns();
-    let c = dm.add_scalar(1.0);
+    v.iter_mut().for_each(|x| {
+      *x = *x + 2;
+    });
     let end_ns = time::precise_time_ns();
     let time = (end_ns - start_ns) as f64;
-    println!("MATRIX {:0.9?} ms", time / 1_000_000.0);
-  }*/
+    println!("BASE {:0.4?} ms", time / 1_000_000.0);
+  }
   {
     let mut v: Vec<u64> = vec![0;n];
     let start_ns = time::precise_time_ns();
     let x: Vec<u64> = v.iter().map(|x| long_running_computation(x)).collect();
     let end_ns = time::precise_time_ns();
     let time = (end_ns - start_ns) as f64;
-    println!("ITER {:0.9?} ms", time / 1_000_000.0);
+    println!("ITER {:0.4?} ms", time / 1_000_000.0);
   }
   {
     let mut v: Vec<u64> = vec![0;n];
     let start_ns = time::precise_time_ns();
-    let x: Vec<u64> = v.par_iter().map(|x| long_running_computation(x)).collect();
+    v.par_iter_mut().for_each(|x| {
+      *x = long_running_computation(x);
+    });
     let end_ns = time::precise_time_ns();
     let time = (end_ns - start_ns) as f64;
-    println!("PARA {:0.9?} ms", time / 1_000_000.0);
+    println!("PARA {:0.4?} ms (for)", time / 1_000_000.0);
   }
 
   Ok(())
