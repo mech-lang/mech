5b82a7b1de4f58ae2cc105a09a8560d9a2c165ec
Corey Montella <cmontella@live.com>
2021-08-29 22:02:01 -0400

    use old transformations

--- src/transformation.rs
+++ src/transformation.rs
@@ -1,4 +1,4 @@
-use crate::{Column};
+use crate::{Column, TableId, TableIndex, Value, Register, NumberLiteralKind};
 use std::cell::RefCell;
 use std::rc::Rc;
 
@@ -25,11 +25,24 @@ pub enum Transformation {
   ParAddVSIP((OutF64, ArgF64)),
   ParMultiplyVS((ArgF64, ArgF64, OutF64)),
   ParOrVV((ArgBool,ArgBool,OutBool)),
-  ParLessThanVS((ArgF64,f64,OutBool)),
-  ParGreaterThanVS((ArgF64,f64,OutBool)),
-  ParCSGreaterThanVS((ArgF64,f64,f64)),
-  ParSetVS((ArgBool,f64,OutF64)),
+  ParLessThanVS((ArgF64,f32,OutBool)),
+  ParGreaterThanVS((ArgF64,f32,OutBool)),
+  ParCSGreaterThanVS((ArgF64,f32,f32)),
+  ParSetVS((ArgBool,f32,OutF64)),
   ParSetVV((ArgBool,ArgF64,OutF64)),
+  Identifier{ name: Vec<char>, id: u64 },
+  NumberLiteral{kind: NumberLiteralKind, bytes: Vec<u8> },
+  
+  TableAlias{table_id: TableId, alias: u64},
+  TableReference{table_id: TableId, reference: Value},
+  NewTable{table_id: TableId, rows: usize, columns: usize },
+  Constant{table_id: TableId, value: Value, unit: u64},
+  ColumnAlias{table_id: TableId, column_ix: usize, column_alias: u64},
+  Set{table_id: TableId, row: TableIndex, column: TableIndex},
+  RowAlias{table_id: TableId, row_ix: usize, row_alias: u64},
+  Whenever{table_id: TableId, row: TableIndex, column: TableIndex, registers: Vec<Register>},
+  Function{name: u64, arguments: Vec<(u64, TableId, TableIndex, TableIndex)>, out: (TableId, TableIndex, TableIndex)},
+  Select{table_id: TableId, indices: Vec<(TableIndex, TableIndex)>, out: TableId},
 }
 
 impl Transformation {
@@ -68,6 +81,7 @@ impl Transformation {
           *out = *x
         });          
       }
+      _ => (),
     }
   }
 }
