0444eb5044521788cd51120dc8339fb0e652cdeb
Corey Montella <cmontella@live.com>
2021-08-31 15:29:46 -0400

    save test

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -1,10 +1,12 @@
 use mech_syntax::parser::Parser;
 use mech_syntax::ast::Ast;
+use mech_syntax::compiler::Compiler;
 
 fn main() {
 
   let mut parser = Parser::new();
   let mut ast = Ast::new();
+  let mut compiler = Compiler::new();
 
   parser.parse("block 
   🤦🏼‍♂️ = 1
@@ -13,6 +15,11 @@ fn main() {
 
   ast.build_syntax_tree(&parser.parse_tree);
 
+  let tfms = compiler.compile_transformation(&ast.syntax_tree);
+
   println!("{:?}", ast.syntax_tree);
+  for t in tfms {
+    println!("{:?}", t);
+  }
 
 }
