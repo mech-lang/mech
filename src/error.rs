9d7b16e1d459baadc9c4f905d86c8f9338ab3396
cmontella <cmontella@live.com>
2021-10-31 00:22:14 -0400

    Add more error kinds

--- src/error.rs
+++ src/error.rs
@@ -4,11 +4,11 @@
 
 // ## Prelude
 
-use crate::{TableIndex, ValueKind, TableId};
+use crate::{TableIndex, ValueKind, TableId, Transformation};
 
 // ## The Error Struct
 
-#[derive(Clone, Debug, PartialEq, Serialize, Deserialize, Eq, Hash)]
+#[derive(Clone, Debug, Serialize, Deserialize)]
 pub struct MechError { 
   pub block_id: u64,
   pub step_text: String,
@@ -18,15 +18,16 @@ pub struct MechError {
 type Rows = usize;
 type Cols = usize;
 
-#[derive(Clone, Debug, PartialEq, Serialize, Deserialize, Eq, Hash)]
+#[derive(Clone, Debug, Serialize, Deserialize)]
 pub enum MechErrorKind {
   MissingTable(TableId),                         // TableId of missing table
   DimensionMismatch(((Rows,Cols),(Rows,Cols))),  // Argument dimensions are mismatched ((row,col),(row,col))
   MissingColumn((TableId,TableIndex)),           // The identified table is missing a needed column
-  ColumnKindMismatch(Vec<ValueKind>)             // Excepted kind versus given kind
-  //IndexOutOfBounds(((u64, u64), (u64, u64))), // (target) vs (actual) index
-  //DuplicateAlias(u64),                        // Alias ID
-  //DomainMismatch(u64, u64),                   // domain IDs (target vs actual)
-  //MissingFunction(u64),                       // ID of missing function
+  ColumnKindMismatch(Vec<ValueKind>),            // Excepted kind versus given kind
+  //IndexOutOfBounds(((u64, u64), (u64, u64))),  // (target) vs (actual) index
+  //DuplicateAlias(u64),                         // Alias ID
+  //DomainMismatch(u64, u64),                    // domain IDs (target vs actual)
+  MissingFunction(u64),                          // ID of missing function
+  TransformationPending(Transformation),         // Block is unsatisfied so the transformation is not added
   //IncorrectFunctionArgumentType,
 }
