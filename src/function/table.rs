87ea5e5b13a3d1b798eba7cc2dbf50a7093d19b7
cmontella <cmontella@live.com>
2021-11-11 00:37:39 -0500

    Implement copyvi and copyvb

--- src/function/table.rs
+++ src/function/table.rs
@@ -72,3 +72,63 @@ where T: Copy + Debug
   }
   fn to_string(&self) -> String { format!("{:#?}", self)}
 }
+
+
+
+// Copy Vector{Bool Ix} : Vector
+#[derive(Debug)]
+pub struct CopyVB<T> 
+where T: Copy + Debug
+{
+  pub arg: Arg<T>, pub ix: Arg<bool>, pub out: Out<T>
+}
+
+impl<T> MechFunction for CopyVB<T> 
+where T: Copy + Debug
+{
+  fn solve(&mut self) {
+    // Filter the column to include only elements with a true index
+    let filtered: Vec<T>  = 
+      self.arg.borrow()
+         .iter()
+         .zip(self.ix.borrow().iter())
+         .filter_map(|(x,ix)| if *ix {Some(*x)} else {None})
+         .collect::<Vec<T>>();
+    let mut out_brrw = self.out.borrow_mut();
+    let rows = filtered.len();
+    if rows > out_brrw.len() {
+      out_brrw.resize(rows,filtered[0]);
+    }
+    for row in 0..filtered.len() {
+      out_brrw[row] = filtered[row];
+    }
+  }
+  fn to_string(&self) -> String { format!("{:#?}", self)}
+}
+
+// Copy Vector{Int Ix} : Vector
+#[derive(Debug)]
+pub struct CopyVI<T> 
+where T: Copy + Debug
+{
+  pub arg: Arg<T>, pub ix: Arg<u8>, pub out: Out<T>
+}
+
+impl<T> MechFunction for CopyVI<T> 
+where T: Copy + Debug
+{
+  fn solve(&mut self) {
+    let mut out_brrw = self.out.borrow_mut();
+    let arg_brrw = self.arg.borrow();
+    let ix_brrw = self.ix.borrow();
+
+    let rows = ix_brrw.len();
+    if rows > out_brrw.len() {
+      out_brrw.resize(rows,arg_brrw[0]);
+    }
+    for (out_ix, row) in ix_brrw.iter().enumerate() {
+      out_brrw[out_ix] = arg_brrw[*row as usize - 1];
+    }
+  }
+  fn to_string(&self) -> String { format!("{:#?}", self)}
+}
