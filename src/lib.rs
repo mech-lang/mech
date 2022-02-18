cf872458b42471934ae6e21249466a3abaca5630
cmontella <cmontella@live.com>
2022-02-17 22:51:53 -0500

    Update to meet v0.1 rewrite

--- src/lib.rs
+++ src/lib.rs
@@ -3,9 +3,8 @@ extern crate mech_utilities;
 extern crate libm;
 #[macro_use]
 extern crate lazy_static;
-use mech_core::{Transaction};
-use mech_core::{Value, ValueMethods, IndexIterator, Table, ValueIterator};
-use mech_core::{Quantity, ToQuantity, QuantityMath, hash_string, Argument};
+use mech_core::*;
+use mech_utilities::*;
 use libm::{sinf, cosf, fmodf, roundf, floorf};
 use std::cell::RefCell;
 use std::rc::Rc;
@@ -13,39 +12,85 @@ use std::rc::Rc;
 static PI: f32 = 3.141592653589793238462643383279502884197169399375105820974944592307816406286;
 
 lazy_static! {
-  static ref ANGLE: u64 = hash_string("angle");
-  static ref TABLE: u64 = hash_string("table");
+  static ref ANGLE: u64 = hash_str("angle");
+  static ref TABLE: u64 = hash_str("table");
 }
 
-#[no_mangle]
-pub extern "C" fn math_sin(arguments:  &mut Vec<Rc<RefCell<Argument>>>) {
-  let arg = arguments[0].borrow();
-  let in_arg_name = arg.name;
-  let vi = arg.iterator.clone();
-  let mut out = arguments.last().unwrap().borrow().iterator.clone();
-  if in_arg_name == *ANGLE {
-    out.resize(vi.rows(),vi.columns());
-    let mut flag: bool = false;
-    for ((value, changed), out_ix) in vi.clone().zip(out.linear_index_iterator()) {
-      match value.as_quantity() {
-        Some(x) => {
-          let result = match fmodf(x.as_f32().unwrap(), 360.0) {
-            0.0 => 0.0,
-            90.0 => 1.0,
-            180.0 => 0.0,
-            270.0 => -1.0,
-            _ => sinf(x.as_f32().unwrap() * PI / 180.0),
-          };
-          out.set_unchecked_linear(out_ix, Value::from_f32(result));
-        },
-        _ => (), // TODO Alert user that there was an error
-      }
+impl MechFunction for MathSinRadCol {
+  fn solve(&mut self) {
+    self.out.borrow_mut().iter_mut().zip(self.col.borrow().iter()).for_each(|(out, rhs)| *out = sinf(*rhs)); 
+  }
+  fn to_string(&self) -> String { format!("{:#?}", self)}
+}
+
+
+#[derive(Debug)]
+pub struct MathSinRadCol {
+  pub col: function::Arg<f32>, pub out: function::Out<f32>
+}
+
+impl MechFunction for MathSinRadCol {
+  fn solve(&mut self) {
+    self.out.borrow_mut().iter_mut().zip(self.col.borrow().iter()).for_each(|(out, rhs)| *out = sinf(*rhs)); 
+  }
+  fn to_string(&self) -> String { format!("{:#?}", self)}
+}
+
+#[derive(Debug)]
+pub struct MathSinDegCol {
+  pub col: function::Arg<f32>, pub out: function::Out<f32>
+}
+
+impl MechFunction for MathSinDegCol {
+  fn solve(&mut self) {
+    self.out.borrow_mut().iter_mut().zip(self.col.borrow().iter()).for_each(|(out, rhs)| *out = sinf(*rhs * PI / 180.0)); 
+  }
+  fn to_string(&self) -> String { format!("{:#?}", self)}
+}
+
+pub struct MathSin{}
+
+impl MechFunctionCompiler for MathSin {
+  fn compile(&self, block: &mut Block, arguments: &Vec<Argument>, out: &(TableId, TableIndex, TableIndex)) -> std::result::Result<(),MechError> {
+    if arguments.len() > 1 {
+      return Err(MechError::GenericError(1347));
     }
-  } else {
-    // TODO Warn about unknown argument
+    let arg_dims = block.get_arg_dims(&arguments)?;
+    let (arg_name,arg_table_id,_) = arguments[0];
+    let (out_table_id, _, _) = out;
+    let out_table = block.get_table(out_table_id)?;
+    let mut out_brrw = out_table.borrow_mut();
+    out_brrw.set_kind(ValueKind::F32);
+    if arg_name == *ANGLE {
+      match arg_dims[0] {
+        TableShape::Scalar => {
+
+        }
+        TableShape::Column(rows) => {
+          let arg = block.get_arg_columns(arguments)?[0].clone();
+          out_brrw.resize(rows,1);
+          let out_col = out_brrw.get_column_unchecked(0).get_f32().unwrap();
+          match arg {
+            (_,Column::F32(col),ColumnIndex::All) => block.plan.push(MathSinRadCol{col: col.clone(), out: out_col.clone()}),
+            x => {return Err(MechError::GenericError(1348));},
+          }
+        }
+        x => return Err(MechError::GenericError(1350)),
+      }
+
+    } else {
+      return Err(MechError::GenericError(1349));
+    }
+    Ok(())
   }
 }
 
+export_mech_function!(math_sin, math_sin_reg);
+
+extern "C" fn math_sin_reg(registrar: &mut dyn MechFunctionRegistrar) {
+  registrar.register_mech_function(hash_str("math/sin"),Box::new(MathSin{}));
+}
+/*
 #[no_mangle]
 pub extern "C" fn math_cos(arguments:  &mut Vec<Rc<RefCell<Argument>>>) {
   let arg = arguments[0].borrow();
@@ -74,7 +119,6 @@ pub extern "C" fn math_cos(arguments:  &mut Vec<Rc<RefCell<Argument>>>) {
     // TODO Warn about unknown argument
   }
 }
-
 
 #[no_mangle]
 pub extern "C" fn math_round(arguments:  &mut Vec<Rc<RefCell<Argument>>>) {
@@ -119,3 +163,4 @@ pub extern "C" fn math_floor(arguments:  &mut Vec<Rc<RefCell<Argument>>>) {
     // TODO Warn about unknown argument
   }
 }
+*/
