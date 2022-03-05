ebaf07cee4e4ece6674e7f0c1c5b6916321d5033
cmontella <cmontella@live.com>
2022-03-05 11:51:10 -0500

    Add toomanyinputargumetns

--- src/error.rs
+++ src/error.rs
@@ -18,7 +18,7 @@ pub struct MechError {
 #[derive(Clone, Debug, Eq, PartialEq, Hash, Serialize, Deserialize)]
 pub enum MechErrorKind {
   MissingTable(TableId),                             // TableId of missing table
-  DimensionMismatch(((Rows,Cols),(Rows,Cols))),      // Argument dimensions are mismatched ((row,col),(row,col))
+  DimensionMismatch(((Rows,Cols),(Rows,Cols))),     // Argument dimensions are mismatched ((row,col),(row,col))
   //MissingColumn((TableId,TableIndex)),             // The identified table is missing a needed column
   //ColumnKindMismatch(Vec<ValueKind>),              // Excepted kind versus given kind
   //SubscriptOutOfBounds(((Rows,Cols),(Rows,Cols))), // (target) vs (actual) index
@@ -28,11 +28,12 @@ pub enum MechErrorKind {
   MissingFunction(u64),                              // ID of missing function
   //TransformationPending(Transformation),           // Block is unsatisfied so the transformation is not added
   //IncorrectFunctionArgumentType,
-  ZeroIndex,                                          // Zero cannot ever be used as an index.
+  ZeroIndex,                                         // Zero cannot ever be used as an index.
   BlockDisabled,
   GenericError(String),
   Unhandled,
   UnknownFunctionArgument(u64),
   UnknownColumnKind(u64),
+  TooManyInputArguments(usize,usize),                // (given,expected)
   None,
 }
