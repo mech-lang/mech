d61819b285c53ce6eb810458bf790f4ea6a44a9d
cmontella <cmontella@live.com>
2022-04-15 17:33:06 -0400

    gui shapes for real! 😁

--- src/bin/native-gui.rs
+++ src/bin/native-gui.rs
@@ -1,4 +1,5 @@
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
+#![recursion_limit="256"]
 
 use eframe::{epi, egui};
 use eframe::egui::{containers::*, *};
@@ -15,20 +16,79 @@ use std::thread::JoinHandle;
 extern crate lazy_static;
 
 lazy_static! {
-  static ref CONTAINS: u64 = hash_str("contains");
-  static ref PARAMETERS: u64 = hash_str("parameters");
-  static ref HEIGHT: u64 = hash_str("height");
-  static ref WIDTH: u64 = hash_str("width");
-  static ref ROOT: u64 = hash_str("root");
-  static ref KIND: u64 = hash_str("kind");
-  static ref TEXT: u64 = hash_str("text");
-  static ref URL: u64 = hash_str("url");
   static ref LINK: u64 = hash_str("link");
-  static ref CANVAS: u64 = hash_str("link");
+  static ref IMG: u64 = hash_str("img");
+  static ref SRC: u64 = hash_str("src");
+  static ref CONTAINS: u64 = hash_str("contains");
+  static ref KIND: u64 = hash_str("kind");
+  static ref BUTTON: u64 = hash_str("button");
   static ref SLIDER: u64 = hash_str("slider");
   static ref MIN: u64 = hash_str("min");
   static ref MAX: u64 = hash_str("max");
   static ref VALUE: u64 = hash_str("value");
+  static ref CANVAS: u64 = hash_str("canvas");
+  static ref PARAMETERS: u64 = hash_str("parameters");
+  static ref HEIGHT: u64 = hash_str("height");
+  static ref WIDTH: u64 = hash_str("width");
+  static ref SHAPE: u64 = hash_str("shape");
+  static ref CIRCLE: u64 = hash_str("circle");
+  static ref RECTANGLE: u64 = hash_str("rectangle");
+  static ref LINE: u64 = hash_str("line");
+  static ref PATH: u64 = hash_str("path");
+  static ref START__POINT: u64 = hash_str("start-point");
+  static ref LINE__WIDTH: u64 = hash_str("line-width");
+  static ref START__ANGLE: u64 = hash_str("start-angle");
+  static ref END__ANGLE: u64 = hash_str("end-angle");
+  static ref QUADRATIC: u64 = hash_str("quadratic");
+  static ref CONTROL__POINT: u64 = hash_str("control-point");
+  static ref CONTROL__POINTS: u64 = hash_str("control-points");
+  static ref END__POINT: u64 = hash_str("end-point");
+  static ref X1: u64 = hash_str("x1");
+  static ref X2: u64 = hash_str("x2");
+  static ref Y1: u64 = hash_str("y1");
+  static ref Y2: u64 = hash_str("y2");
+  static ref RADIUS: u64 = hash_str("radius");
+  static ref STROKE: u64 = hash_str("stroke");
+  static ref FILL: u64 = hash_str("fill");
+  static ref CENTER__X: u64 = hash_str("center-x");
+  static ref CENTER__Y: u64 = hash_str("center-y");
+  static ref IMAGE: u64 = hash_str("image");
+  static ref X: u64 = hash_str("x");
+  static ref Y: u64 = hash_str("y");
+  static ref ROTATE: u64 = hash_str("rotate");
+  static ref TRANSLATE: u64 = hash_str("translate");
+  static ref SOURCE: u64 = hash_str("source");
+  static ref TIME_TIMER: u64 = hash_str("time/timer");
+  static ref PERIOD: u64 = hash_str("period");
+  static ref TICKS: u64 = hash_str("ticks");
+  static ref TARGET: u64 = hash_str("target");
+  static ref KEY: u64 = hash_str("key");
+  static ref EVENT__ID: u64 = hash_str("event-id");
+  static ref ARC: u64 = hash_str("arc");
+  static ref ELLIPSE: u64 = hash_str("ellipse");
+  static ref MAJOR__AXIS: u64 = hash_str("major-axis");
+  static ref MINOR__AXIS: u64 = hash_str("minor-axis");
+  static ref STARTING__ANGLE: u64 = hash_str("starting-angle");
+  static ref ENDING__ANGLE: u64 = hash_str("ending-angle");
+  static ref TEXT: u64 = hash_str("text");
+  static ref FONT: u64 = hash_str("font");
+  static ref SIZE: u64 = hash_str("size");
+  static ref FACE: u64 = hash_str("face");
+  static ref STYLE: u64 = hash_str("style");
+  static ref WEIGHT: u64 = hash_str("weight");
+  static ref BOLD: u64 = hash_str("bold");
+  static ref NORMAL: u64 = hash_str("normal");
+  static ref ITALIC: u64 = hash_str("italic");
+  static ref FAMILY: u64 = hash_str("family");
+  static ref DIRECTION: u64 = hash_str("direction");
+  static ref ALIGNMENT: u64 = hash_str("alignment");
+  static ref START: u64 = hash_str("start");
+  static ref END: u64 = hash_str("end");
+  static ref LEFT: u64 = hash_str("left");
+  static ref RIGHT: u64 = hash_str("right");
+  static ref CENTER: u64 = hash_str("center");
+  static ref BEZIER: u64 = hash_str("bezier");
+  static ref URL: u64 = hash_str("url");
 }
 
 struct MechApp {
@@ -36,6 +96,7 @@ struct MechApp {
   ticks: f32,
   core: mech_core::Core,
   maestro_thread: Option<JoinHandle<()>>,
+  shapes: Vec<epaint::Shape>,
 }
 
 //static LONG_STRING: &'static str = include_str!(concat!(env!("OUT_DIR"), "/hello.rs"));
@@ -55,17 +116,28 @@ impl MechApp {
       }
     }
     
+    let code = r#"
+timer  
+  #time/timer = [|period<s> ticks<u64>|]"#;
+
+    let mut compiler = Compiler::new();
+    let blocks = compiler.compile_str(&code).unwrap();
+    mech_core.load_blocks(blocks);
+
+    let mut shapes = vec![epaint::Shape::Noop; 100000];
+
     Self {
       ticks: 0.0,
       //mech_client,
       core: mech_core,
       maestro_thread: None,
+      shapes,
     }
   }
   
   pub fn render_app(&mut self, ui: &mut egui::Ui) -> Result<(), MechError> {
     match self.core.get_table("mech/app") {
-      Ok(app_table) => {        
+      Ok(app_table) => { 
         let app_table_brrw = app_table.borrow();
         ui.columns(app_table_brrw.cols, |cols| {
           for (col, col_ui) in cols.iter_mut().enumerate() {
@@ -126,10 +198,12 @@ impl MechApp {
               // Render an element
               if raw_kind == *LINK { self.render_link(table,ui)?; }
               else if raw_kind == *SLIDER { self.render_slider(table,ui)?; }
-              /*else if raw_kind == *A { render_link(table,&mut container,wasm_core)?; }
-              else if raw_kind == *IMG { render_img(table,&mut container,wasm_core)?; }
+              /*else if raw_kind == *IMG { render_img(table,&mut container,wasm_core)?; }
               else if raw_kind == *BUTTON { render_button(table,&mut container,wasm_core)?; }
-              */else if raw_kind == *CANVAS { self.render_canvas(table,ui)?; }
+              */
+              else if raw_kind == *CANVAS { 
+                self.render_canvas(table,ui)?; 
+              }
               else {
                 return Err(MechError{id: 6489, kind: MechErrorKind::GenericError(format!("{:?}", raw_kind))});
               }
@@ -173,38 +247,68 @@ impl MechApp {
   }
 
   pub fn render_canvas(&mut self, table: &Table, container: &mut egui::Ui) -> Result<(),MechError> {
-    /*for row in 1..=table.rows {
+    for row in 1..=table.rows {
       match table.get(&TableIndex::Index(row), &TableIndex::Alias(*CONTAINS)) {
-        Ok(contents) => {
-          // Is there a parameters field?
-          match table.get(&TableIndex::Index(row), &TableIndex::Alias(*PARAMETERS)) {
-            Ok(Value::Reference(parameters_table_id)) => {
-              let parameters_table = self.core.get_table_by_id(*parameters_table_id.unwrap()).unwrap();
-              let parameters_table_brrw = parameters_table.borrow();
-              match (parameters_table_brrw.get(&TableIndex::Index(1), &TableIndex::Alias(*HEIGHT)),
-                     parameters_table_brrw.get(&TableIndex::Index(1), &TableIndex::Alias(*WIDTH))) {
-                (Ok(Value::F32(height)),Ok(Value::F32(width))) => {
-                  //canvas.set_attribute("height", &format!("{:?}",height));
-                  //canvas.set_attribute("width", &format!("{:?}",width));
+        Ok(Value::Reference(contains_table_id)) => {
+          Frame::none().show(container, |ui| {
+            let table = self.core.get_table_by_id(*contains_table_id.unwrap()).unwrap();
+            let table_brrw = table.borrow();
+            match (table_brrw.get(&TableIndex::Index(1), &TableIndex::Alias(*SHAPE)),
+                    table_brrw.get(&TableIndex::Index(1), &TableIndex::Alias(*PARAMETERS)))  {
+              (Ok(Value::String(kind)),Ok(Value::Reference(contains_table_id))) => {
+                let table = self.core.get_table_by_id(*contains_table_id.unwrap()).unwrap();
+                let table_brrw = table.borrow();
+                let raw_kind = kind.hash();
+                // Render an element
+                if raw_kind == *CIRCLE { 
+                  let shapes = self.render_circle(&table_brrw,ui)?;
+                  ui.painter().extend(shapes);
+                } else {
+                  return Err(MechError{id: 6489, kind: MechErrorKind::GenericError(format!("{:?}", raw_kind))});
                 }
-                x => {return Err(MechError{id: 6496, kind: MechErrorKind::GenericError(format!("{:?}", x))});},
               }
+              x => {
+                return Err(MechError{id: 6496, kind: MechErrorKind::GenericError(format!("{:?}", x))});
+              },
             }
-            x => {return Err(MechError{id: 6496, kind: MechErrorKind::GenericError(format!("{:?}", x))});},
-          }
-          // Add the contents
-          match table.get(&TableIndex::Index(row), &TableIndex::Alias(*CONTAINS)) {
-            Ok(Value::Reference(contains_table_id)) => {
-              //canvas.set_attribute("elements", &format!("{}",contains_table_id.unwrap()));
-            }
-            x => {return Err(MechError{id: 6496, kind: MechErrorKind::GenericError(format!("{:?}", x))});},
-          }
-          //container.append_child(&canvas)?;
+            Ok(())
+          });
         }
         x => {return Err(MechError{id: 6496, kind: MechErrorKind::GenericError(format!("{:?}", x))});},
       }
-    }*/
+    }
     Ok(())
+  }
+
+  pub fn render_circle(&mut self, table: &Table, container: &mut egui::Ui) -> Result<Vec<epaint::Shape>,MechError> {
+    let x = table.get_column_unchecked(0);
+    let y = table.get_column_unchecked(1);
+    let r = table.get_column_unchecked(2);
+
+    let desired_size = container.available_width() * vec2(1.0, 1.00);
+    let (_id, rect) = container.allocate_space(desired_size);
+
+    //let to_screen = emath::RectTransform::from_to(Rect::from_x_y_ranges(0.0..=500.0, 0.0..=500.0), rect);
+    let mut shapes = vec![];
+    let color = Color32::from_additive_luminance(196);
+
+    match (x,y,r) {
+      (Column::F32(x), Column::F32(y), Column::F32(r)) => {
+        let x_brrw = x.borrow();
+        let y_brrw = y.borrow();
+        let r_brrw = r.borrow();
+        for i in 0..table.rows {
+          shapes.push(epaint::Shape::Circle(epaint::CircleShape{
+            center: Pos2{x: x_brrw[i].into(), y: y_brrw[i].into()},
+            radius: r_brrw[i].into(),
+            fill: color,
+            stroke: epaint::Stroke::new(1.0,color),
+          }));
+        }
+      }
+      x => {return Err(MechError{id: 6496, kind: MechErrorKind::GenericError(format!("{:?}", x))});},
+    }
+    Ok(shapes)
   }
 
   pub fn render_slider(&mut self, table: &Table, container: &mut egui::Ui) -> Result<(),MechError> {
@@ -233,7 +337,6 @@ impl MechApp {
     Ok(())
   }  
 
-
 }
 
 impl epi::App for MechApp {
@@ -244,50 +347,16 @@ impl epi::App for MechApp {
 
   fn update(&mut self, ctx: &egui::Context, frame: &epi::Frame) {
     let Self { ticks, core, .. } = self;
-
     egui::CentralPanel::default().show(ctx, |ui| {
+      ui.ctx().request_repaint();
+      let desired_size = ui.available_width() * vec2(1.0, 0.35);
+      let (_id, rect) = ui.allocate_space(desired_size);
+
       self.render_app(ui);
-      /*let color = Color32::from_additive_luminance(196);
 
-      Frame::none().show(ui, |ui| {
-        
-        ui.ctx().request_repaint();
-        let time = ui.input().time;
-        let table = self.core.get_table("balls").unwrap();
-        let table_brrw = table.borrow();
-        let x = table_brrw.get_column_unchecked(0);
-        let y = table_brrw.get_column_unchecked(1);
-        let r = table_brrw.get_column_unchecked(4);
-
-        let desired_size = ui.available_width() * vec2(1.0, 0.35);
-        let (_id, rect) = ui.allocate_space(desired_size);
-
-        //let to_screen = emath::RectTransform::from_to(Rect::from_x_y_ranges(0.0..=500.0, 0.0..=500.0), rect);
-
-        let mut shapes = vec![];
-
-        match (x,y,r) {
-          (Column::F32(x), Column::F32(y), Column::F32(r)) => {
-            let x_brrw = x.borrow();
-            let y_brrw = y.borrow();
-            let r_brrw = r.borrow();
-            for i in 0..table_brrw.rows {
-              shapes.push(epaint::Shape::Circle(epaint::CircleShape{
-                center: Pos2{x: x_brrw[i].into(), y: y_brrw[i].into()},
-                radius: r_brrw[i].into(),
-                fill: color,
-                stroke: epaint::Stroke::new(1.0,color),
-              }));
-            }
-          }
-          _ => (),
-        }
-
-        ui.painter().extend(shapes);
-        let change = Change::Set((hash_str("time/timer"),vec![(TableIndex::Index(1),TableIndex::Index(2),Value::U64(U64::new(time as u64)))]));
-        self.core.process_transaction(&vec![change]);
-      });*/
-      //let table = self.core.get_table("balls");
+      let time = ui.input().time;
+      let change = Change::Set((hash_str("time/timer"),vec![(TableIndex::Index(1),TableIndex::Index(2),Value::U64(U64::new(time as u64)))]));
+      self.core.process_transaction(&vec![change]);
     });
   }
 }
