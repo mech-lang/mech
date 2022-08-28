c0d1dca95b9323a77630df8dfd92da946cb476d6
cmontella <cmontella@live.com>
2022-08-28 14:59:33 -0400

    remove dead code

--- src/out.rs
+++ src/out.rs
@@ -1,7 +1,6 @@
 extern crate crossbeam_channel;
 use mech_core::*;
 use mech_utilities::*;
-//use std::sync::mpsc::{self, Sender};
 use std::thread::{self};
 use crossbeam_channel::Sender;
 use std::collections::HashMap;
