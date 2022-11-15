8b5a0006451ad4acf0e474dfcbec19cfdfb978fb
Xinhao Tang <xit223@lehigh.edu>
2022-11-14 20:23:08 -0500

    fix dependency of typescript

--- editor-modes/vscode/src/extension.ts
+++ editor-modes/vscode/src/extension.ts
@@ -44,7 +44,7 @@ export function activate(context: ExtensionContext) {
 	// Options to control the language client
 	const clientOptions: LanguageClientOptions = {
 		// Register the server for plain text documents
-		documentSelector: [{ scheme: 'file', language: 'plaintext' }],
+		documentSelector: [{ scheme: 'file', language: 'Mech' }],
 		synchronize: {
 			// Notify the server about file changes to '.clientrc files contained in the workspace
 			fileEvents: workspace.createFileSystemWatcher('**/.clientrc')
