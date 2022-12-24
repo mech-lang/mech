4c2f7930c41016a865761a6875bc12c232db4975
cmontella <cmontella@live.com>
2022-12-23 21:56:04 -0500

    upgrade repl

--- src/repl.rs
+++ src/repl.rs
@@ -1,9 +1,10 @@
 use mech_syntax::*;
 use mech_syntax::parser::*;
+use syntax::parser::tag;
 use mech_utilities::*;
 use mech_core::*;
 use mech_core::nodes::*;
-use crate::minify_blocks;
+use crate::{minify_blocks, read_mech_files};
 
 #[macro_use]
 use nom::{
@@ -13,7 +14,6 @@ use nom::{
   combinator::opt,
   error::{context, convert_error, ErrorKind, ParseError, VerboseError},
   multi::{many1, many0},
-  bytes::complete::{tag},
   character::complete::{alphanumeric1, alpha1, digit1, space0, space1},
 };
 
@@ -25,95 +25,104 @@ pub enum ReplCommand {
   Pause,
   Resume,
   Stop,
-  SaveCore(u64),
-  PrintCore(Option<u64>),
+  Save,
+  Debug,
   Clear,
+  Info,
+  Core(u64),
   Table(u64),
-  Code(MechCode),
-  //ParsedCode(ParserNode),
+  Code(Vec<MechCode>),
   Empty,
   Error,
 }
 
-/*fn mech_code(input: &str) -> IResult<&str, ReplCommand, VerboseError<&str>> {
+fn core(input: ParseString) -> ParseResult<ReplCommand> {
+  let (input, _) = tag("core")(input)?;
+  let (input, _) = syntax::parser::skip_spaces(input)?;
+  let (input, text) = syntax::parser::text(input).unwrap();
+  let core_id = syntax::compiler::compile_text(&text).unwrap();
+  match core_id.parse::<u64>() {
+    Ok(core_id) =>  Ok((input, ReplCommand::Core(core_id))), 
+    Err(err) => Ok((input, ReplCommand::Error)),
+  }
+}
+
+fn mech_code(input: ParseString) -> ParseResult<ReplCommand> {
   // Try parsing mech code fragment
+  let (input,parse_tree) = syntax::parser::parse_mech_fragment(input)?;
   let mut compiler = compiler::Compiler::new();
-  match compiler.compile_fragment(input) {
+  match compiler.compile_fragment_from_parse_tree(parse_tree) {
     Ok(blocks) => {
       let mut mb = minify_blocks(&blocks);
-      Ok((input, ReplCommand::Code(MechCode::MiniBlocks(mb))))
+      Ok((input, ReplCommand::Code(vec![MechCode::MiniBlocks(mb)])))
     },
     Err(_) => Ok((input, ReplCommand::Error)),
   }
 }
 
-fn clear(input: &str) -> IResult<&str, ReplCommand, VerboseError<&str>> {
-  let (input, _) = tag("clear")(input)?;
-  Ok((input, ReplCommand::Clear))
-}
-
-fn core(input: &str) -> IResult<&str, ReplCommand, VerboseError<&str>> {
-  let (input, _) = tag("core")(input)?;
-  let (input, _) = space0(input)?;
-  let (input, core_id) = opt(digit1)(input)?;
-  let core_id = match core_id {
-    Some(core_id) => Some(core_id.parse::<u64>().unwrap()),
-    None => None,
-  };
-  Ok((input, ReplCommand::PrintCore(core_id)))
-}
-
-fn save(input: &str) -> IResult<&str, ReplCommand, VerboseError<&str>> {
-  let (input, _) = tag("save")(input)?;
-  let (input, _) = space0(input)?;
-  let (input, core_id) = opt(digit1)(input)?;
-  let core_id = match core_id {
-    Some(core_id) => core_id.parse::<u64>().unwrap(),
-    None => 0,
-  };
-  Ok((input, ReplCommand::SaveCore(core_id)))
-}
-
-fn quit(input: &str) -> IResult<&str, ReplCommand, VerboseError<&str>> {
-  let (input, _) = alt((tag("quit"),tag("exit")))(input)?;
-  Ok((input, ReplCommand::Quit))
-}
-
-fn resume(input: &str) -> IResult<&str, ReplCommand, VerboseError<&str>> {
-  let (input, _) = tag("resume")(input)?;
-  Ok((input, ReplCommand::Resume))
-}
-
-fn pause(input: &str) -> IResult<&str, ReplCommand, VerboseError<&str>> {
-  let (input, _) = tag("pause")(input)?;
-  Ok((input, ReplCommand::Pause))
-}
-
-fn load(input: &str) -> IResult<&str, ReplCommand, VerboseError<&str>> {
+fn load(input: ParseString) -> ParseResult<ReplCommand> {
   let (input, _) = tag("load")(input)?;
-  let (input, _) = space1(input)?;
-  let (input, id) = syntax::parser::text(input).unwrap();
-  Ok((input, ReplCommand::Code(MechCode::String("".to_string()))))
+  let (input, _) = syntax::parser::skip_spaces(input)?;
+  let (input, text) = syntax::parser::text(input).unwrap();
+  let string = syntax::compiler::compile_text(&text).unwrap();
+  match read_mech_files(&vec![string.clone()]) {
+    Ok(code) => {
+      Ok((input, ReplCommand::Code(code)))
+    }
+    Err(err) => {
+      Ok((input, ReplCommand::Error))
+    }
+  }
 }
 
-fn help(input: &str) -> IResult<&str, ReplCommand, VerboseError<&str>> {
-  let (input, _) = tag("help")(input)?;
-  Ok((input, ReplCommand::Help))
+pub fn quit(input: ParseString) -> ParseResult<ReplCommand> {
+  let (input,_) = alt((tag("quit"),tag("exit")))(input)?;
+  Ok((input,ReplCommand::Quit))
 }
 
-fn command(input: &str) -> IResult<&str, ReplCommand, VerboseError<&str>> {
-  let (input, _) = tag(":")(input)?;
-  let (input, command) = alt((quit, help, pause, resume, core, clear, save))(input)?;
-  Ok((input, command))
-}*/
+pub fn clear(input: ParseString) -> ParseResult<ReplCommand> {
+  let (input,_) = tag("clear")(input)?;
+  Ok((input,ReplCommand::Clear))
+}
+
+pub fn save(input: ParseString) -> ParseResult<ReplCommand> {
+  let (input,_) = tag("save")(input)?;
+  Ok((input,ReplCommand::Save))
+}
+
+pub fn info(input: ParseString) -> ParseResult<ReplCommand> {
+  let (input,_) = tag("info")(input)?;
+  Ok((input,ReplCommand::Info))
+}
+
+pub fn debug(input: ParseString) -> ParseResult<ReplCommand> {
+  let (input,_) = tag("debug")(input)?;
+  Ok((input,ReplCommand::Debug))
+}
 
 pub fn help(input: ParseString) -> ParseResult<ReplCommand> {
-  let (input,_) = syntax::parser::tag("help")(input)?;
+  let (input,_) = tag("help")(input)?;
   Ok((input,ReplCommand::Help))
 }
 
+pub fn pause(input: ParseString) -> ParseResult<ReplCommand> {
+  let (input,_) = tag("pause")(input)?;
+  Ok((input,ReplCommand::Pause))
+}
+
+pub fn resume(input: ParseString) -> ParseResult<ReplCommand> {
+  let (input,_) = tag("resume")(input)?;
+  Ok((input,ReplCommand::Resume))
+}
+
+fn command(input: ParseString) -> ParseResult<ReplCommand> {
+  let (input, _) = tag(":")(input)?;
+  let (input, command) = alt((help,quit,clear,load,save,debug,info))(input)?;
+  Ok((input, command))
+}
+
 pub fn parse_repl_command(input: ParseString) -> ParseResult<ReplCommand> {
-  let (input,command) = help(input)?;
+  let (input,command) = alt((command, mech_code))(input)?;
   Ok((input, command))
 }
 
@@ -124,14 +133,8 @@ pub fn parse(text: &str) -> Result<ReplCommand, MechError> {
   let remaining: ParseString;
 
   match parse_repl_command(ParseString::new(&graphemes)) {
-    Ok(_) => {
-
-    }
-    Err(_)=> {
-
-    }
+    Ok((input,command)) => Ok(command),
+    Err(_)=> Ok(ReplCommand::Error),
   }
 
-
-  Ok(ReplCommand::Empty)
 }
