85e5686d1ab4435eb5941e1acdbbc09234ae30e2
cmontella <cmontella@live.com>
2024-06-17 21:23:34 -0400

    Update main

--- src/bin/mech2.rs
+++ src/bin/mech2.rs
@@ -14,40 +14,40 @@ use std::env;
 #[tokio::main]
 async fn main() -> Result<(), MechError> {
   
-    let args: Vec<_> = env::args().collect();
-    match args.get(1) {
-      Some(filename) => {
-        let s = fs::read_to_string(&filename).unwrap();
-        match parser2::parse(&s) {
-          Ok(tree) => { 
-            println!("\n-------------------------------- 🌳 Syntax Tree --------------------------------\n");
-            let tree_hash = hash_str(&format!("{:#?}", tree));
-            println!("Tree Hash: {:?}", tree_hash);
-            println!("{:#?}", tree);
-            let mut intrp = Interpreter::new();
-            let result = intrp.interpret(&tree);
-            println!("\n-------------------------------- 💻 Interpreter --------------------------------\n");
-            println!("Symbols: {:#?}", intrp.symbols); 
-            println!("Plan:"); 
-            for (ix,fxn) in intrp.plan.borrow().iter().enumerate() {
-              println!("  {}. {}", ix + 1, fxn.to_string());
-            }
-            for fxn in intrp.plan.borrow().iter() {
-              fxn.solve();
-            }
-            println!("\n---------------------------------- 🌟 Result ----------------------------------\n");
-            println!("Result:  {:#?}", result);
-          },
-          Err(err) => {
-            if let MechErrorKind::ParserError(tree, report, _) = err.kind {
-              parser::print_err_report(&s, &report);
-            } else {
-              panic!("Unexpected error type");
-            }
+  let args: Vec<_> = env::args().collect();
+  match args.get(1) {
+    Some(filename) => {
+      let s = fs::read_to_string(&filename).unwrap();
+      match parser2::parse(&s) {
+        Ok(tree) => { 
+          println!("\n-------------------------------- 🌳 Syntax Tree --------------------------------\n");
+          let tree_hash = hash_str(&format!("{:#?}", tree));
+          println!("Tree Hash: {:?}", tree_hash);
+          println!("{:#?}", tree);
+          let mut intrp = Interpreter::new();
+          let result = intrp.interpret(&tree);
+          println!("\n-------------------------------- 💻 Interpreter --------------------------------\n");
+          println!("Symbols: {:#?}", intrp.symbols); 
+          println!("Plan:"); 
+          for (ix,fxn) in intrp.plan.borrow().iter().enumerate() {
+            println!("  {}. {}", ix + 1, fxn.to_string());
+          }
+          for fxn in intrp.plan.borrow().iter() {
+            fxn.solve();
+          }
+          println!("\n---------------------------------- 🌟 Result ----------------------------------\n");
+          println!("Result:  {:#?}", result);
+        },
+        Err(err) => {
+          if let MechErrorKind::ParserError(tree, report, _) = err.kind {
+            parser::print_err_report(&s, &report);
+          } else {
+            panic!("Unexpected error type");
           }
         }
       }
-      None => println!("Missing path to .mec file."),
     }
+    None => println!("Missing path to .mec file."),
+  }
   Ok(())
 }
