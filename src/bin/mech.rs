e9a3754f8c743e69a7a7d180a7857fb6f151eca3
cmontella <cmontella@live.com>
2024-09-16 14:48:09 -0400

    bump version

--- src/bin/mech.rs
+++ src/bin/mech.rs
@@ -25,7 +25,7 @@ use serde_json;
 
 
 fn main() -> Result<(), MechError> {
-  let version = "0.2.10";
+  let version = "0.2.11";
   let text_logo = r#"
   ┌─────────┐ ┌──────┐ ┌─┐ ┌──┐ ┌─┐   ┌─┐
   └───┐ ┌───┘ └──────┘ │ │ └┐ │ │ │   │ │
