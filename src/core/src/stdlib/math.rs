ffb397b7db61f0ca1392f86244729cfcddf3a527
cmontella <cmontella@live.com>
2024-11-18 00:31:07 -0500

    Moar!

--- src/core/src/stdlib/math.rs
+++ src/core/src/stdlib/math.rs
@@ -14,26 +14,41 @@ macro_rules! impl_math_fxns {
 
 #[macro_export]
 macro_rules! impl_urnop_match_arms2 {
-  ($lib:ident, $arg:expr, $($lhs_type:ident => $($matrix_kind:ident, $target_type:ident, $default:expr),+);+ $(;)?) => {
+  ($lib:ident, $arg:expr, $($lhs_type:ident => $($matrix_kind:ident, $target_type:ident, $default:expr, $value_string:tt),+);+ $(;)?) => {
     paste!{
       match $arg {
         $(
           $(
             (Value::$lhs_type(arg)) => Ok(Box::new([<$lib $lhs_type S>]{arg: arg.clone(), out: new_ref($default) })),
+            #[cfg(all(feature = $value_string, feature = "Matrix1"))]
             (Value::$matrix_kind(Matrix::<$target_type>::Matrix1(arg))) => Ok(Box::new([<$lib $lhs_type M1>]{arg, out: new_ref(Matrix1::from_element($default))})),
+            #[cfg(all(feature = $value_string, feature = "Matrix2"))]
             (Value::$matrix_kind(Matrix::<$target_type>::Matrix2(arg))) => Ok(Box::new([<$lib $lhs_type M2>]{arg, out: new_ref(Matrix2::from_element($default))})),
+            #[cfg(all(feature = $value_string, feature = "Matrix3"))]
             (Value::$matrix_kind(Matrix::<$target_type>::Matrix3(arg))) => Ok(Box::new([<$lib $lhs_type M3>]{arg, out: new_ref(Matrix3::from_element($default))})),
+            #[cfg(all(feature = $value_string, feature = "Matrix4"))]
             (Value::$matrix_kind(Matrix::<$target_type>::Matrix4(arg))) => Ok(Box::new([<$lib $lhs_type M4>]{arg, out: new_ref(Matrix4::from_element($default))})),
-            (Value::$matrix_kind(Matrix::<$target_type>::RowVector2(arg))) => Ok(Box::new([<$lib $lhs_type R2>]{arg: arg.clone(), out: new_ref(RowVector2::from_element($default)) })),
-            (Value::$matrix_kind(Matrix::<$target_type>::RowVector3(arg))) => Ok(Box::new([<$lib $lhs_type R3>]{arg: arg.clone(), out: new_ref(RowVector3::from_element($default)) })),
-            (Value::$matrix_kind(Matrix::<$target_type>::RowVector4(arg))) => Ok(Box::new([<$lib $lhs_type R4>]{arg: arg.clone(), out: new_ref(RowVector4::from_element($default)) })),
-            (Value::$matrix_kind(Matrix::<$target_type>::Vector2(arg))) => Ok(Box::new([<$lib $lhs_type V2>]{arg: arg.clone(), out: new_ref(Vector2::from_element($default)) })),
-            (Value::$matrix_kind(Matrix::<$target_type>::Vector3(arg))) => Ok(Box::new([<$lib $lhs_type V3>]{arg: arg.clone(), out: new_ref(Vector3::from_element($default)) })),
-            (Value::$matrix_kind(Matrix::<$target_type>::Vector4(arg))) => Ok(Box::new([<$lib $lhs_type V4>]{arg: arg.clone(), out: new_ref(Vector4::from_element($default)) })),
+            #[cfg(all(feature = $value_string, feature = "Matrix2x3"))]
             (Value::$matrix_kind(Matrix::<$target_type>::Matrix2x3(arg))) => Ok(Box::new([<$lib $lhs_type M2x3>]{arg, out: new_ref(Matrix2x3::from_element($default))})),         
+            #[cfg(all(feature = $value_string, feature = "Matrix3x2"))]
             (Value::$matrix_kind(Matrix::<$target_type>::Matrix3x2(arg))) => Ok(Box::new([<$lib $lhs_type M3x2>]{arg, out: new_ref(Matrix3x2::from_element($default))})),         
+            #[cfg(all(feature = $value_string, feature = "RowVector2"))]
+            (Value::$matrix_kind(Matrix::<$target_type>::RowVector2(arg))) => Ok(Box::new([<$lib $lhs_type R2>]{arg: arg.clone(), out: new_ref(RowVector2::from_element($default)) })),
+            #[cfg(all(feature = $value_string, feature = "RowVector3"))]
+            (Value::$matrix_kind(Matrix::<$target_type>::RowVector3(arg))) => Ok(Box::new([<$lib $lhs_type R3>]{arg: arg.clone(), out: new_ref(RowVector3::from_element($default)) })),
+            #[cfg(all(feature = $value_string, feature = "RowVector4"))]
+            (Value::$matrix_kind(Matrix::<$target_type>::RowVector4(arg))) => Ok(Box::new([<$lib $lhs_type R4>]{arg: arg.clone(), out: new_ref(RowVector4::from_element($default)) })),
+            #[cfg(all(feature = $value_string, feature = "RowVectorD"))]
             (Value::$matrix_kind(Matrix::<$target_type>::RowDVector(arg))) => Ok(Box::new([<$lib $lhs_type RD>]{arg: arg.clone(), out: new_ref(RowDVector::from_element(arg.borrow().len(),$default))})),
+            #[cfg(all(feature = $value_string, feature = "Vector2"))]
+            (Value::$matrix_kind(Matrix::<$target_type>::Vector2(arg))) => Ok(Box::new([<$lib $lhs_type V2>]{arg: arg.clone(), out: new_ref(Vector2::from_element($default)) })),
+            #[cfg(all(feature = $value_string, feature = "Vector3"))]
+            (Value::$matrix_kind(Matrix::<$target_type>::Vector3(arg))) => Ok(Box::new([<$lib $lhs_type V3>]{arg: arg.clone(), out: new_ref(Vector3::from_element($default)) })),
+            #[cfg(all(feature = $value_string, feature = "Vector4"))]
+            (Value::$matrix_kind(Matrix::<$target_type>::Vector4(arg))) => Ok(Box::new([<$lib $lhs_type V4>]{arg: arg.clone(), out: new_ref(Vector4::from_element($default)) })),
+            #[cfg(all(feature = $value_string, feature = "VectorD"))]
             (Value::$matrix_kind(Matrix::<$target_type>::DVector(arg))) => Ok(Box::new([<$lib $lhs_type VD>]{arg: arg.clone(), out: new_ref(DVector::from_element(arg.borrow().len(),$default))})),
+            #[cfg(all(feature = $value_string, feature = "MatrixD"))]
             (Value::$matrix_kind(Matrix::<$target_type>::DMatrix(arg))) => {
               let (rows,cols) = {arg.borrow().shape()};
               Ok(Box::new([<$lib $lhs_type MD>]{arg, out: new_ref(DMatrix::from_element(rows,cols,$default))}))},
@@ -77,21 +92,36 @@ macro_rules! impl_math_urop {
   ($fxn_name:ident, $type:ident, $op_fxn:ident) => {
     paste!{
       impl_urop!([<$fxn_name $type S>], $type, $type, [<$op_fxn _op>]);
+      #[cfg(feature = "Matrix1")]
       impl_urop!([<$fxn_name $type M1>], Matrix1<$type>, Matrix1<$type>, [<$op_fxn _vec_op>]);
+      #[cfg(feature = "Matrix2")]
       impl_urop!([<$fxn_name $type M2>], Matrix2<$type>, Matrix2<$type>, [<$op_fxn _vec_op>]);
+      #[cfg(feature = "Matrix3")]
       impl_urop!([<$fxn_name $type M3>], Matrix3<$type>, Matrix3<$type>, [<$op_fxn _vec_op>]);
+      #[cfg(feature = "Matrix4")]
       impl_urop!([<$fxn_name $type M4>], Matrix4<$type>, Matrix4<$type>, [<$op_fxn _vec_op>]);
-      impl_urop!([<$fxn_name $type R2>], RowVector2<$type>, RowVector2<$type>, [<$op_fxn _vec_op>]);
-      impl_urop!([<$fxn_name $type R3>], RowVector3<$type>, RowVector3<$type>, [<$op_fxn _vec_op>]);
-      impl_urop!([<$fxn_name $type R4>], RowVector4<$type>, RowVector4<$type>, [<$op_fxn _vec_op>]);
-      impl_urop!([<$fxn_name $type V2>], Vector2<$type>, Vector2<$type>, [<$op_fxn _vec_op>]);
-      impl_urop!([<$fxn_name $type V3>], Vector3<$type>, Vector3<$type>, [<$op_fxn _vec_op>]);
-      impl_urop!([<$fxn_name $type V4>], Vector4<$type>, Vector4<$type>, [<$op_fxn _vec_op>]);
+      #[cfg(feature = "Matrix2x3")]
       impl_urop!([<$fxn_name $type M2x3>], Matrix2x3<$type>, Matrix2x3<$type>, [<$op_fxn _vec_op>]);
+      #[cfg(feature = "Matrix3x2")]
       impl_urop!([<$fxn_name $type M3x2>], Matrix3x2<$type>, Matrix3x2<$type>, [<$op_fxn _vec_op>]);
-      impl_urop!([<$fxn_name $type VD>], DVector<$type>, DVector<$type>, [<$op_fxn _vec_op>]);
-      impl_urop!([<$fxn_name $type RD>], RowDVector<$type>, RowDVector<$type>, [<$op_fxn _vec_op>]);
+      #[cfg(feature = "MatrixD")]
       impl_urop!([<$fxn_name $type MD>], DMatrix<$type>, DMatrix<$type>, [<$op_fxn _vec_op>]);
+      #[cfg(feature = "RowVector2")]
+      impl_urop!([<$fxn_name $type R2>], RowVector2<$type>, RowVector2<$type>, [<$op_fxn _vec_op>]);
+      #[cfg(feature = "RowVector3")]
+      impl_urop!([<$fxn_name $type R3>], RowVector3<$type>, RowVector3<$type>, [<$op_fxn _vec_op>]);
+      #[cfg(feature = "RowVector4")]
+      impl_urop!([<$fxn_name $type R4>], RowVector4<$type>, RowVector4<$type>, [<$op_fxn _vec_op>]);
+      #[cfg(feature = "RowVectorD")]
+      impl_urop!([<$fxn_name $type RD>], RowDVector<$type>, RowDVector<$type>, [<$op_fxn _vec_op>]);
+      #[cfg(feature = "Vector2")]
+      impl_urop!([<$fxn_name $type V2>], Vector2<$type>, Vector2<$type>, [<$op_fxn _vec_op>]);
+      #[cfg(feature = "Vector3")]
+      impl_urop!([<$fxn_name $type V3>], Vector3<$type>, Vector3<$type>, [<$op_fxn _vec_op>]);
+      #[cfg(feature = "Vector4")]
+      impl_urop!([<$fxn_name $type V4>], Vector4<$type>, Vector4<$type>, [<$op_fxn _vec_op>]);
+      #[cfg(feature = "VectorD")]
+      impl_urop!([<$fxn_name $type VD>], DVector<$type>, DVector<$type>, [<$op_fxn _vec_op>]);
     }
   }
 }
@@ -104,8 +134,8 @@ fn impl_cos_fxn(lhs_value: Value) -> Result<Box<dyn MechFunction>, MechError> {
   impl_urnop_match_arms2!(
     MathCos,
     (lhs_value),
-    F32 => MatrixF32, F32, F32::zero();
-    F64 => MatrixF64, F64, F64::zero();
+    F32 => MatrixF32, F32, F32::zero(), "F32";
+    F64 => MatrixF64, F64, F64::zero(), "F64";
   )
 }
 
@@ -163,8 +193,8 @@ fn impl_sin_fxn(lhs_value: Value) -> Result<Box<dyn MechFunction>, MechError> {
   impl_urnop_match_arms2!(
     MathSin,
     (lhs_value),
-    F32 => MatrixF32, F32, F32::zero();
-    F64 => MatrixF64, F64, F64::zero();
+    F32 => MatrixF32, F32, F32::zero(), "F32";
+    F64 => MatrixF64, F64, F64::zero(), "F64";
   )
 }
 
@@ -216,18 +246,18 @@ fn impl_add_fxn(lhs_value: Value, rhs_value: Value) -> Result<Box<dyn MechFuncti
   impl_binop_match_arms!(
     Add,
     (lhs_value, rhs_value),
-    I8,   I8   => MatrixI8,   i8,   i8::zero();
-    I16,  I16  => MatrixI16,  i16,  i16::zero();
-    I32,  I32  => MatrixI32,  i32,  i32::zero();
-    I64,  I64  => MatrixI64,  i64,  i64::zero();
-    I128, I128 => MatrixI128, i128, i128::zero();
-    U8,   U8   => MatrixU8,   u8,   u8::zero();
-    U16,  U16  => MatrixU16,  u16,  u16::zero();
-    U32,  U32  => MatrixU32,  u32,  u32::zero();
-    U64,  U64  => MatrixU64,  u64,  u64::zero();
-    U128, U128 => MatrixU128, u128, u128::zero();
-    F32,  F32  => MatrixF32,  F32,  F32::zero();
-    F64,  F64  => MatrixF64,  F64,  F64::zero();
+    I8,   I8   => MatrixI8,   i8,   i8::zero(), "I8";
+    I16,  I16  => MatrixI16,  i16,  i16::zero(), "I16";
+    I32,  I32  => MatrixI32,  i32,  i32::zero(), "I32";
+    I64,  I64  => MatrixI64,  i64,  i64::zero(), "I64";
+    I128, I128 => MatrixI128, i128, i128::zero(), "I128";
+    U8,   U8   => MatrixU8,   u8,   u8::zero(), "U8";
+    U16,  U16  => MatrixU16,  u16,  u16::zero(), "U16";
+    U32,  U32  => MatrixU32,  u32,  u32::zero(), "U32";
+    U64,  U64  => MatrixU64,  u64,  u64::zero(), "U64";
+    U128, U128 => MatrixU128, u128, u128::zero(), "U128";
+    F32,  F32  => MatrixF32,  F32,  F32::zero(), "F32";
+    F64,  F64  => MatrixF64,  F64,  F64::zero(), "F64";
   )
 }
 
@@ -265,18 +295,18 @@ fn impl_sub_fxn(lhs_value: Value, rhs_value: Value) -> Result<Box<dyn MechFuncti
   impl_binop_match_arms!(
     Sub,
     (lhs_value, rhs_value),
-    I8,   I8   => MatrixI8,   i8,   i8::zero();
-    I16,  I16  => MatrixI16,  i16,  i16::zero();
-    I32,  I32  => MatrixI32,  i32,  i32::zero();
-    I64,  I64  => MatrixI64,  i64,  i64::zero();
-    I128, I128 => MatrixI128, i128, i128::zero();
-    U8,   U8   => MatrixU8,   u8,   u8::zero();
-    U16,  U16  => MatrixU16,  u16,  u16::zero();
-    U32,  U32  => MatrixU32,  u32,  u32::zero();
-    U64,  U64  => MatrixU64,  u64,  u64::zero();
-    U128, U128 => MatrixU128, u128, u128::zero();
-    F32,  F32  => MatrixF32,  F32,  F32::zero();
-    F64,  F64  => MatrixF64,  F64,  F64::zero();
+    I8,   I8   => MatrixI8,   i8,   i8::zero(), "I8";
+    I16,  I16  => MatrixI16,  i16,  i16::zero(), "I16";
+    I32,  I32  => MatrixI32,  i32,  i32::zero(), "I32";
+    I64,  I64  => MatrixI64,  i64,  i64::zero(), "I64";
+    I128, I128 => MatrixI128, i128, i128::zero(), "I128";
+    U8,   U8   => MatrixU8,   u8,   u8::zero(), "U8";
+    U16,  U16  => MatrixU16,  u16,  u16::zero(), "U16";
+    U32,  U32  => MatrixU32,  u32,  u32::zero(), "U32";
+    U64,  U64  => MatrixU64,  u64,  u64::zero(), "U64";
+    U128, U128 => MatrixU128, u128, u128::zero(), "U128";
+    F32,  F32  => MatrixF32,  F32,  F32::zero(), "F32";
+    F64,  F64  => MatrixF64,  F64,  F64::zero(), "F64";
   )
 }
 
@@ -306,18 +336,18 @@ fn impl_mul_fxn(lhs_value: Value, rhs_value: Value) -> Result<Box<dyn MechFuncti
   impl_binop_match_arms!(
     Mul,
     (lhs_value, rhs_value),
-    I8,   I8   => MatrixI8,   i8,   i8::zero();
-    I16,  I16  => MatrixI16,  i16,  i16::zero();
-    I32,  I32  => MatrixI32,  i32,  i32::zero();
-    I64,  I64  => MatrixI64,  i64,  i64::zero();
-    I128, I128 => MatrixI128, i128, i128::zero();
-    U8,   U8   => MatrixU8,   u8,   u8::zero();
-    U16,  U16  => MatrixU16,  u16,  u16::zero();
-    U32,  U32  => MatrixU32,  u32,  u32::zero();
-    U64,  U64  => MatrixU64,  u64,  u64::zero();
-    U128, U128 => MatrixU128, u128, u128::zero();
-    F32,  F32  => MatrixF32,  F32,  F32::zero();
-    F64,  F64  => MatrixF64,  F64,  F64::zero();
+    I8,   I8   => MatrixI8,   i8,   i8::zero(), "I8";
+    I16,  I16  => MatrixI16,  i16,  i16::zero(), "I16";
+    I32,  I32  => MatrixI32,  i32,  i32::zero(), "I32";
+    I64,  I64  => MatrixI64,  i64,  i64::zero(), "I64";
+    I128, I128 => MatrixI128, i128, i128::zero(), "I128";
+    U8,   U8   => MatrixU8,   u8,   u8::zero(), "U8";
+    U16,  U16  => MatrixU16,  u16,  u16::zero(), "U16";
+    U32,  U32  => MatrixU32,  u32,  u32::zero(), "U32";
+    U64,  U64  => MatrixU64,  u64,  u64::zero(), "U64";
+    U128, U128 => MatrixU128, u128, u128::zero(), "U128";
+    F32,  F32  => MatrixF32,  F32,  F32::zero(), "F32";
+    F64,  F64  => MatrixF64,  F64,  F64::zero(), "F64";
   )
 }
 
@@ -356,18 +386,18 @@ fn impl_div_fxn(lhs_value: Value, rhs_value: Value) -> Result<Box<dyn MechFuncti
   impl_binop_match_arms!(
     Div,
     (lhs_value, rhs_value),
-    I8,   I8   => MatrixI8,   i8,   i8::zero();
-    I16,  I16  => MatrixI16,  i16,  i16::zero();
-    I32,  I32  => MatrixI32,  i32,  i32::zero();
-    I64,  I64  => MatrixI64,  i64,  i64::zero();
-    I128, I128 => MatrixI128, i128, i128::zero();
-    U8,   U8   => MatrixU8,   u8,   u8::zero();
-    U16,  U16  => MatrixU16,  u16,  u16::zero();
-    U32,  U32  => MatrixU32,  u32,  u32::zero();
-    U64,  U64  => MatrixU64,  u64,  u64::zero();
-    U128, U128 => MatrixU128, u128, u128::zero();
-    F32,  F32  => MatrixF32,  F32,  F32::zero();
-    F64,  F64  => MatrixF64,  F64,  F64::zero();
+    I8,   I8   => MatrixI8,   i8,   i8::zero(), "I8";
+    I16,  I16  => MatrixI16,  i16,  i16::zero(), "I16";
+    I32,  I32  => MatrixI32,  i32,  i32::zero(), "I32";
+    I64,  I64  => MatrixI64,  i64,  i64::zero(), "I64";
+    I128, I128 => MatrixI128, i128, i128::zero(), "I128";
+    U8,   U8   => MatrixU8,   u8,   u8::zero(), "U8";
+    U16,  U16  => MatrixU16,  u16,  u16::zero(), "U16";
+    U32,  U32  => MatrixU32,  u32,  u32::zero(), "U32";
+    U64,  U64  => MatrixU64,  u64,  u64::zero(), "U64";
+    U128, U128 => MatrixU128, u128, u128::zero(), "U128";
+    F32,  F32  => MatrixF32,  F32,  F32::zero(), "F32";
+    F64,  F64  => MatrixF64,  F64,  F64::zero(), "F64";
   )
 }
 
@@ -445,11 +475,11 @@ fn impl_exp_fxn(lhs_value: Value, rhs_value: Value) -> Result<Box<dyn MechFuncti
   impl_binop_match_arms!(
     Exp,
     (lhs_value, rhs_value),
-    U8,   U8   => MatrixU8,  u8,  u8::zero();
-    U16,  U16  => MatrixU16, u16, u16::zero();
-    U32,  U32  => MatrixU32, u32, u32::zero();
-    F32,  F32  => MatrixF32,  F32,  F32::zero();
-    F64,  F64  => MatrixF64,  F64,  F64::zero();
+    U8,   U8   => MatrixU8,  u8,  u8::zero(), "U8";
+    U16,  U16  => MatrixU16, u16, u16::zero(), "U16";
+    U32,  U32  => MatrixU32, u32, u32::zero(), "U32";
+    F32,  F32  => MatrixF32,  F32,  F32::zero(), "F32";
+    F64,  F64  => MatrixF64,  F64,  F64::zero(), "F64";
   )
 }
 
@@ -489,33 +519,48 @@ macro_rules! impl_neg_op {
     }};}
 
 impl_neg_op!(NegateS, T, neg_op);
+#[cfg(feature = "Matrix1")]
 impl_neg_op!(NegateM1, Matrix1<T>,neg_op);
+#[cfg(feature = "Matrix2")]
 impl_neg_op!(NegateM2, Matrix2<T>,neg_op);
+#[cfg(feature = "Matrix3")]
 impl_neg_op!(NegateM3, Matrix3<T>,neg_op);
+#[cfg(feature = "Matrix4")]
 impl_neg_op!(NegateM4, Matrix4<T>,neg_op);
+#[cfg(feature = "Matrix2x3")]
 impl_neg_op!(NegateM2x3, Matrix2x3<T>,neg_op);
+#[cfg(feature = "Matrix3x2")]
 impl_neg_op!(NegateM3x2, Matrix3x2<T>,neg_op);
-impl_neg_op!(NegateR2, RowVector2<T>,neg_op);
-impl_neg_op!(NegateR3, RowVector3<T>,neg_op);
-impl_neg_op!(NegateR4, RowVector4<T>,neg_op);     
-impl_neg_op!(NegateRD, RowDVector<T>,neg_vec_op);
-impl_neg_op!(NegateV2, Vector2<T>,neg_op);
-impl_neg_op!(NegateV3, Vector3<T>,neg_op);
-impl_neg_op!(NegateV4, Vector4<T>,neg_op);     
-impl_neg_op!(NegateVD, DVector<T>,neg_vec_op);
+#[cfg(feature = "MatrixD")]
 impl_neg_op!(NegateMD, DMatrix<T>,neg_vec_op);
+#[cfg(feature = "RowVector2")]
+impl_neg_op!(NegateR2, RowVector2<T>,neg_op);
+#[cfg(feature = "RowVector3")]
+impl_neg_op!(NegateR3, RowVector3<T>,neg_op);
+#[cfg(feature = "RowVector4")]
+impl_neg_op!(NegateR4, RowVector4<T>,neg_op);     
+#[cfg(feature = "RowVectorD")]
+impl_neg_op!(NegateRD, RowDVector<T>,neg_vec_op);
+#[cfg(feature = "Vector2")]
+impl_neg_op!(NegateV2, Vector2<T>,neg_op);
+#[cfg(feature = "Vector3")]
+impl_neg_op!(NegateV3, Vector3<T>,neg_op);
+#[cfg(feature = "Vector4")]
+impl_neg_op!(NegateV4, Vector4<T>,neg_op);     
+#[cfg(feature = "VectorD")]
+impl_neg_op!(NegateVD, DVector<T>,neg_vec_op);
   
 fn impl_neg_fxn(lhs_value: Value) -> Result<Box<dyn MechFunction>, MechError> {
   impl_urnop_match_arms!(
     Negate,
     (lhs_value),
-    I8 => MatrixI8, i8, i8::zero();
-    I16 => MatrixI16, i16, i16::zero();
-    I32 => MatrixI32, i32, i32::zero();
-    I64 => MatrixI64, i64, i64::zero();
-    I128 => MatrixI128, i128, i128::zero();
-    F32 => MatrixF32, F32, F32::zero();
-    F64 => MatrixF64, F64, F64::zero();
+    I8 => MatrixI8, i8, i8::zero(), "I8";
+    I16 => MatrixI16, i16, i16::zero(), "I16";
+    I32 => MatrixI32, i32, i32::zero(), "I32";
+    I64 => MatrixI64, i64, i64::zero(), "I64";
+    I128 => MatrixI128, i128, i128::zero(), "I128";
+    F32 => MatrixF32, F32, F32::zero(), "F32";
+    F64 => MatrixF64, F64, F64::zero(), "F64";
   )
 }
 
