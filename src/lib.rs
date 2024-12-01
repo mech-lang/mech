b5c9dbab72e19c6c475474a3896796a5202310ad
cmontella <cmontella@live.com>
2024-12-01 18:12:58 -0500

    export exp

--- src/lib.rs
+++ src/lib.rs
@@ -26,6 +26,7 @@ pub mod add;
 pub mod sub;
 pub mod mul;
 pub mod div;
+pub mod exp;
 pub mod negate;
 
 pub use self::cos::*;
@@ -34,6 +35,7 @@ pub use self::add::*;
 pub use self::sub::*;
 pub use self::mul::*;
 pub use self::div::*;
+pub use self::exp::*;
 pub use self::negate::*;
 
 // ----------------------------------------------------------------------------
