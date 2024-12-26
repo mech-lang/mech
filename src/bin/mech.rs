861c6dbc3994785cf1fd6defe49c929a8e03bc12
cmontella <cmontella@live.com>
2024-12-26 13:20:51 -0500

    parse repl command arguments

--- src/bin/mech.rs
+++ src/bin/mech.rs
@@ -9,6 +9,14 @@ use std::time::Instant;
 use std::fs;
 use std::env;
 use std::io;
+use nom::{
+  IResult,
+  bytes::complete::tag,
+  branch::alt,
+  bytes::complete::take_while,
+  combinator::opt,
+  character::complete::{space0,space1,digit1},
+};
 use colored::*;
 use std::io::{Write, BufReader, BufWriter, stdout};
 use crossterm::{
@@ -25,7 +33,99 @@ use tabled::{
 use serde_json;
 use std::panic;
 
+#[derive(Debug)]
+enum ReplCommand {
+  Help,
+  Quit,
+  Symbols(Option<String>),
+  Plan,
+  Whos(Option<String>),
+  Clear(Option<String>),
+  Clc,
+  Load(String),
+  Save(String),
+  Step(Option<usize>),
+  Unknown(String),  
+}
+
 const VERSION: &str = env!("CARGO_PKG_VERSION");
+
+fn parse_repl_command(input: &str) -> IResult<&str, ReplCommand> {
+  let (input, _) = tag(":")(input)?;
+  let (input, command) = alt((
+    step_rpl,
+    save_rpl,
+    help_rpl,
+    quit_rpl,
+    symbols_rpl,
+    plan_rpl,
+    whos_rpl,
+    clear_rpl,
+    clc_rpl,
+    load_rpl,
+  ))(input)?;
+  Ok((input, command))
+}
+
+fn help_rpl(input: &str) -> IResult<&str, ReplCommand> {
+  let (input, _) = alt((tag("h"), tag("help")))(input)?;
+  Ok((input, ReplCommand::Help))
+}
+
+fn quit_rpl(input: &str) -> IResult<&str, ReplCommand> {
+  let (input, _) = alt((tag("q"), tag("quit"), tag("exit")))(input)?;
+  Ok((input, ReplCommand::Quit))
+}
+
+fn symbols_rpl(input: &str) -> IResult<&str, ReplCommand> {
+  let (input, _) = alt((tag("s"), tag("symbols")))(input)?;
+  let (input, _) = space0(input)?;
+  let (input, name) = opt(take_while(|c: char| c.is_alphanumeric()))(input)?;
+  Ok((input, ReplCommand::Symbols(name.map(|s| s.to_string()))))
+}
+
+fn plan_rpl(input: &str) -> IResult<&str, ReplCommand> {
+  let (input, _) = alt((tag("p"), tag("plan")))(input)?;
+  Ok((input, ReplCommand::Plan))
+}
+
+fn whos_rpl(input: &str) -> IResult<&str, ReplCommand> {
+  let (input, _) = alt((tag("w"), tag("whos")))(input)?;
+  let (input, _) = space0(input)?;
+  let (input, name) = opt(take_while(|c: char| c.is_alphanumeric()))(input)?;
+  Ok((input, ReplCommand::Whos(name.map(|s| s.to_string()))))
+}
+
+fn clear_rpl(input: &str) -> IResult<&str, ReplCommand> {
+  let (input, _) = tag("clear")(input)?;
+  Ok((input, ReplCommand::Clear(None)))
+}
+
+fn clc_rpl(input: &str) -> IResult<&str, ReplCommand> {
+  let (input, _) = tag("clc")(input)?;
+  Ok((input, ReplCommand::Clc))
+}
+
+fn load_rpl(input: &str) -> IResult<&str, ReplCommand> {
+  let (input, _) = tag("load")(input)?;
+  let (input, _) = space1(input)?;
+  Ok((input, ReplCommand::Load(input.to_string())))
+}
+
+fn save_rpl(input: &str) -> IResult<&str, ReplCommand> {
+  let (input, _) = tag("save")(input)?;
+  let (input, _) = space1(input)?;
+  let (input, path) = take_while(|c: char| c.is_alphanumeric())(input)?;
+  Ok((input, ReplCommand::Save(path.to_string())))
+}
+
+fn step_rpl(input: &str) -> IResult<&str, ReplCommand> {
+  let (input, _) = tag("step")(input)?;
+  let (input, _) = space0(input)?;
+  let (input, count) = opt(digit1)(input)?;
+  Ok((input, ReplCommand::Step(count.map(|s| s.parse().unwrap()))))
+}
+
 
 fn main() -> Result<(), MechError> {
   /*panic::set_hook(Box::new(|panic_info| {
@@ -151,22 +251,29 @@ fn main() -> Result<(), MechError> {
     io::stdin().read_line(&mut input).unwrap();
 
     if input.chars().nth(0) == Some(':') {
-      // Treat as command 
-      match input.as_str().trim() {
-        ":h" | ":help" => todo!(),
-        ":q" | ":quit" | ":exit" => break 'REPL,
-        ":s" | ":symbols" => println!("{}", intrp.symbols.borrow().pretty_print()),
-        ":p" | ":plan" => println!("{}", pretty_print_plan(&intrp)),
-        ":w" | ":whos" => println!("{}",whos(&intrp)),
-        ":clear" => {
+      // loop
+      let repl_command = parse_repl_command(&input.as_str());
+
+      match repl_command {
+        Ok((_, ReplCommand::Help)) => println!("Mech REPL Commands: \n  :help, :h - Display this help message\n  :quit, :q, :exit - Quit the REPL\n  :symbols, :s - Display all symbols\n  :plan, :p - Display the plan\n  :whos, :w - Display all symbols\n  :clear - Clear the screen\n  :clc - Clear the screen\n  :load - Load a file\n  :save - Save a file\n  :step - Step through the plan"),
+        Ok((_, ReplCommand::Quit)) => break 'REPL,
+        Ok((_, ReplCommand::Symbols(name))) => println!("{}", intrp.symbols.borrow().pretty_print()),
+        Ok((_, ReplCommand::Plan)) => println!("{}", pretty_print_plan(&intrp)),
+        Ok((_, ReplCommand::Whos(name))) => println!("{}",whos(&intrp)),
+        Ok((_, ReplCommand::Clear(name))) => {
           // Drop the old interpreter replace it with a new one
           intrp = Interpreter::new();
-          // Clear the screen.
-          clc();
         }
-        ":clc" => clc(),
-        ":load" => {
-          /*let s = fs::read_to_string(&mech_paths).unwrap();
+        Ok((_, ReplCommand::Clc)) => clc(),
+        Ok((_, ReplCommand::Load(path))) => {
+          println!("Loading: {:?}", path.trim());
+          let s = match fs::read_to_string(&path.trim()) {
+            Ok(s) => s,
+            Err(err) => {
+              println!("Error reading file: {:?} {:?}", err, path);
+              continue;
+            }
+          };
           let now = Instant::now();
           let parse_result = parser::parse(&s);
           let elapsed_time = now.elapsed();
@@ -190,14 +297,20 @@ fn main() -> Result<(), MechError> {
                 panic!("Unexpected error type");
               }
             }
-          }*/
+          }
         }
-        ":step" => {
+        Ok((_, ReplCommand::Step(count))) => {
+          let n = match count {
+            Some(n) => n,
+            None => 1,
+          };
           let plan = intrp.plan.as_ptr();
           let plan_brrw = unsafe { &*plan };
           let now = Instant::now();
-          for fxn in plan_brrw {
-            fxn.solve();
+          for i in 0..n {
+            for fxn in plan_brrw {
+              fxn.solve();
+            }
           }
           let elapsed_time = now.elapsed();
           let cycle_duration = elapsed_time.as_nanos() as f64;
@@ -206,12 +319,12 @@ fn main() -> Result<(), MechError> {
         x => {
           let err = MechError{
             file: file!().to_string(),  
-            tokens: vec![], 
-            msg: "".to_string(), 
-            id: line!(), 
-            kind: MechErrorKind::UnknownCommand(x.to_string()) 
+            tokens: vec![],
+            msg: "".to_string(),
+            id: line!(),
+            kind: MechErrorKind::UnknownCommand(input.clone()),
           };
-          println!("{:?}",err);
+          println!("{:?}",x);
         }
       }
     } else if input.trim() == "" {
