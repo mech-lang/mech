42a4738be02b292829ba3a39b61f1de8c5ef0586
cmontella <cmontella@live.com>
2025-05-21 23:23:02 -0400

    remove unused imports

--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,6 @@
 extern crate nalgebra as na;
 extern crate mech_core;
 
-use na::*;
-use num_traits::*;
-use std::ops::*;
-use std::fmt::Debug;
 use mech_core::matrix::Matrix;
 
 pub mod n_choose_k;
