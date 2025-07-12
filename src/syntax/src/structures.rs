5a84f82d2d80420ea2cdbd4f347541c60d2a12be
cmontella <cmontella@live.com>
2025-07-12 17:58:06 -0400

    Reoganize structures

--- src/syntax/src/structures.rs
+++ src/syntax/src/structures.rs
@@ -74,66 +74,24 @@ pub fn structure(input: ParseString) -> ParseResult<Structure> {
   }
 }
 
-// tuple := "(", list0(",", expression), ")" ;
-pub fn tuple(input: ParseString) -> ParseResult<Tuple> {
-  let (input, _) = left_parenthesis(input)?;
-  let (input, _) = whitespace0(input)?;
-  let (input, exprs) = separated_list0(list_separator, expression)(input)?;
-  let (input, _) = whitespace0(input)?;
-  let (input, _) = right_parenthesis(input)?;
-  Ok((input, Tuple{elements: exprs}))
-}
+// Matrix
+// ----------------------------------------------------------------------------
 
-// atom := "`", identifier ;
-pub fn atom(input: ParseString) -> ParseResult<Atom> {
-  let (input, _) = grave(input)?;
-  let (input, name) = identifier(input)?;
-  Ok((input, Atom{name}))
-}
-
-// tuple_struct = atom, "(", expression, ")" ;
-pub fn tuple_struct(input: ParseString) -> ParseResult<TupleStruct> {
-  let (input, _) = grave(input)?;
-  let (input, name) = identifier(input)?;
-  let (input, _) = left_parenthesis(input)?;
+// matrix := matrix_start, (box_drawing_char | whitespace)*, matrix_row*, box_drawing_char*, matrix_end ;
+pub fn matrix(input: ParseString) -> ParseResult<Matrix> {
+  let msg = "Expects right bracket ']' to finish the matrix";
+  let (input, (_, r)) = range(matrix_start)(input)?;
+  let (input, _) = many0(alt((box_drawing_char,whitespace)))(input)?;
+  let (input, rows) = many0(matrix_row)(input)?;
+  let (input, _) = many0(alt((box_drawing_char,whitespace)))(input)?;
   let (input, _) = whitespace0(input)?;
-  let (input, value) = expression(input)?;
-  let (input, _) = whitespace0(input)?;
-  let (input, _) = right_parenthesis(input)?;
-  Ok((input, TupleStruct{name, value: Box::new(value)}))
-}
-
-// binding := identifier, kind_annotation?, colon, expression, ","? ;
-
-pub fn binding(input: ParseString) -> ParseResult<Binding> {
-  let msg1 = "Unexpected space before colon ':'";
-  let msg2 = "Expects a value";
-  let msg3 = "Expects whitespace or comma followed by whitespace";
-  let msg4 = "Expects whitespace";
-  let (input, _) = whitespace0(input)?;
-  let (input, name) = identifier(input)?;
-  let (input, kind) = opt(kind_annotation)(input)?;
-  let (input, _) = whitespace0(input)?;
-  let (input, _) = colon(input)?;
-  let (input, _) = whitespace0(input)?;
-  let (input, value) = label!(expression, msg2)(input)?;
-  let (input, _) = whitespace0(input)?;
-  let (input, _) = opt(comma)(input)?;
-  let (input, _) = whitespace0(input)?;
-  Ok((input, Binding{name, kind, value}))
-}
-
-// table_column := (space | tab)*, expression, ((space | tab)*, ("," | table_separator)?, (space | tab)*) ;
-pub fn table_column(input: ParseString) -> ParseResult<TableColumn> {
-  let (input, _) = many0(space_tab)(input)?;
-  let (input, element) = match expression(input) {
-    Ok(result) => result,
+  let (input, _) = match label!(matrix_end, msg, r)(input) {
+    Ok(k) => k,
     Err(err) => {
       return Err(err);
     }
   };
-  let (input, _) = nom_tuple((many0(space_tab),opt(alt((comma,table_separator))), many0(space_tab)))(input)?;
-  Ok((input, TableColumn{element}))
+  Ok((input, Matrix{rows}))
 }
 
 // matrix_column := (space | tab)*, expression, ((space | tab)*, ("," | table_separator)?, (space | tab)*) ;
@@ -147,18 +105,6 @@ pub fn matrix_column(input: ParseString) -> ParseResult<MatrixColumn> {
   };
   let (input, _) = nom_tuple((many0(space_tab),opt(alt((comma,table_separator))), many0(space_tab)))(input)?;
   Ok((input, MatrixColumn{element}))
-}
-
-// table-row := bar, list1((space | tab)*, expression), (space | tab)*, bar, new-line ;
-pub fn table_row(input: ParseString) -> ParseResult<TableRow> {
-  let (input, _) = whitespace0(input)?;
-  let (input, _) = bar(input)?;
-  let (input, row) = many1(nom_tuple((many0(space_tab), expression)))(input)?;
-  let (input, _) = many0(space_tab)(input)?;
-  let (input, _) = bar(input)?;
-  let (input, _) = new_line(input)?;
-  let row = row.into_iter().map(|(_,tkn)| TableColumn{element:tkn}).collect();
-  Ok((input, TableRow{columns: row}))
 }
 
 // matrix_row := table_separator?, (space | tab)*, matrix_column+, semicolon?, new_line?, (box_drawing_char+, new_line)? ;
@@ -176,6 +122,31 @@ pub fn matrix_row(input: ParseString) -> ParseResult<MatrixRow> {
   Ok((input, MatrixRow{columns}))
 }
 
+// matrix_start := box_tl_round | box_tl | left_bracket ;
+pub fn matrix_start(input: ParseString) -> ParseResult<Token> {
+  alt((box_tl_round, box_tl, box_tl_bold, left_bracket))(input)
+}
+
+// matrix_end := box_br_round | box_br | right_bracket ;
+pub fn matrix_end(input: ParseString) -> ParseResult<Token> {
+  let result = alt((box_br_round, box_br, box_br_bold, right_bracket))(input);
+  result
+}
+
+// Table
+// ----------------------------------------------------------------------------
+
+// table := table_start, (box_drawing_char | whitespace)*, table_header, (box_drawing_char | whitespace)*, table_row+, box_drawing_char*, whitespace*, table_end ;
+pub fn table(input: ParseString) -> ParseResult<Table> {
+  let msg = "Expects right bracket '}' to finish the table";
+  let (input, (_, r)) = range(table_start)(input)?;
+  let (input, _) = many0(alt((box_drawing_char,whitespace)))(input)?;
+  let (input, header) = table_header(input)?;
+  let (input, _) = many0(alt((box_drawing_char,whitespace)))(input)?;
+  let (input, rows) = many1(table_row)(input)?;
+  Ok((input, Table{header,rows}))
+}
+
 // table_header := list1(space_tab+, field), (space | tab)*, (bar| box_vert), whitespace* ;
 pub fn table_header(input: ParseString) -> ParseResult<Vec<Field>> {
   let (input, fields) = separated_list1(many1(space_tab),field)(input)?;
@@ -183,6 +154,31 @@ pub fn table_header(input: ParseString) -> ParseResult<Vec<Field>> {
   let (input, _) = alt((bar,box_vert,box_vert_bold))(input)?;
   let (input, _) = whitespace0(input)?;
   Ok((input, fields))
+}
+
+// table-row := bar, list1((space | tab)*, expression), (space | tab)*, bar, new-line ;
+pub fn table_row(input: ParseString) -> ParseResult<TableRow> {
+  let (input, _) = whitespace0(input)?;
+  let (input, _) = bar(input)?;
+  let (input, row) = many1(nom_tuple((many0(space_tab), expression)))(input)?;
+  let (input, _) = many0(space_tab)(input)?;
+  let (input, _) = bar(input)?;
+  let (input, _) = new_line(input)?;
+  let row = row.into_iter().map(|(_,tkn)| TableColumn{element:tkn}).collect();
+  Ok((input, TableRow{columns: row}))
+}
+
+// table_column := (space | tab)*, expression, ((space | tab)*, ("," | table_separator)?, (space | tab)*) ;
+pub fn table_column(input: ParseString) -> ParseResult<TableColumn> {
+  let (input, _) = many0(space_tab)(input)?;
+  let (input, element) = match expression(input) {
+    Ok(result) => result,
+    Err(err) => {
+      return Err(err);
+    }
+  };
+  let (input, _) = nom_tuple((many0(space_tab),opt(alt((comma,table_separator))), many0(space_tab)))(input)?;
+  Ok((input, TableColumn{element}))
 }
 
 // field := identifier, kind_annotation? ;
@@ -200,17 +196,6 @@ pub fn box_drawing_char(input: ParseString) -> ParseResult<Token> {
 // box_drawing_emoji := box_tl | box_br | box_bl | box_tr | box_tl_round | box_br_round | box_tr_round | box_bl_round | box_vert | box_cross | box_horz | box_t_left | box_t_right | box_t_top | box_t_bottom ;
 pub fn box_drawing_emoji(input: ParseString) -> ParseResult<Token> {
   alt((box_tl, box_bl, box_tr, box_tl_bold, box_bl_bold, box_tr_bold, box_tl_round, box_br_round, box_tr_round, box_bl_round, box_vert, box_cross, box_horz, box_t_left, box_t_right, box_t_top, box_t_bottom))(input)
-}
-
-// matrix_start := box_tl_round | box_tl | left_bracket ;
-pub fn matrix_start(input: ParseString) -> ParseResult<Token> {
-  alt((box_tl_round, box_tl, box_tl_bold, left_bracket))(input)
-}
-
-// matrix_end := box_br_round | box_br | right_bracket ;
-pub fn matrix_end(input: ParseString) -> ParseResult<Token> {
-  let result = alt((box_br_round, box_br, box_br_bold, right_bracket))(input);
-  result
 }
 
 // table_start := box_tl_round | box_tl | left_brace ;
@@ -235,33 +220,8 @@ pub fn table_horz(input: ParseString) -> ParseResult<Token> {
   Ok((input, token))
 }
 
-// matrix := matrix_start, (box_drawing_char | whitespace)*, matrix_row*, box_drawing_char*, matrix_end ;
-pub fn matrix(input: ParseString) -> ParseResult<Matrix> {
-  let msg = "Expects right bracket ']' to finish the matrix";
-  let (input, (_, r)) = range(matrix_start)(input)?;
-  let (input, _) = many0(alt((box_drawing_char,whitespace)))(input)?;
-  let (input, rows) = many0(matrix_row)(input)?;
-  let (input, _) = many0(alt((box_drawing_char,whitespace)))(input)?;
-  let (input, _) = whitespace0(input)?;
-  let (input, _) = match label!(matrix_end, msg, r)(input) {
-    Ok(k) => k,
-    Err(err) => {
-      return Err(err);
-    }
-  };
-  Ok((input, Matrix{rows}))
-}
-
-// table := table_start, (box_drawing_char | whitespace)*, table_header, (box_drawing_char | whitespace)*, table_row+, box_drawing_char*, whitespace*, table_end ;
-pub fn table(input: ParseString) -> ParseResult<Table> {
-  let msg = "Expects right bracket '}' to finish the table";
-  let (input, (_, r)) = range(table_start)(input)?;
-  let (input, _) = many0(alt((box_drawing_char,whitespace)))(input)?;
-  let (input, header) = table_header(input)?;
-  let (input, _) = many0(alt((box_drawing_char,whitespace)))(input)?;
-  let (input, rows) = many1(table_row)(input)?;
-  Ok((input, Table{header,rows}))
-}
+// Map
+// ----------------------------------------------------------------------------
 
 // empty_table := table_start, whitespace*, table_end ;
 pub fn empty_map(input: ParseString) -> ParseResult<Map> {
@@ -271,27 +231,6 @@ pub fn empty_map(input: ParseString) -> ParseResult<Map> {
   let (input, _) = whitespace0(input)?;
   let (input, _) = right_brace(input)?;
   Ok((input, Map{elements: vec![]}))
-}
-
-// empty_set := table_start, whitespace*, empty, whitespace*, table_end ;
-pub fn empty_set(input: ParseString) -> ParseResult<Set> {
-  let (input, _) = left_brace(input)?;
-  let (input, _) = whitespace0(input)?;
-  let (input, _) = empty(input)?;
-  let (input, _) = whitespace0(input)?;
-  let (input, _) = right_brace(input)?;
-  Ok((input,  Set{elements: vec![]}))
-}
-
-// record := table_start, whitespace*, binding+, whitespace*, table_end ;
-pub fn record(input: ParseString) -> ParseResult<Record> {
-  let msg = "Expects right bracket ']' to terminate inline table";
-  let (input, (_, r)) = range(table_start)(input)?;
-  let (input, _) = whitespace0(input)?;
-  let (input, bindings) = many1(binding)(input)?;
-  let (input, _) = whitespace0(input)?;
-  let (input, _) = label!(table_end, msg, r)(input)?;
-  Ok((input, Record{bindings}))
 }
 
 // map := "{", whitespace*, mapping*, whitespace*, "}" ;
@@ -323,6 +262,53 @@ pub fn mapping(input: ParseString) -> ParseResult<Mapping> {
   Ok((input, Mapping{key, value}))
 }
 
+
+// Record
+// ----------------------------------------------------------------------------
+
+// record := table_start, whitespace*, binding+, whitespace*, table_end ;
+pub fn record(input: ParseString) -> ParseResult<Record> {
+  let msg = "Expects right bracket ']' to terminate inline table";
+  let (input, (_, r)) = range(table_start)(input)?;
+  let (input, _) = whitespace0(input)?;
+  let (input, bindings) = many1(binding)(input)?;
+  let (input, _) = whitespace0(input)?;
+  let (input, _) = label!(table_end, msg, r)(input)?;
+  Ok((input, Record{bindings}))
+}
+
+// binding := identifier, kind_annotation?, colon, expression, ","? ;
+pub fn binding(input: ParseString) -> ParseResult<Binding> {
+  let msg1 = "Unexpected space before colon ':'";
+  let msg2 = "Expects a value";
+  let msg3 = "Expects whitespace or comma followed by whitespace";
+  let msg4 = "Expects whitespace";
+  let (input, _) = whitespace0(input)?;
+  let (input, name) = identifier(input)?;
+  let (input, kind) = opt(kind_annotation)(input)?;
+  let (input, _) = whitespace0(input)?;
+  let (input, _) = colon(input)?;
+  let (input, _) = whitespace0(input)?;
+  let (input, value) = label!(expression, msg2)(input)?;
+  let (input, _) = whitespace0(input)?;
+  let (input, _) = opt(comma)(input)?;
+  let (input, _) = whitespace0(input)?;
+  Ok((input, Binding{name, kind, value}))
+}
+
+// Set
+// ----------------------------------------------------------------------------
+
+// empty_set := table_start, whitespace*, empty, whitespace*, table_end ;
+pub fn empty_set(input: ParseString) -> ParseResult<Set> {
+  let (input, _) = left_brace(input)?;
+  let (input, _) = whitespace0(input)?;
+  let (input, _) = empty(input)?;
+  let (input, _) = whitespace0(input)?;
+  let (input, _) = right_brace(input)?;
+  Ok((input,  Set{elements: vec![]}))
+}
+
 // set := "{", whitespace*, list0(("," | whitespace+), expression), whitespace*, "}" ;
 pub fn set(input: ParseString) -> ParseResult<Set> {
   let msg = "Expects right bracket '}' to terminate inline table";
@@ -332,4 +318,36 @@ pub fn set(input: ParseString) -> ParseResult<Set> {
   let (input, _) = whitespace0(input)?;
   let (input, _) = label!(right_brace, msg, r)(input)?;
   Ok((input, Set{elements}))
+}
+
+// Tuple
+// ----------------------------------------------------------------------------
+
+// tuple := "(", list0(",", expression), ")" ;
+pub fn tuple(input: ParseString) -> ParseResult<Tuple> {
+  let (input, _) = left_parenthesis(input)?;
+  let (input, _) = whitespace0(input)?;
+  let (input, exprs) = separated_list0(list_separator, expression)(input)?;
+  let (input, _) = whitespace0(input)?;
+  let (input, _) = right_parenthesis(input)?;
+  Ok((input, Tuple{elements: exprs}))
+}
+
+// tuple_struct = atom, "(", expression, ")" ;
+pub fn tuple_struct(input: ParseString) -> ParseResult<TupleStruct> {
+  let (input, _) = grave(input)?;
+  let (input, name) = identifier(input)?;
+  let (input, _) = left_parenthesis(input)?;
+  let (input, _) = whitespace0(input)?;
+  let (input, value) = expression(input)?;
+  let (input, _) = whitespace0(input)?;
+  let (input, _) = right_parenthesis(input)?;
+  Ok((input, TupleStruct{name, value: Box::new(value)}))
+}
+
+// atom := "`", identifier ;
+pub fn atom(input: ParseString) -> ParseResult<Atom> {
+  let (input, _) = grave(input)?;
+  let (input, name) = identifier(input)?;
+  Ok((input, Atom{name}))
 }
