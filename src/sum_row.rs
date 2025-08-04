2dbe7d287507fe69eb90c3828a9998a7ae584b3a
cmontella <cmontella@live.com>
2025-08-04 17:21:35 -0400

    bump version

--- src/sum_row.rs
+++ src/sum_row.rs
@@ -87,17 +87,17 @@ macro_rules! sum_row_op {
     impl_stats_sum_row_match_arms!(
       lhs_value,
       I8   => MatrixI8,   i8,   i8::zero(), "I8";
-      I16  => MatrixI16,  i16,  i16::zero(), "I16";
-      I32  => MatrixI32,  i32,  i32::zero(), "I32";
-      I64  => MatrixI64,  i64,  i64::zero(), "I64";
-      I128 => MatrixI128, i128, i128::zero(), "I128";
-      U8   => MatrixU8,   u8,   u8::zero(), "U8";
-      U16  => MatrixU16,  u16,  u16::zero(), "U16";
-      U32  => MatrixU32,  u32,  u32::zero(), "U32";
-      U64  => MatrixU64,  u64,  u64::zero(), "U64";
-      U128 => MatrixU128, u128, u128::zero(), "U128";
-      F32  => MatrixF32,  F32,  F32::zero(), "F32";
-      F64  => MatrixF64,  F64,  F64::zero(), "F64";
+      //I16  => MatrixI16,  i16,  i16::zero(), "I16";
+      //I32  => MatrixI32,  i32,  i32::zero(), "I32";
+      //I64  => MatrixI64,  i64,  i64::zero(), "I64";
+      //I128 => MatrixI128, i128, i128::zero(), "I128";
+      //U8   => MatrixU8,   u8,   u8::zero(), "U8";
+      //U16  => MatrixU16,  u16,  u16::zero(), "U16";
+      //U32  => MatrixU32,  u32,  u32::zero(), "U32";
+      //U64  => MatrixU64,  u64,  u64::zero(), "U64";
+      //U128 => MatrixU128, u128, u128::zero(), "U128";
+      //F32  => MatrixF32,  F32,  F32::zero(), "F32";
+      //F64  => MatrixF64,  F64,  F64::zero(), "F64";
     )
   }
     
