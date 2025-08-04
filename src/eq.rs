abfa45c3ee2fb65217e5c715eda95f80b72f8908
cmontella <cmontella@live.com>
2025-08-04 16:25:12 -0400

    Switch crate to dylib

---
+++ src/eq.rs
@@ -0,0 +1,116 @@
+use crate::*;
+use mech_core::*;
+
+// Equal ---------------------------------------------------------------
+
+macro_rules! eq_scalar_lhs_op {
+  ($lhs:expr, $rhs:expr, $out:expr) => {
+    unsafe {
+      for i in 0..(*$lhs).len() {
+        (&mut (*$out))[i] = (&(*$lhs))[i] == (*$rhs);
+      }}};}
+
+macro_rules! eq_scalar_rhs_op {
+  ($lhs:expr, $rhs:expr, $out:expr) => {
+    unsafe {
+      for i in 0..(*$rhs).len() {
+        (&mut (*$out))[i] = (*$lhs) != (&(*$rhs))[i];
+      }}};}
+
+
+macro_rules! eq_vec_op {
+  ($lhs:expr, $rhs:expr, $out:expr) => {
+    unsafe {
+      for i in 0..(*$lhs).len() {
+        (&mut (*$out))[i] = (&(*$lhs))[i] == (&(*$rhs))[i];
+      }}};}
+
+macro_rules! eq_op {
+  ($lhs:expr, $rhs:expr, $out:expr) => {
+    unsafe {
+      (*$out) = (*$lhs) == (*$rhs);
+    }};}
+
+macro_rules! eq_mat_vec_op {
+  ($lhs:expr, $rhs:expr, $out:expr) => {
+    unsafe {
+      let mut out_deref = &mut (*$out);
+      let lhs_deref = &(*$lhs);
+      let rhs_deref = &(*$rhs);
+      for (mut col, lhs_col) in out_deref.column_iter_mut().zip(lhs_deref.column_iter()) {
+        for i in 0..col.len() {
+          col[i] = lhs_col[i] == rhs_deref[i];
+        }
+      }
+    }
+  };}   
+      
+macro_rules! eq_vec_mat_op {
+  ($lhs:expr, $rhs:expr, $out:expr) => {
+      unsafe {
+        let mut out_deref = &mut (*$out);
+        let lhs_deref = &(*$lhs);
+        let rhs_deref = &(*$rhs);
+        for (mut col, rhs_col) in out_deref.column_iter_mut().zip(rhs_deref.column_iter()) {
+          for i in 0..col.len() {
+            col[i] = lhs_deref[i] == rhs_col[i];
+          }
+        }
+      }
+  };}
+  
+macro_rules! eq_mat_row_op {
+  ($lhs:expr, $rhs:expr, $out:expr) => {
+      unsafe {
+      let mut out_deref = &mut (*$out);
+      let lhs_deref = &(*$lhs);
+      let rhs_deref = &(*$rhs);
+      for (mut row, lhs_row) in out_deref.row_iter_mut().zip(lhs_deref.row_iter()) {
+          for i in 0..row.len() {
+          row[i] = lhs_row[i] == rhs_deref[i];
+          }
+      }
+      }
+  };}
+
+macro_rules! eq_row_mat_op {
+  ($lhs:expr, $rhs:expr, $out:expr) => {
+      unsafe {
+      let mut out_deref = &mut (*$out);
+      let lhs_deref = &(*$lhs);
+      let rhs_deref = &(*$rhs);
+      for (mut row, rhs_row) in out_deref.row_iter_mut().zip(rhs_deref.row_iter()) {
+          for i in 0..row.len() {
+          row[i] = lhs_deref[i] == rhs_row[i];
+          }
+      }
+      }
+  };}
+
+impl_compare_fxns_bool!(EQ);
+
+fn impl_eq_fxn(lhs_value: Value, rhs_value: Value) -> Result<Box<dyn MechFunction>, MechError> {
+  impl_binop_match_arms!(
+    EQ,
+    (lhs_value, rhs_value),
+    Bool, Bool => MatrixBool, bool, false, "Bool";
+    I8,   I8   => MatrixI8,   i8,   false, "I8";
+    I16,  I16  => MatrixI16,  i16,  false, "I16";
+    I32,  I32  => MatrixI32,  i32,  false, "I32";
+    I64,  I64  => MatrixI64,  i64,  false, "I64";
+    I128, I128 => MatrixI128, i128, false, "I128";
+    U8,   U8   => MatrixU8,   u8,   false, "U8";
+    U16,  U16  => MatrixU16,  u16,  false, "U16";
+    U32,  U32  => MatrixU32,  u32,  false, "U32";
+    U64,  U64  => MatrixU64,  u64,  false, "U64";
+    U128, U128 => MatrixU128, u128, false, "U128";
+    F32,  F32  => MatrixF32,  F32,  false, "F32";
+    F64,  F64  => MatrixF64,  F64,  false, "F64";
+    String,  String  => MatrixString,  String,  false, "String";
+    RationalNumber,  RationalNumber  => MatrixRationalNumber,  RationalNumber,  false, "RationalNumber";
+    ComplexNumber,  ComplexNumber  => MatrixComplexNumber,  ComplexNumber,  false, "ComplexNumber";
+  )
+}
+
+impl_mech_binop_fxn!(CompareEqual,impl_eq_fxn);
+  
---
+++ src/lib.rs
@@ -0,0 +1,68 @@
+#![no_main]
+#![allow(warnings)]
+#[macro_use]
+extern crate mech_core;
+extern crate nalgebra as na;
+extern crate paste;
+
+use mech_core::*;
+use na::{Vector3, DVector, Vector2, Vector4, RowDVector, Matrix1, Matrix3, Matrix4, RowVector3, RowVector4, RowVector2, DMatrix, Rotation3, Matrix2x3, Matrix3x2, Matrix6, Matrix2};
+use paste::paste;
+use mech_core::matrix::Matrix;
+
+pub mod gt;
+pub mod lt;
+pub mod lte;
+pub mod gte;
+pub mod eq;
+pub mod neq;
+
+pub use self::gt::*;
+pub use self::lt::*;
+pub use self::lte::*;
+pub use self::gte::*;
+pub use self::eq::*;
+pub use self::neq::*;
+
+// ----------------------------------------------------------------------------
+// Compare Library
+// ----------------------------------------------------------------------------
+
+#[macro_export]
+macro_rules! impl_compare_binop {
+  ($struct_name:ident, $arg1_type:ty, $arg2_type:ty, $out_type:ty, $op:ident) => {
+    #[derive(Debug)]
+    struct $struct_name<T> {
+    lhs: Ref<$arg1_type>,
+    rhs: Ref<$arg2_type>,
+    out: Ref<$out_type>,
+    }
+    impl<T> MechFunction for $struct_name<T>
+    where
+    T: std::fmt::Debug + Clone + Sync + Send + 'static + 
+    PartialEq + PartialOrd,
+    Ref<$out_type>: ToValue
+    {
+    fn solve(&self) {
+      let lhs_ptr = self.lhs.as_ptr();
+      let rhs_ptr = self.rhs.as_ptr();
+      let out_ptr = self.out.as_ptr();
+      $op!(lhs_ptr,rhs_ptr,out_ptr);
+    }
+    fn out(&self) -> Value { self.out.to_value() }
+    fn to_string(&self) -> String { format!("{:#?}", self) }
+    }};}
+
+#[macro_export]
+macro_rules! impl_compare_fxns {
+  ($lib:ident) => {
+    impl_fxns!($lib,T,bool,impl_compare_binop);
+  }
+}
+
+#[macro_export]
+macro_rules! impl_compare_fxns_bool {
+  ($lib:ident) => {
+    impl_fxns!($lib,T,bool,impl_compare_binop);
+  }
+}
