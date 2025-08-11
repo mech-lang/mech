26655f1c02d8ab43a443b5dcc733523aca682536
cmontella <cmontella@live.com>
2025-08-11 18:30:19 -0400

    Refactor div_assign module to implement operation assignment functions using macros for better code organization and maintainability

--- src/interpreter/src/stdlib/assign/op_assign/div_assign.rs
+++ src/interpreter/src/stdlib/assign/op_assign/div_assign.rs
@@ -40,96 +40,15 @@ macro_rules! impl_div_assign_match_arms {
 
 macro_rules! impl_div_assign_range_fxn_s {
   ($struct_name:ident, $op:ident, $ix:ty) => {
-    #[derive(Debug)]
-    pub struct $struct_name<T, MatA, IxVec> {
-      pub source: Ref<T>,
-      pub ixes: Ref<IxVec>,
-      pub sink: Ref<MatA>,
-      pub _marker: PhantomData<T>,
-    }
-    impl<T, R1, C1, S1, IxVec> MechFunction for $struct_name<T, naMatrix<T, R1, C1, S1>, IxVec>
-    where
-      Ref<naMatrix<T, R1, C1, S1>>: ToValue,
-      T: Copy + Debug + Clone + Sync + Send + 'static +
-        Div<Output = T> + DivAssign +
-        Zero + One +
-        PartialEq + PartialOrd,
-      IxVec: AsRef<[$ix]> + Debug,
-      R1: Dim, C1: Dim, S1: StorageMut<T, R1, C1> + Clone + Debug,
-    {
-      fn solve(&self) {
-        unsafe {
-          let sink_ptr = &mut *self.sink.as_ptr();
-          let source_ptr = &*self.source.as_ptr();
-          let ix_ptr = &(*self.ixes.as_ptr()).as_ref();
-          $op!(source_ptr,ix_ptr,sink_ptr);
-        }
-      }
-      fn out(&self) -> Value {self.sink.to_value()}
-      fn to_string(&self) -> String {format!("{:#?}", self)}
-    }};}
+    impl_op_assign_range_fxn_s!($struct_name, $op, $ix);
+  }
+}
 
 macro_rules! impl_div_assign_range_fxn_v {
   ($struct_name:ident, $op:ident, $ix:ty) => {
-    #[derive(Debug)]
-    pub struct $struct_name<T, MatA, MatB, IxVec> {
-      pub source: Ref<MatB>,
-      pub ixes: Ref<IxVec>,
-      pub sink: Ref<MatA>,
-      pub _marker: PhantomData<T>,
-    }
-
-    impl<T, R1, C1, S1, R2, C2, S2, IxVec>
-      MechFunction for $struct_name<T, naMatrix<T, R1, C1, S1>, naMatrix<T, R2, C2, S2>, IxVec>
-    where
-      Ref<naMatrix<T, R1, C1, S1>>: ToValue,
-      T: Copy + Debug + Clone + Sync + Send + 'static +
-        Div<Output = T> + DivAssign +
-        Zero + One +
-        PartialEq + PartialOrd,
-      IxVec: AsRef<[$ix]> + Debug,
-      R1: Dim, C1: Dim, S1: StorageMut<T, R1, C1> + Clone + Debug,
-      R2: Dim, C2: Dim, S2: Storage<T, R2, C2> + Clone + Debug,
-    {
-      fn solve(&self) {
-        unsafe {
-          let sink_ptr = &mut *self.sink.as_ptr();
-          let source_ptr = &*self.source.as_ptr();
-          let ix_ptr = &(*self.ixes.as_ptr()).as_ref();
-          $op!(source_ptr,ix_ptr,sink_ptr);
-        }
-      }
-      fn out(&self) -> Value {self.sink.to_value()}
-      fn to_string(&self) -> String {format!("{:#?}", self)}
-    }};}
-
-macro_rules! impl_div_assign_fxn {
-  ($struct_name:ident, $matrix_shape:ident, $source_matrix_shape:ty, $op:ident, $ix:ty) => {
-    #[derive(Debug)]
-    struct $struct_name<T> {
-      source: Ref<$source_matrix_shape>,
-      ixes: Ref<DVector<$ix>>,
-      sink: Ref<$matrix_shape<T>>,
-    }
-    impl<T> MechFunction for $struct_name<T>
-    where
-      T: Copy + Debug + Clone + Sync + Send + 'static +
-      Div<Output = T> + DivAssign +
-      Zero + One +
-      PartialEq + PartialOrd,
-      Ref<$matrix_shape<T>>: ToValue
-    {
-      fn solve(&self) {
-        unsafe {
-          let ix_ptr = (*(self.ixes.as_ptr())).clone();
-          let mut sink_ptr = (&mut *(self.sink.as_ptr()));
-          let source_ptr = (*(self.source.as_ptr())).clone();
-          $op!(source_ptr,ix_ptr,sink_ptr);
-        }
-      }
-      fn out(&self) -> Value { self.sink.to_value() }
-      fn to_string(&self) -> String { format!("{:#?}", self) }
-    }};}
+    impl_op_assign_range_fxn_v!($struct_name, $op, $ix);
+  }
+}
 
 // x /= 1 ----------------------------------------------------------------------
 
--- src/interpreter/src/stdlib/assign/op_assign/mod.rs
+++ src/interpreter/src/stdlib/assign/op_assign/mod.rs
@@ -1,10 +1,87 @@
 #[macro_use]
 use crate::stdlib::*;
 
-//pub mod add_assign;
+use std::fmt::Debug;
+use std::ops::DivAssign;
+use std::marker::PhantomData;
+use nalgebra::{
+  base::{Matrix as naMatrix, Storage, StorageMut},
+  Dim, Scalar,
+};
+
+pub mod add_assign;
 //pub mod sub_assign;
 pub mod div_assign;
 
-//pub use self::add_assign::*;
+pub use self::add_assign::*;
 //pub use self::sub_assign::*;
 pub use self::div_assign::*;
+
+#[macro_export]
+macro_rules! impl_op_assign_range_fxn_s {
+  ($struct_name:ident, $op:ident, $ix:ty) => {
+    #[derive(Debug)]
+    pub struct $struct_name<T, MatA, IxVec> {
+      pub source: Ref<T>,
+      pub ixes: Ref<IxVec>,
+      pub sink: Ref<MatA>,
+      pub _marker: PhantomData<T>,
+    }
+    impl<T, R1, C1, S1, IxVec> MechFunction for $struct_name<T, naMatrix<T, R1, C1, S1>, IxVec>
+    where
+      Ref<naMatrix<T, R1, C1, S1>>: ToValue,
+      T: Copy + Debug + Clone + Sync + Send + 'static +
+        Div<Output = T> + DivAssign +
+        Add<Output = T> + AddAssign +
+        Zero + One +
+        PartialEq + PartialOrd,
+      IxVec: AsRef<[$ix]> + Debug,
+      R1: Dim, C1: Dim, S1: StorageMut<T, R1, C1> + Clone + Debug,
+    {
+      fn solve(&self) {
+        unsafe {
+          let sink_ptr = &mut *self.sink.as_ptr();
+          let source_ptr = &*self.source.as_ptr();
+          let ix_ptr = &(*self.ixes.as_ptr()).as_ref();
+          $op!(source_ptr,ix_ptr,sink_ptr);
+        }
+      }
+      fn out(&self) -> Value {self.sink.to_value()}
+      fn to_string(&self) -> String {format!("{:#?}", self)}
+    }};}
+
+#[macro_export]
+macro_rules! impl_op_assign_range_fxn_v {
+  ($struct_name:ident, $op:ident, $ix:ty) => {
+    #[derive(Debug)]
+    pub struct $struct_name<T, MatA, MatB, IxVec> {
+      pub source: Ref<MatB>,
+      pub ixes: Ref<IxVec>,
+      pub sink: Ref<MatA>,
+      pub _marker: PhantomData<T>,
+    }
+
+    impl<T, R1, C1, S1, R2, C2, S2, IxVec>
+      MechFunction for $struct_name<T, naMatrix<T, R1, C1, S1>, naMatrix<T, R2, C2, S2>, IxVec>
+    where
+      Ref<naMatrix<T, R1, C1, S1>>: ToValue,
+      T: Copy + Debug + Clone + Sync + Send + 'static +
+        Div<Output = T> + DivAssign +
+        Add<Output = T> + AddAssign +
+        Zero + One +
+        PartialEq + PartialOrd,
+      IxVec: AsRef<[$ix]> + Debug,
+      R1: Dim, C1: Dim, S1: StorageMut<T, R1, C1> + Clone + Debug,
+      R2: Dim, C2: Dim, S2: Storage<T, R2, C2> + Clone + Debug,
+    {
+      fn solve(&self) {
+        unsafe {
+          let sink_ptr = &mut *self.sink.as_ptr();
+          let source_ptr = &*self.source.as_ptr();
+          let ix_ptr = &(*self.ixes.as_ptr()).as_ref();
+          $op!(source_ptr,ix_ptr,sink_ptr);
+        }
+      }
+      fn out(&self) -> Value {self.sink.to_value()}
+      fn to_string(&self) -> String {format!("{:#?}", self)}
+    }};}
