c64e0379c9839f11ef9c89986db127c84f53c831
cmontella <cmontella@live.com>
2025-08-12 17:44:34 -0400

    Fix NotS

--- src/not.rs
+++ src/not.rs
@@ -6,10 +6,31 @@ use mech_core::matrix::Matrix;
 use std::ops::Neg;
 use std::fmt::Debug;
 use std::marker::PhantomData;
+use std::ops::Not;
 
 // Not ------------------------------------------------------------------------
 
-use std::ops::Not;
+
+#[derive(Debug)]
+struct NotS<T> {
+  pub arg: Ref<T>,
+  pub out: Ref<T>,
+  pub _marker: PhantomData<T>,
+}
+
+impl<T> MechFunction for NotS<T>
+where
+  T: Copy + Debug + Clone + Sync + Send + PartialEq + 'static + Not<Output = T>,
+  Ref<T>: ToValue,
+{
+  fn solve(&self) {
+    let arg_ptr = self.arg.as_ptr();
+    let out_ptr = self.out.as_ptr();
+    unsafe { *out_ptr = !*arg_ptr; }
+  }
+  fn out(&self) -> Value { self.out.to_value() }
+  fn to_string(&self) -> String { format!("{:#?}", self) }
+}
 
 #[derive(Debug)]
 pub struct NotV<T, MatA> {
@@ -21,7 +42,7 @@ pub struct NotV<T, MatA> {
 impl<T, MatA> MechFunction for NotV<T, MatA>
 where
   Ref<MatA>: ToValue,
-  T: Debug + Clone + Sync + Send + 'static + std::ops::Not<Output = T>,
+  T: Debug + Clone + Sync + Send + 'static + Not<Output = T>,
   for<'a> &'a MatA: IntoIterator<Item = &'a T>,
   for<'a> &'a mut MatA: IntoIterator<Item = &'a mut T>,
   MatA: Debug,
@@ -40,28 +61,6 @@ where
   fn out(&self) -> Value {self.out.to_value()}
   fn to_string(&self) -> String {format!("{:#?}", self)}
 }
-
-#[derive(Debug)]
-struct NotS<O> {
-  pub arg: Ref<O>,
-  pub out: Ref<O>,
-  pub _marker: PhantomData<O>,
-}
-
-impl<O> MechFunction for NotS<O>
-where
-  O: Copy + Debug + Clone + Sync + Send + PartialEq + 'static,
-  Ref<O>: ToValue,
-{
-  fn solve(&self) {
-    let arg_ptr = self.arg.as_ptr();
-    let out_ptr = self.out.as_ptr();
-    unsafe { *out_ptr != *arg_ptr; }
-  }
-  fn out(&self) -> Value { self.out.to_value() }
-  fn to_string(&self) -> String { format!("{:#?}", self) }
-}
-
 
 fn impl_not_fxn(arg_value: Value) -> Result<Box<dyn MechFunction>, MechError> {
   impl_urnop_match_arms!(
