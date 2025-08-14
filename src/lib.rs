8013d2f951fdd42aeb7a528c989376a9a0b1a489
cmontella <cmontella@live.com>
2025-08-14 04:08:06 -0400

    add flags

--- src/lib.rs
+++ src/lib.rs
@@ -2,10 +2,12 @@
 #![allow(warnings)]
 #[macro_use]
 extern crate mech_core;
+#[cfg(feature = "matrix")]
 extern crate nalgebra as na;
 extern crate paste;
 
 use paste::paste;
+#[cfg(feature = "matrix")]
 use na::{Vector3, DVector, Vector2, Vector4, RowDVector, Matrix1, Matrix3, Matrix4, RowVector3, RowVector4, RowVector2, DMatrix, Rotation3, Matrix2x3, Matrix3x2, Matrix6, Matrix2};
 use std::marker::PhantomData;
 use std::fmt::{Debug, Display};
