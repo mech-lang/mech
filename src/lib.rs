3c769085f369a6de512670d7485009029d96f614
cmontella <cmontella@live.com>
2025-08-14 04:08:45 -0400

    add flags

--- src/lib.rs
+++ src/lib.rs
@@ -1,12 +1,17 @@
-extern crate nalgebra as na;
+#![no_main]
+#![allow(warnings)]
+#[macro_use]
 extern crate mech_core;
+extern crate nalgebra as na;
+extern crate paste;
 
-use na::*;
+use mech_core::*;
+use na::{Vector3, DVector, Vector2, Vector4, RowDVector, Matrix1, Matrix3, Matrix4, RowVector3, RowVector4, RowVector2, DMatrix, Rotation3, Matrix2x3, Matrix3x2, Matrix6, Matrix2};
+use paste::paste;
+use mech_core::matrix::Matrix;
 use num_traits::*;
 use std::ops::*;
 use std::fmt::Debug;
-use mech_core::matrix::Matrix;
-use paste::paste;
 
 #[cfg(feature = "sum")]
 pub mod sum_column;
