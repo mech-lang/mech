17064e4ab45989db65c6a562740c07fe7b4364d2
cmontella <cmontella@live.com>
2025-08-15 04:07:53 -0400

    make nalgebra optional

--- src/lib.rs
+++ src/lib.rs
@@ -4,17 +4,20 @@
 #[macro_use]
 extern crate mech_core;
 extern crate libm;
+#[cfg(feature = "matrix")]
 extern crate nalgebra as na;
 extern crate paste;
 extern crate simba;
 
 use paste::paste;
+#[cfg(feature = "matrix")]
 use na::{Vector3, DVector, Vector2, Vector4, RowDVector, Matrix1, Matrix3, Matrix4, RowVector3, RowVector4, RowVector2, DMatrix, Rotation3, Matrix2x3, Matrix3x2, Matrix6, Matrix2};
 use std::ops::*;
 use num_traits::*;
 use std::fmt::Debug;
 use simba::scalar::ClosedNeg;
 use num_traits::Pow;
+#[cfg(feature = "matrix")]
 use mech_core::matrix::Matrix;
 
 #[cfg(feature = "exclusive")]
