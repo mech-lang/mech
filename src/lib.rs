417590f44086e2c47bbf87c0c7781c0acc329391
cmontella <cmontella@live.com>
2025-08-15 04:07:55 -0400

    make nalgebra optional

--- src/lib.rs
+++ src/lib.rs
@@ -2,12 +2,15 @@
 #![allow(warnings)]
 #[macro_use]
 extern crate mech_core;
+#[cfg(feature = "matrix")]
 extern crate nalgebra as na;
 extern crate paste;
 
 use mech_core::*;
+#[cfg(feature = "matrix")]
 use na::{Vector3, DVector, Vector2, Vector4, RowDVector, Matrix1, Matrix3, Matrix4, RowVector3, RowVector4, RowVector2, DMatrix, Rotation3, Matrix2x3, Matrix3x2, Matrix6, Matrix2};
 use paste::paste;
+#[cfg(feature = "matrix")]
 use mech_core::matrix::Matrix;
 
 #[cfg(feature = "or")]
