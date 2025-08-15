3d82d58d1d75d3620328da2a7a7ba26ce6becc36
cmontella <cmontella@live.com>
2025-08-15 04:08:08 -0400

    make nalgebra optional

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,11 @@
 #![no_main]
 #![allow(warnings)]
+
+#[cfg(feature = "matrix")]
 extern crate nalgebra as na;
 extern crate mech_core;
 
+#[cfg(feature = "matrix")]
 use mech_core::matrix::Matrix;
 
 #[cfg(feature = "n_choose_k")]
