c993b2a1740518e75f76bea7f7bb737068393e72
cmontella <cmontella@live.com>
2025-08-16 12:02:43 -0400

    Refactor new_ref

--- src/exclusive.rs
+++ src/exclusive.rs
@@ -20,7 +20,7 @@ where
   fn solve(&self) {
     let max_ptr = self.max.as_ptr();
     let min_ptr = self.min.as_ptr();
-    let out_ptr = self.out.as_ptr();
+    let out_ptr = self.out.as_mut_ptr();
     
     unsafe {
       let rng = (*min_ptr..*max_ptr).collect::<Vec<T>>();
@@ -40,29 +40,29 @@ impl NativeFunctionCompiler for RangeExclusive {
     }
     match (arguments[0].clone(), arguments[1].clone()) {
       #[cfg(feature = "i8")]
-      (Value::I8(min),   Value::I8(max))   => Ok(Box::new(RangeExclusiveScalar{max,min, out: new_ref(RowDVector::from_element(1,0))})),
+      (Value::I8(min),   Value::I8(max))   => Ok(Box::new(RangeExclusiveScalar{max,min, out: Ref::new(RowDVector::from_element(1,0))})),
       #[cfg(feature = "i16")]
-      (Value::I16(min),  Value::I16(max))  => Ok(Box::new(RangeExclusiveScalar{max,min, out: new_ref(RowDVector::from_element(1,0))})),
+      (Value::I16(min),  Value::I16(max))  => Ok(Box::new(RangeExclusiveScalar{max,min, out: Ref::new(RowDVector::from_element(1,0))})),
       #[cfg(feature = "i32")]
-      (Value::I32(min),  Value::I32(max))  => Ok(Box::new(RangeExclusiveScalar{max,min, out: new_ref(RowDVector::from_element(1,0))})),
+      (Value::I32(min),  Value::I32(max))  => Ok(Box::new(RangeExclusiveScalar{max,min, out: Ref::new(RowDVector::from_element(1,0))})),
       #[cfg(feature = "i64")]
-      (Value::I64(min),  Value::I64(max))  => Ok(Box::new(RangeExclusiveScalar{max,min, out: new_ref(RowDVector::from_element(1,0))})),
+      (Value::I64(min),  Value::I64(max))  => Ok(Box::new(RangeExclusiveScalar{max,min, out: Ref::new(RowDVector::from_element(1,0))})),
       #[cfg(feature = "i128")]
-      (Value::I128(min), Value::I128(max)) => Ok(Box::new(RangeExclusiveScalar{max,min, out: new_ref(RowDVector::from_element(1,0))})),
+      (Value::I128(min), Value::I128(max)) => Ok(Box::new(RangeExclusiveScalar{max,min, out: Ref::new(RowDVector::from_element(1,0))})),
       #[cfg(feature = "u8")]
-      (Value::U8(min),   Value::U8(max))   => Ok(Box::new(RangeExclusiveScalar{max,min, out: new_ref(RowDVector::from_element(1,0))})),
+      (Value::U8(min),   Value::U8(max))   => Ok(Box::new(RangeExclusiveScalar{max,min, out: Ref::new(RowDVector::from_element(1,0))})),
       #[cfg(feature = "u16")]
-      (Value::U16(min),  Value::U16(max))  => Ok(Box::new(RangeExclusiveScalar{max,min, out: new_ref(RowDVector::from_element(1,0))})),
+      (Value::U16(min),  Value::U16(max))  => Ok(Box::new(RangeExclusiveScalar{max,min, out: Ref::new(RowDVector::from_element(1,0))})),
       #[cfg(feature = "u32")]
-      (Value::U32(min),  Value::U32(max))  => Ok(Box::new(RangeExclusiveScalar{max,min, out: new_ref(RowDVector::from_element(1,0))})),
+      (Value::U32(min),  Value::U32(max))  => Ok(Box::new(RangeExclusiveScalar{max,min, out: Ref::new(RowDVector::from_element(1,0))})),
       #[cfg(feature = "u64")]
-      (Value::U64(min),  Value::U64(max))  => Ok(Box::new(RangeExclusiveScalar{max,min, out: new_ref(RowDVector::from_element(1,0))})),
+      (Value::U64(min),  Value::U64(max))  => Ok(Box::new(RangeExclusiveScalar{max,min, out: Ref::new(RowDVector::from_element(1,0))})),
       #[cfg(feature = "u128")]
-      (Value::U128(min), Value::U128(max)) => Ok(Box::new(RangeExclusiveScalar{max,min, out: new_ref(RowDVector::from_element(1,0))})),
+      (Value::U128(min), Value::U128(max)) => Ok(Box::new(RangeExclusiveScalar{max,min, out: Ref::new(RowDVector::from_element(1,0))})),
       #[cfg(feature = "f32")]
-      (Value::F32(min),  Value::F32(max))  => Ok(Box::new(RangeExclusiveScalar{max,min, out: new_ref(RowDVector::from_element(1,F32::new(0.0)))})),
+      (Value::F32(min),  Value::F32(max))  => Ok(Box::new(RangeExclusiveScalar{max,min, out: Ref::new(RowDVector::from_element(1,F32::new(0.0)))})),
       #[cfg(feature = "f64")]
-      (Value::F64(min),  Value::F64(max))  => Ok(Box::new(RangeExclusiveScalar{max,min, out: new_ref(RowDVector::from_element(1,F64::new(0.0)))})),
+      (Value::F64(min),  Value::F64(max))  => Ok(Box::new(RangeExclusiveScalar{max,min, out: Ref::new(RowDVector::from_element(1,F64::new(0.0)))})),
       x => Err(MechError{file: file!().to_string(), tokens: vec![], msg: "".to_string(), id: line!(), kind: MechErrorKind::UnhandledFunctionArgumentKind}),
     }
   }
