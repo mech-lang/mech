51b18e1a7ac4136d5098c9e0df025f3403e8b7ef
cmontella <cmontella@live.com>
2025-08-17 16:57:04 -0400

    impl the header

--- src/interpreter/src/compiler.rs
+++ src/interpreter/src/compiler.rs
@@ -11,7 +11,8 @@
 // End marker: 0x45 4e 44 21 (END!)
 
 use crate::*;
-use byteorder::{LittleEndian, WriteBytesExt};
+use byteorder::{LittleEndian, WriteBytesExt, ReadBytesExt};
+use std::io::{self, Write, Read};
 
 #[repr(u16)]
 #[derive(Debug, Clone, Copy, PartialEq, Eq)]
@@ -69,6 +70,128 @@ pub struct ByteCodeHeader {
   checksum:       u32,         // optional (CRC32/xxHash), or 0 if unused
   reserved:       u32,         // pad/alignment
 }
+
+impl ByteCodeHeader {
+  /// Header byte size when serialized. This is the number of bytes `write_to` will write.
+  /// (Computed from the sum of sizes of each field written in little-endian.)
+  pub const HEADER_SIZE: usize = 4  // magic
+    + 1   // version
+    + 2   // mech_ver
+    + 2   // flags
+    + 4   // reg_count
+    + 4   // instr_count
+    + 4   // feature_count
+    + 8   // feature_off
+    + 4   // const_count
+    + 8   // const_tbl_off
+    + 8   // const_tbl_len
+    + 8   // const_blob_off
+    + 8   // const_blob_len
+    + 8   // instr_off
+    + 8   // instr_len
+    + 8   // feat_off
+    + 8   // feat_len
+    + 4   // checksum
+    + 4;  // reserved
+
+  /// Serialize header into `w` using little-endian encoding.
+  pub fn write_to(&self, w: &mut impl Write) -> io::Result<()> {
+    // magic (4 bytes)
+    w.write_all(&self.magic)?;
+
+    // small fields
+    w.write_u8(self.version)?;
+    w.write_u16::<LittleEndian>(self.mech_ver)?;
+    w.write_u16::<LittleEndian>(self.flags)?;
+
+    // counts
+    w.write_u32::<LittleEndian>(self.reg_count)?;
+    w.write_u32::<LittleEndian>(self.instr_count)?;
+
+    // features (count + offset)
+    w.write_u32::<LittleEndian>(self.feature_count)?;
+    w.write_u64::<LittleEndian>(self.feature_off)?;
+
+    // constants table / blob
+    w.write_u32::<LittleEndian>(self.const_count)?;
+    w.write_u64::<LittleEndian>(self.const_tbl_off)?;
+    w.write_u64::<LittleEndian>(self.const_tbl_len)?;
+    w.write_u64::<LittleEndian>(self.const_blob_off)?;
+    w.write_u64::<LittleEndian>(self.const_blob_len)?;
+
+    // instructions
+    w.write_u64::<LittleEndian>(self.instr_off)?;
+    w.write_u64::<LittleEndian>(self.instr_len)?;
+
+    // feature section (alternative/extra)
+    w.write_u64::<LittleEndian>(self.feat_off)?;
+    w.write_u64::<LittleEndian>(self.feat_len)?;
+
+    // footer
+    w.write_u32::<LittleEndian>(self.checksum)?;
+    w.write_u32::<LittleEndian>(self.reserved)?;
+    Ok(())
+  }
+
+  /// Read a header from `r`. Expects the same layout as `write_to`.
+  pub fn read_from(r: &mut impl Read) -> io::Result<Self> {
+    let mut magic = [0u8; 4];
+    r.read_exact(&mut magic)?;
+
+    let version = r.read_u8()?;
+    let mech_ver = r.read_u16::<LittleEndian>()?;
+    let flags = r.read_u16::<LittleEndian>()?;
+
+    let reg_count = r.read_u32::<LittleEndian>()?;
+    let instr_count = r.read_u32::<LittleEndian>()?;
+
+    let feature_count = r.read_u32::<LittleEndian>()?;
+    let feature_off = r.read_u64::<LittleEndian>()?;
+
+    let const_count = r.read_u32::<LittleEndian>()?;
+    let const_tbl_off = r.read_u64::<LittleEndian>()?;
+    let const_tbl_len = r.read_u64::<LittleEndian>()?;
+    let const_blob_off = r.read_u64::<LittleEndian>()?;
+    let const_blob_len = r.read_u64::<LittleEndian>()?;
+
+    let instr_off = r.read_u64::<LittleEndian>()?;
+    let instr_len = r.read_u64::<LittleEndian>()?;
+
+    let feat_off = r.read_u64::<LittleEndian>()?;
+    let feat_len = r.read_u64::<LittleEndian>()?;
+
+    let checksum = r.read_u32::<LittleEndian>()?;
+    let reserved = r.read_u32::<LittleEndian>()?;
+
+    Ok(Self {
+      magic,
+      version,
+      mech_ver,
+      flags,
+      reg_count,
+      instr_count,
+      feature_count,
+      feature_off,
+      const_count,
+      const_tbl_off,
+      const_tbl_len,
+      const_blob_off,
+      const_blob_len,
+      instr_off,
+      instr_len,
+      feat_off,
+      feat_len,
+      checksum,
+      reserved,
+    })
+  }
+
+  /// Quick check: does the header magic match the expected magic?
+  pub fn validate_magic(&self, expected: &[u8;4]) -> bool {
+    &self.magic == expected
+  }
+}
+
 
 #[repr(u16)]
 #[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
