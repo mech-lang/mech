9128659ac60e0633eb103c0c4a215fe687c3bce9
cmontella <cmontella@live.com>
2025-08-20 15:23:06 -0400

    add flags

--- src/lib.rs
+++ src/lib.rs
@@ -7,8 +7,40 @@ extern crate nalgebra as na;
 extern crate paste;
 
 use paste::paste;
-#[cfg(feature = "matrix")]
-use na::{Vector3, DVector, Vector2, Vector4, RowDVector, Matrix1, Matrix3, Matrix4, RowVector3, RowVector4, RowVector2, DMatrix, Rotation3, Matrix2x3, Matrix3x2, Matrix6, Matrix2};
+
+#[cfg(feature = "vector3")]
+use nalgebra::Vector3;
+#[cfg(feature = "vectord")]
+use nalgebra::DVector;
+#[cfg(feature = "vector2")]
+use nalgebra::Vector2;
+#[cfg(feature = "vector4")]
+use nalgebra::Vector4;
+#[cfg(feature = "rowdvector")]
+use nalgebra::RowDVector;
+#[cfg(feature = "row_vectord")]
+use nalgebra::RowDVector;
+#[cfg(feature = "matrix1")]
+use nalgebra::Matrix1;
+#[cfg(feature = "matrix3")]
+use nalgebra::Matrix3;
+#[cfg(feature = "matrix4")]
+use nalgebra::Matrix4;
+#[cfg(feature = "row_vector3")]
+use nalgebra::RowVector3;
+#[cfg(feature = "row_vector4")]
+use nalgebra::RowVector4;
+#[cfg(feature = "row_vector2")]
+use nalgebra::RowVector2;
+#[cfg(feature = "matrixd")]
+use nalgebra::DMatrix;
+#[cfg(feature = "matrix2x3")]
+use nalgebra::Matrix2x3;
+#[cfg(feature = "matrix3x2")]
+use nalgebra::Matrix3x2;
+#[cfg(feature = "matrix2")]
+use nalgebra::Matrix2;
+
 use std::marker::PhantomData;
 use std::fmt::{Debug, Display};
 
