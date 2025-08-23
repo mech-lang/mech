7fd460bb889431b5a197def6b366396ddd56185a
cmontella <cmontella@live.com>
2025-08-22 21:27:02 -0400

    compiles with no flags

--- src/lib.rs
+++ src/lib.rs
@@ -43,13 +43,19 @@ use std::ops::*;
 use std::fmt::{Display, Debug};
 use std::marker::PhantomData;
 
-pub mod trig;
+//#[cfg(feature = "trig")]
+//pub mod trig;
+#[cfg(feature = "ops")]
 pub mod ops;
-pub mod op_assign;
+//#[cfg(feature = "op_assign")]
+//pub mod op_assign;
 
-pub use self::trig::*;
+//#[cfg(feature = "trig")]
+//pub use self::trig::*;
+#[cfg(feature = "ops")]
 pub use self::ops::*;
-pub use self::op_assign::*;
+//#[cfg(feature = "op_assign")]
+//pub use self::op_assign::*;
 
 // ----------------------------------------------------------------------------
 // Math Library
