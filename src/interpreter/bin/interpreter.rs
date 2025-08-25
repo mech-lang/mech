18baa81906b4e93f667f2b407396d70b1e1576b7
cmontella <cmontella@live.com>
2025-08-25 01:08:25 -0400

    update the interpreter bin

--- src/interpreter/bin/interpreter.rs
+++ src/interpreter/bin/interpreter.rs
@@ -1,45 +1,22 @@
-//use mech_interpreter::*;
-//use mech_core::*;
-//#[cfg(feature = "serde")]
-//use bincode::config::standard;
-
+use mech_interpreter::*;
+use mech_core::*;
 fn main() {
-  /*// 1. Create a new interpreter
   let mut intrp = Interpreter::new(0);
-
-  // 2. Include the serialized code at compile time
-  //    Suppose you saved it previously as "program.mecb" using bincode::encode_to_vec
-  let serialized_code: &[u8] = include_bytes!("bar.mecb");
-
-  // 3. Deserialize into MechSourceCode
-  #[cfg(feature = "serde")]
-  let (decoded_code, _len): (MechSourceCode, _) =
-      bincode::serde::decode_from_slice(serialized_code, standard()).unwrap();
-  let decoded_code: MechSourceCode = MechSourceCode::String("This is a test".to_string());
-  match decoded_code {
-    MechSourceCode::Program(code_vec) => {
-      for c in code_vec {
-        match c { 
-          MechSourceCode::Tree(tree) => {
-            // 4. Interpret the tree
-            match intrp.interpret(&tree) {
-              Ok(result) => {
-                //println!("Interpretation Result: {:?}", result);
-              }
-              Err(err) => {
-                eprintln!("Error during interpretation: {:?}", err);
-              }
-            }
-          },
-          _ => {
-            //println!("Skipping non-tree MechSourceCode variant.");
-          }
+  let serialized_code: &[u8] = include_bytes!("output.mecb");
+  match ParsedProgram::from_bytes(serialized_code) {
+    Ok(prog) => {
+      match intrp.run_program(&prog) {
+        Ok(result) => {
+          println!("Program executed successfully. Result: {}", result);
+        },
+        Err(e) => {
+          eprintln!("Error running program: {:?}", e);
         }
       }
-    }
-    _ => {
-      //println!("The included file does not contain a valid Mech program.");
+    },
+    Err(e) => {
+      eprintln!("Error deserializing program: {:?}", e);
     }
   }
-  //println!("Loaded interpreter code: {:?}", intrp.code);*/
 }
+
