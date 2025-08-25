0fb6e2126f42677e9cddfe50a59063969d4db0dd
cmontella <cmontella@live.com>
2025-08-25 01:37:23 -0400

    Add bytecode tests and update CI configuration for improved testing

---
+++ tests/bytecode.rs
@@ -0,0 +1,46 @@
+#![allow(warnings)]
+extern crate mech_syntax;
+extern crate mech_core;
+extern crate nalgebra as na;
+use std::cell::RefCell;
+use std::rc::Rc;
+use mech_core::matrix::Matrix;
+use mech_syntax::*;
+use mech_core::*;
+use mech_interpreter::*;
+use indexmap::set::IndexSet;
+
+macro_rules! bytecode_test {
+  ($name:ident, $code:expr, $expected:expr) => {
+    #[test]
+    fn $name() {
+      let mut intrp = Interpreter::new(0);
+      match parser::parse($code) {
+        Ok(tree) => {
+          let mut intrp = Interpreter::new(0);
+          let _ = intrp.interpret(&tree).unwrap();
+          let bytecode = intrp.compile().unwrap();
+          match ParsedProgram::from_bytes(&bytecode) {
+            Ok(prog) => {
+              match intrp.run_program(&prog) {
+                Ok(result) => {
+                  assert_eq!(result, $expected);
+                },
+                Err(e) => {
+                  eprintln!("Error running program: {:?}", e);
+                }
+              }
+            },
+            Err(e) => {
+              eprintln!("Error deserializing program: {:?}", e);
+            }
+          }
+        },
+        Err(err) => { panic!("{:?}", err); }
+      }
+    }
+  };
+}
+
+bytecode_test!(bytecode_math,"1 + 2",Value::F64(Ref::new(F64::new(3.0))));
+bytecode_test!(bytecode_math_assign,"x := 1 + 2; y := x + 4",Value::F64(Ref::new(F64::new(7.0))));
