d94758e23f54d8b42cb9a1b9654ae96350019168
cmontella <cmontella@live.com>
2025-08-25 01:21:27 -0400

    allow interpreter to build without feaures

--- src/interpreter/bin/interpreter.rs
+++ src/interpreter/bin/interpreter.rs
@@ -3,6 +3,7 @@ use mech_core::*;
 fn main() {
   let mut intrp = Interpreter::new(0);
   let serialized_code: &[u8] = include_bytes!("output.mecb");
+  #[cfg(feature = "program")]
   match ParsedProgram::from_bytes(serialized_code) {
     Ok(prog) => {
       match intrp.run_program(&prog) {
