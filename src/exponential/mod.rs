a0589c5791f1cb86f527960a99e0b6910876e2cd
Liam Messinger <liam.messinger@gmail.com>
2025-08-31 14:53:08 -0400

    Add expm1 function implementation and update Cargo.toml

--- src/exponential/mod.rs
+++ src/exponential/mod.rs
@@ -4,8 +4,12 @@ pub use crate::*;
 pub mod exp2;
 #[cfg(feature = "exp10")]
 pub mod exp10;
+#[cfg(feature = "expm1")]
+pub mod expm1;
 
 #[cfg(feature = "exp2")]
 pub use self::exp2::*;
 #[cfg(feature = "exp10")]
 pub use self::exp10::*;
+#[cfg(feature = "expm1")]
+pub use self::expm1::*;
