dacbb4e46b3822bd5119c2bae35fbd05c129293b
Steven McPhillimey <slm526@lehigh.edu>
2025-09-02 16:13:03 -0400

    Added logs

---
+++ src/funcs/log10.rs
@@ -0,0 +1,66 @@
+use crate::*;
+use mech_core::*;
+use num_traits::*;
+#[cfg(feature = "matrix")]
+use mech_core::matrix::Matrix;
+
+// Log10 ------------------------------------------------------------------------
+
+use libm::{log10,log10f};
+macro_rules! log10_op {
+  ($arg:expr, $out:expr) => {
+    unsafe{(*$out).0 = log10((*$arg).0);}
+  };}
+
+macro_rules! log10_vec_op {
+  ($arg:expr, $out:expr) => {
+    unsafe {
+      for i in 0..(*$arg).len() {
+        ((&mut (*$out))[i]).0 = log10(((&(*$arg))[i]).0);
+      }}};}
+
+macro_rules! log10f_op {
+  ($arg:expr, $out:expr) => {
+    unsafe{(*$out).0 = log10f((*$arg).0);}
+  };}  
+
+macro_rules! log10f_vec_op {
+  ($arg:expr, $out:expr) => {
+    unsafe {
+      for i in 0..(*$arg).len() {
+        ((&mut (*$out))[i]).0 = log10f(((&(*$arg))[i]).0);
+      }}};}
+
+#[cfg(feature = "f32")]
+impl_math_unop!(MathLog10, F32, log10f, FeatureFlag::Custom(hash_str("math/log10")));
+#[cfg(feature = "f64")]
+impl_math_unop!(MathLog10, F64, log10, FeatureFlag::Custom(hash_str("math/log10")));
+
+fn impl_log10_fxn(lhs_value: Value) -> Result<Box<dyn MechFunction>, MechError> {
+  impl_urnop_match_arms2!(
+    MathLog10,
+    (lhs_value),
+    F32 => MatrixF32, F32, F32::zero(), "f32";
+    F64 => MatrixF64, F64, F64::zero(), "f64";
+  )
+}
+
+pub struct MathLog10 {}
+
+impl NativeFunctionCompiler for MathLog10 {
+  fn compile(&self, arguments: &Vec<Value>) -> MResult<Box<dyn MechFunction>> {
+    if arguments.len() != 1 {
+      return Err(MechError{file: file!().to_string(), tokens: vec![], msg: "".to_string(), id: line!(), kind: MechErrorKind::IncorrectNumberOfArguments});
+    }
+    let input = arguments[0].clone();
+    match impl_log10_fxn(input.clone()) {
+      Ok(fxn) => Ok(fxn),
+      Err(_) => {
+        match (input) {
+          (Value::MutableReference(input)) => {impl_log10_fxn(input.borrow().clone())}
+          x => Err(MechError{file: file!().to_string(),  tokens: vec![], msg: "".to_string(), id: line!(), kind: MechErrorKind::UnhandledFunctionArgumentKind }),
+        }
+      }
+    }
+  }
+}
---
+++ src/funcs/log2.rs
@@ -0,0 +1,66 @@
+use crate::*;
+use mech_core::*;
+use num_traits::*;
+#[cfg(feature = "matrix")]
+use mech_core::matrix::Matrix;
+
+// Log2 ------------------------------------------------------------------------
+
+use libm::{log2,log2f};
+macro_rules! log2_op {
+  ($arg:expr, $out:expr) => {
+    unsafe{(*$out).0 = log2((*$arg).0);}
+  };}
+
+macro_rules! log2_vec_op {
+  ($arg:expr, $out:expr) => {
+    unsafe {
+      for i in 0..(*$arg).len() {
+        ((&mut (*$out))[i]).0 = log2(((&(*$arg))[i]).0);
+      }}};}
+
+macro_rules! log2f_op {
+  ($arg:expr, $out:expr) => {
+    unsafe{(*$out).0 = log2f((*$arg).0);}
+  };}  
+
+macro_rules! log2f_vec_op {
+  ($arg:expr, $out:expr) => {
+    unsafe {
+      for i in 0..(*$arg).len() {
+        ((&mut (*$out))[i]).0 = log2f(((&(*$arg))[i]).0);
+      }}};}
+
+#[cfg(feature = "f32")]
+impl_math_unop!(MathLog2, F32, log2f, FeatureFlag::Custom(hash_str("math/log2")));
+#[cfg(feature = "f64")]
+impl_math_unop!(MathLog2, F64, log2, FeatureFlag::Custom(hash_str("math/log2")));
+
+fn impl_log2_fxn(lhs_value: Value) -> Result<Box<dyn MechFunction>, MechError> {
+  impl_urnop_match_arms2!(
+    MathLog2,
+    (lhs_value),
+    F32 => MatrixF32, F32, F32::zero(), "f32";
+    F64 => MatrixF64, F64, F64::zero(), "f64";
+  )
+}
+
+pub struct MathLog2 {}
+
+impl NativeFunctionCompiler for MathLog2 {
+  fn compile(&self, arguments: &Vec<Value>) -> MResult<Box<dyn MechFunction>> {
+    if arguments.len() != 1 {
+      return Err(MechError{file: file!().to_string(), tokens: vec![], msg: "".to_string(), id: line!(), kind: MechErrorKind::IncorrectNumberOfArguments});
+    }
+    let input = arguments[0].clone();
+    match impl_log2_fxn(input.clone()) {
+      Ok(fxn) => Ok(fxn),
+      Err(_) => {
+        match (input) {
+          (Value::MutableReference(input)) => {impl_log2_fxn(input.borrow().clone())}
+          x => Err(MechError{file: file!().to_string(),  tokens: vec![], msg: "".to_string(), id: line!(), kind: MechErrorKind::UnhandledFunctionArgumentKind }),
+        }
+      }
+    }
+  }
+}
