4e4036faedda15fdcfa2409b49baa4e9a93c8393
cmontella <cmontella@live.com>
2025-09-06 16:19:17 -0400

    Enhance register_fxn_descriptor for NegateV and NegateS to include type string representations for improved clarity

--- src/ops/negate.rs
+++ src/ops/negate.rs
@@ -54,7 +54,17 @@ where
   }
 }
 
-register_fxn_descriptor!(NegateV, i8, i16, i32, i64, i128, F32, F64, R64, C64);
+register_fxn_descriptor!(NegateV, 
+  i8, "i8",
+  i16, "i16",
+  i32, "i32",
+  i64, "i64",
+  i128, "i128",
+  F32, "f32",
+  F64, "f64",
+  R64, "r64",
+  C64, "c64"
+);
 
 #[derive(Debug)]
 struct NegateS<O> {
@@ -103,7 +113,17 @@ where
   }
 }
 
-register_fxn_descriptor!(NegateS, i8, i16, i32, i64, i128, F32, F64, R64, C64);
+register_fxn_descriptor!(NegateS,
+  i8, "i8",
+  i16, "i16",
+  i32, "i32",
+  i64, "i64",
+  i128, "i128",
+  F32, "f32",
+  F64, "f64",
+  R64, "r64",
+  C64, "c64"
+);
 
 fn impl_neg_fxn(lhs_value: Value) -> Result<Box<dyn MechFunction>, MechError> {
   impl_urnop_match_arms!(
