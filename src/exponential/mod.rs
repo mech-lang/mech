bd612b75a1995e3407c447540aa3ebf94f48a5d8
Liam Messinger <liam.messinger@gmail.com>
2025-09-07 20:05:34 -0400

    Add exponential (e^x) module

--- src/exponential/mod.rs
+++ src/exponential/mod.rs
@@ -1,5 +1,7 @@
 #[macro_use]
 pub use crate::*;
+#[cfg(feature = "exponential")]
+pub mod exponential;
 #[cfg(feature = "exp2")]
 pub mod exp2;
 #[cfg(feature = "exp10")]
@@ -7,6 +9,8 @@ pub mod exp10;
 #[cfg(feature = "expm1")]
 pub mod expm1;
 
+#[cfg(feature = "exponential")]
+pub use self::exponential::*;
 #[cfg(feature = "exp2")]
 pub use self::exp2::*;
 #[cfg(feature = "exp10")]
