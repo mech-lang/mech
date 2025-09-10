716de13d3dbc7ade17ade96d0f4eab080d8e27e8
cmontella <cmontella@live.com>
2025-09-10 00:13:53 -0400

    Update Cargo.toml and implement RangeInclusiveScalar with matrix support

--- src/inclusive.rs
+++ src/inclusive.rs
@@ -2,66 +2,156 @@
 use crate::*;
 use mech_core::*;
 use std::iter::Step;
+use nalgebra::{
+  base::{Matrix as naMatrix, Storage, StorageMut},
+  Dim, Scalar,
+};
+use mech_core::matrix::Matrix;
+use std::marker::PhantomData;
 
 // Inclusive ------------------------------------------------------------------
 
-#[cfg(feature = "row_vectord")]
 #[derive(Debug)]
-struct RangeInclusiveScalar<T> {
-  max: Ref<T>,
-  min: Ref<T>,
-  out: Ref<RowDVector<T>>,
+pub struct RangeInclusiveScalar<T, MatA> {
+  pub from: Ref<T>,
+  pub to: Ref<T>,
+  pub out: Ref<MatA>,
+  phantom: PhantomData<T>,
 }
-#[cfg(feature = "row_vectord")]
-impl<T> MechFunctionFactory for RangeInclusiveScalar<T>
+impl<T, R1, C1, S1> MechFunctionFactory for RangeInclusiveScalar<T, naMatrix<T, R1, C1, S1>>
 where
-    T: Copy + Debug + Clone + Sync + Send + Step + 
-    CompileConst + ConstElem + AsValueKind +
-    PartialEq + 'static,
-    Ref<RowDVector<T>>: ToValue
+  T: Copy + Debug + Clone + Sync + Send + Step + 
+  CompileConst + ConstElem + AsValueKind +
+  PartialOrd + 'static + One + Add<Output = T>,
+  Ref<naMatrix<T, R1, C1, S1>>: ToValue,
+  naMatrix<T, R1, C1, S1>: CompileConst + ConstElem + AsValueKind,
+  R1: Dim + 'static, C1: Dim, S1: StorageMut<T, R1, C1> + Clone + Debug + 'static,
 {
   fn new(args: FunctionArgs) -> MResult<Box<dyn MechFunction>> {
     match args {
-      FunctionArgs::Binary(out, min, max) => {
-        let min = unsafe{ min.as_unchecked().clone() };
-        let max = unsafe{ max.as_unchecked().clone() };
-        let out = unsafe{ out.as_unchecked().clone() };
-        Ok(Box::new(RangeInclusiveScalar { max, min, out }))
-      }
-      _ => Err(MechError{file: file!().to_string(), tokens: vec![], msg: "".to_string(), id: line!(), kind: MechErrorKind::None})
+      FunctionArgs::Binary(out, from, to) => {
+        let from: Ref<T> = unsafe { from.as_unchecked() }.clone();
+        let to: Ref<T> = unsafe { to.as_unchecked() }.clone();
+        let out: Ref<naMatrix<T, R1, C1, S1>> = unsafe { out.as_unchecked() }.clone();
+        Ok(Box::new(Self { from, to, out, phantom: PhantomData::default() }))
+      },
+      _ => Err(MechError{file: file!().to_string(), tokens: vec![], msg: format!("RangeInclusiveScalar requires 3 arguments, got {:?}", args), id: line!(), kind: MechErrorKind::IncorrectNumberOfArguments})
     }
   }
 }
-#[cfg(feature = "row_vectord")]
-impl<T> MechFunctionImpl for RangeInclusiveScalar<T>
+impl<T, R1, C1, S1> MechFunctionImpl for RangeInclusiveScalar<T, naMatrix<T, R1, C1, S1>>
 where
-    T: Copy + Debug + Clone + Sync + Send + Step + PartialEq + 'static,
-    Ref<RowDVector<T>>: ToValue
+  Ref<naMatrix<T, R1, C1, S1>>: ToValue,
+  T: Scalar + Clone + Debug + Sync + Send + 'static + PartialOrd + One + Add<Output = T> + 'static,
+  R1: Dim, C1: Dim, S1: StorageMut<T, R1, C1> + Clone + Debug,
 {
   fn solve(&self) {
-    let max_ptr = self.max.as_ptr();
-    let min_ptr = self.min.as_ptr();
+    let from_ptr = self.from.as_ptr();
+    let to_ptr = self.to.as_ptr();
     let out_ptr = self.out.as_mut_ptr();
-    
+    let mut current = from_ptr;
     unsafe {
-      let rng = (*min_ptr..=*max_ptr).collect::<Vec<T>>();
-      *out_ptr = RowDVector::from_vec(rng);
+      for val in (*out_ptr).iter_mut() {
+        *val = (*current).clone();
+        current = &(*current).clone().add(T::one());
+      }
     }
   }
   fn out(&self) -> Value { self.out.to_value() }
-  fn to_string(&self) -> String { format!("{:#?}", self)}
+  fn to_string(&self) -> String { format!("{:#?}", self) }
 }
 #[cfg(feature = "compiler")]
-impl<T> MechFunctionCompiler for RangeInclusiveScalar<T> 
+impl<T, R1, C1, S1> MechFunctionCompiler for RangeInclusiveScalar<T, naMatrix<T, R1, C1, S1>> 
 where
   T: CompileConst + ConstElem + AsValueKind,
+  naMatrix<T, R1, C1, S1>: CompileConst + ConstElem + AsValueKind,
 {
   fn compile(&self, ctx: &mut CompileCtx) -> MResult<Register> {
-    let name = format!("RangeInclusiveScalar<{}>", T::as_value_kind());
-    compile_binop!(name, self.out, self.min, self.max, ctx, FeatureFlag::Custom(hash_str("range/inclusive")));
+    let name = format!("RangeInclusiveScalar<{}>", naMatrix::<T, R1, C1, S1>::as_value_kind());
+    compile_binop!(name, self.out, self.from, self.to, ctx, FeatureFlag::Builtin(FeatureKind::RangeInclusive) );
   }
 }
-register_fxn_descriptor!(RangeInclusiveScalar, u8, "u8", u16, "u16", u32, "u32", u64, "u64", u128, "u128", i8, "i8", i16, "i16", i32, "i32", i64, "i64", i128, "i128", F32, "f32", F64, "f64");
+
+macro_rules! register_range_inclusive_scalar {
+  ($($ty:tt, $feat:tt);+ $(;)?) => {
+    paste!{
+      $(
+        #[cfg(feature = $feat)]
+        inventory::submit! {
+          FunctionDescriptor {
+            name: concat!("RangeInclusiveScalar<", stringify!([<$ty:lower>]), ":1,4>"),
+            ptr: RangeInclusiveScalar::<$ty, RowVector4<$ty>>::new,
+          }
+        }
+      )+
+    }
+  };
+}
+
+register_range_inclusive_scalar!(
+  F64, "f64";
+  F32, "f32";
+  i8,  "i8";
+  i16, "i16";
+  i32, "i32";
+  i64, "i64";
+  i128,"i128";
+  u8,  "u8";
+  u16, "u16";
+  u32, "u32";
+  u64, "u64";
+  u128,"u128";  
+);
+
+
+#[macro_export]
+macro_rules! impl_binop_atan2 {
+  ($fxn:ident, $arg1:expr, $arg2:expr, $($ty:tt, $feat:tt);+ $(;)?) => {
+    paste! {
+      match ($arg1, $arg2) {
+        $(
+          #[cfg(feature = $feat)]
+          (Value::[<$ty:camel>](from), Value::[<$ty:camel>](to))  => {
+            let from_val = *from.borrow();
+            let to_val = *to.borrow();
+            let diff = to_val - from_val;
+            if diff < $ty::zero() {
+              return Err(MechError {file: file!().to_string(),tokens: vec![],msg: "Range size must be > 0".to_string(),id: line!(),kind: MechErrorKind::UnhandledFunctionArgumentKind,});
+            }
+            let size = (diff + $ty::one()).try_into().map_err(|_| MechError {file: file!().to_string(),tokens: vec![],msg: "Range size overflow".to_string(),id: line!(),kind: MechErrorKind::UnhandledFunctionArgumentKind,})?;            
+            let mut vec = vec![from_val; size];
+            match size {
+              0 => Err(MechError {file: file!().to_string(),tokens: vec![],msg: "Range size must be > 0".to_string(),id: line!(),kind: MechErrorKind::UnhandledFunctionArgumentKind,}),
+              1 => Ok(Box::new($fxn::<$ty,Matrix1<$ty>>{from: from.clone(), to: to.clone(), out: Ref::new(Matrix1::from_element(vec[0])), phantom: PhantomData::default()})),
+              2 => Ok(Box::new($fxn::<$ty,RowVector2<$ty>>{from: from.clone(), to: to.clone(), out: Ref::new(RowVector2::from_vec(vec)), phantom: PhantomData::default()})),
+              3 => Ok(Box::new($fxn::<$ty,RowVector3<$ty>>{from: from.clone(), to: to.clone(), out: Ref::new(RowVector3::from_vec(vec)), phantom: PhantomData::default()})),
+              4 => Ok(Box::new($fxn::<$ty,RowVector4<$ty>>{from: from.clone(), to: to.clone(), out: Ref::new(RowVector4::from_vec(vec)), phantom: PhantomData::default()})),
+              n => Ok(Box::new($fxn::<$ty,RowDVector<$ty>>{from: from.clone(), to: to.clone(), out: Ref::new(RowDVector::from_vec(vec)), phantom: PhantomData::default()})),
+            }
+          }
+        )+
+        x => Err(MechError {file: file!().to_string(),tokens: vec![],msg: format!("{:?}", x),id: line!(),kind: MechErrorKind::UnhandledFunctionArgumentKind,})
+      }
+    }
+  }
+}
+
+fn impl_range_inclusive_fxn(arg1_value: Value, arg2_value: Value) -> MResult<Box<dyn MechFunction>> {
+  impl_binop_atan2!(RangeInclusiveScalar, arg1_value, arg2_value,
+    F32, "f32";
+    F64, "f64";
+    i8,  "i8";
+    i16, "i16";
+    i32, "i32";
+    i64, "i64";
+    i128,"i128";
+    u8,  "u8";
+    u16, "u16";
+    u32, "u32";
+    u64, "u64";
+    u128,"u128";
+  )
+}
 
 pub struct RangeInclusive {}
 
@@ -70,32 +160,18 @@ impl NativeFunctionCompiler for RangeInclusive {
     if arguments.len() != 2 {
       return Err(MechError{file: file!().to_string(), tokens: vec![], msg: "".to_string(), id: line!(), kind: MechErrorKind::IncorrectNumberOfArguments});
     }
-    match (arguments[0].clone(), arguments[1].clone()) {
-      #[cfg(all(feature = "i8", feature = "row_vectord"))]
-      (Value::I8(min),   Value::I8(max))   => Ok(Box::new(RangeInclusiveScalar{max,min, out: Ref::new(RowDVector::from_element(1,0))})),
-      #[cfg(all(feature = "i16", feature = "row_vectord"))]
-      (Value::I16(min),  Value::I16(max))  => Ok(Box::new(RangeInclusiveScalar{max,min, out: Ref::new(RowDVector::from_element(1,0))})),
-      #[cfg(all(feature = "i32", feature = "row_vectord"))]
-      (Value::I32(min),  Value::I32(max))  => Ok(Box::new(RangeInclusiveScalar{max,min, out: Ref::new(RowDVector::from_element(1,0))})),
-      #[cfg(all(feature = "i64", feature = "row_vectord"))]
-      (Value::I64(min),  Value::I64(max))  => Ok(Box::new(RangeInclusiveScalar{max,min, out: Ref::new(RowDVector::from_element(1,0))})),
-      #[cfg(all(feature = "i128", feature = "row_vectord"))]
-      (Value::I128(min), Value::I128(max)) => Ok(Box::new(RangeInclusiveScalar{max,min, out: Ref::new(RowDVector::from_element(1,0))})),
-      #[cfg(all(feature = "u8", feature = "row_vectord"))]
-      (Value::U8(min),   Value::U8(max))   => Ok(Box::new(RangeInclusiveScalar{max,min, out: Ref::new(RowDVector::from_element(1,0))})),
-      #[cfg(all(feature = "u16", feature = "row_vectord"))]
-      (Value::U16(min),  Value::U16(max))  => Ok(Box::new(RangeInclusiveScalar{max,min, out: Ref::new(RowDVector::from_element(1,0))})),
-      #[cfg(all(feature = "u32", feature = "row_vectord"))]
-      (Value::U32(min),  Value::U32(max))  => Ok(Box::new(RangeInclusiveScalar{max,min, out: Ref::new(RowDVector::from_element(1,0))})),
-      #[cfg(all(feature = "u64", feature = "row_vectord"))]
-      (Value::U64(min),  Value::U64(max))  => Ok(Box::new(RangeInclusiveScalar{max,min, out: Ref::new(RowDVector::from_element(1,0))})),
-      #[cfg(all(feature = "u128", feature = "row_vectord"))]
-      (Value::U128(min), Value::U128(max)) => Ok(Box::new(RangeInclusiveScalar{max,min, out: Ref::new(RowDVector::from_element(1,0))})),
-      #[cfg(all(feature = "f32", feature = "row_vectord"))]
-      (Value::F32(min),  Value::F32(max))  => Ok(Box::new(RangeInclusiveScalar{max,min, out: Ref::new(RowDVector::from_element(1,F32::new(0.0)))})),
-      #[cfg(all(feature = "f64", feature = "row_vectord"))]
-      (Value::F64(min),  Value::F64(max))  => Ok(Box::new(RangeInclusiveScalar{max,min, out: Ref::new(RowDVector::from_element(1,F64::new(0.0)))})),
-      x => Err(MechError{file: file!().to_string(), tokens: vec![], msg: "".to_string(), id: line!(), kind: MechErrorKind::UnhandledFunctionArgumentKind})
+    let arg1 = arguments[0].clone();
+    let arg2 = arguments[1].clone();
+    match impl_range_inclusive_fxn(arg1.clone(), arg2.clone()) {
+      Ok(fxn) => Ok(fxn),
+      Err(_) => {
+        match (arg1,arg2) {
+          (Value::MutableReference(arg1),Value::MutableReference(arg2)) => {impl_range_inclusive_fxn(arg1.borrow().clone(),arg2.borrow().clone())}
+          (Value::MutableReference(arg1),arg2) => {impl_range_inclusive_fxn(arg1.borrow().clone(),arg2.clone())}
+          (arg1,Value::MutableReference(arg2)) => {impl_range_inclusive_fxn(arg1.clone(),arg2.borrow().clone())}
+          x => Err(MechError{file: file!().to_string(),  tokens: vec![], msg: "".to_string(), id: line!(), kind: MechErrorKind::UnhandledFunctionArgumentKind }),
+        }
+      }
     }
-    }
+  }
 }
