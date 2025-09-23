93ffaceb1dd55ccf806a59cc95ffe5697f1fc806
cmontella <cmontella@live.com>
2025-09-23 18:57:49 -0400

    Refactor range assignment function for matrix operations to improve clarity and reduce redundancy

--- src/interpreter/src/stdlib/assign/matrix.rs
+++ src/interpreter/src/stdlib/assign/matrix.rs
@@ -369,51 +369,43 @@ impl_set_all_fxn_s!(Set1DRB,set_1d_range_b,bool);
 impl_set_all_fxn_v!(Set1DRV,set_1d_range_vec,usize);
 impl_set_all_fxn_v!(Set1DRVB,set_1d_range_vec_b,bool);
 
-macro_rules! matrix_assign_range_fxn {
-  ($op_fxn_name:tt, $fxn_name:ident) => {
-    paste::paste! {
-      fn $op_fxn_name(sink: Value, source: Value, ixes: Vec<Value>) -> MResult<Box<dyn MechFunction>> {
-        let arg = (sink, ixes.as_slice(), source);
-                     impl_assign_fxn!(impl_set_range_arms, $fxn_name, arg, u8,   "u8")
-        .or_else(|_| impl_assign_fxn!(impl_set_range_arms, $fxn_name, arg, u16,  "u16"))
-        .or_else(|_| impl_assign_fxn!(impl_set_range_arms, $fxn_name, arg, u32,  "u32"))
-        .or_else(|_| impl_assign_fxn!(impl_set_range_arms, $fxn_name, arg, u64,  "u64"))
-        .or_else(|_| impl_assign_fxn!(impl_set_range_arms, $fxn_name, arg, u128, "u128"))
-        .or_else(|_| impl_assign_fxn!(impl_set_range_arms, $fxn_name, arg, i8,   "i8"))
-        .or_else(|_| impl_assign_fxn!(impl_set_range_arms, $fxn_name, arg, i16,  "i16"))
-        .or_else(|_| impl_assign_fxn!(impl_set_range_arms, $fxn_name, arg, i32,  "i32"))
-        .or_else(|_| impl_assign_fxn!(impl_set_range_arms, $fxn_name, arg, i64,  "i64"))
-        .or_else(|_| impl_assign_fxn!(impl_set_range_arms, $fxn_name, arg, i128, "i128"))
-        .or_else(|_| impl_assign_fxn!(impl_set_range_arms, $fxn_name, arg, F32,  "f32"))
-        .or_else(|_| impl_assign_fxn!(impl_set_range_arms, $fxn_name, arg, F64,  "f64"))
-        .or_else(|_| impl_assign_fxn!(impl_set_range_arms, $fxn_name, arg, R64,  "rational"))
-        .or_else(|_| impl_assign_fxn!(impl_set_range_arms, $fxn_name, arg, C64,  "complex"))
-        .or_else(|_| impl_assign_fxn!(impl_set_range_arms, $fxn_name, arg, bool, "bool"))
-        .or_else(|_| impl_assign_fxn!(impl_set_range_arms, $fxn_name, arg, String, "string"))
+fn impl_assign_range_fxn(sink: Value, source: Value, ixes: Vec<Value>) -> MResult<Box<dyn MechFunction>> {
+  let arg = (sink, ixes.as_slice(), source);
+                impl_assign_fxn!(impl_set_range_arms, Set1DR, arg, u8,   "u8")
+  .or_else(|_| impl_assign_fxn!(impl_set_range_arms,  Set1DR, arg, u16,  "u16"))
+  .or_else(|_| impl_assign_fxn!(impl_set_range_arms,  Set1DR, arg, u32,  "u32"))
+  .or_else(|_| impl_assign_fxn!(impl_set_range_arms,  Set1DR, arg, u64,  "u64"))
+  .or_else(|_| impl_assign_fxn!(impl_set_range_arms,  Set1DR, arg, u128, "u128"))
+  .or_else(|_| impl_assign_fxn!(impl_set_range_arms,  Set1DR, arg, i8,   "i8"))
+  .or_else(|_| impl_assign_fxn!(impl_set_range_arms,  Set1DR, arg, i16,  "i16"))
+  .or_else(|_| impl_assign_fxn!(impl_set_range_arms,  Set1DR, arg, i32,  "i32"))
+  .or_else(|_| impl_assign_fxn!(impl_set_range_arms,  Set1DR, arg, i64,  "i64"))
+  .or_else(|_| impl_assign_fxn!(impl_set_range_arms,  Set1DR, arg, i128, "i128"))
+  .or_else(|_| impl_assign_fxn!(impl_set_range_arms,  Set1DR, arg, F32,  "f32"))
+  .or_else(|_| impl_assign_fxn!(impl_set_range_arms,  Set1DR, arg, F64,  "f64"))
+  .or_else(|_| impl_assign_fxn!(impl_set_range_arms,  Set1DR, arg, R64,  "rational"))
+  .or_else(|_| impl_assign_fxn!(impl_set_range_arms,  Set1DR, arg, C64,  "complex"))
+  .or_else(|_| impl_assign_fxn!(impl_set_range_arms,  Set1DR, arg, bool, "bool"))
+  .or_else(|_| impl_assign_fxn!(impl_set_range_arms,  Set1DR, arg, String, "string"))
 
-        .or_else(|_| impl_set_range_arms_b!($fxn_name, &arg, u8,  "u8"))
-        .or_else(|_| impl_set_range_arms_b!($fxn_name, &arg, u16,  "u16"))
-        .or_else(|_| impl_set_range_arms_b!($fxn_name, &arg, u32,  "u32"))
-        .or_else(|_| impl_set_range_arms_b!($fxn_name, &arg, u64,  "u64"))
-        .or_else(|_| impl_set_range_arms_b!($fxn_name, &arg, u128, "u128"))
-        .or_else(|_| impl_set_range_arms_b!($fxn_name, &arg, i8,   "i8"))
-        .or_else(|_| impl_set_range_arms_b!($fxn_name, &arg, i16,  "i16"))
-        .or_else(|_| impl_set_range_arms_b!($fxn_name, &arg, i32,  "i32"))
-        .or_else(|_| impl_set_range_arms_b!($fxn_name, &arg, i64,  "i64"))
-        .or_else(|_| impl_set_range_arms_b!($fxn_name, &arg, i128, "i128"))
-        .or_else(|_| impl_set_range_arms_b!($fxn_name, &arg, F32,  "f32"))
-        .or_else(|_| impl_set_range_arms_b!($fxn_name, &arg, F64,  "f64"))
-        .or_else(|_| impl_set_range_arms_b!($fxn_name, &arg, R64,  "rational"))
-        .or_else(|_| impl_set_range_arms_b!($fxn_name, &arg, C64,  "complex"))
-        .or_else(|_| impl_set_range_arms_b!($fxn_name, &arg, bool, "bool"))
-        .or_else(|_| impl_set_range_arms_b!($fxn_name, &arg, String, "string"))
-        .map_err(|_| MechError { file: file!().to_string(), tokens: vec![], msg: format!("Unsupported argument: {:?}", &arg), id: line!(), kind: MechErrorKind::UnhandledFunctionArgumentKind})
-      }
-    }
-  }
+  .or_else(|_| impl_set_range_arms_b!(Set1DR, &arg, u8,  "u8"))
+  .or_else(|_| impl_set_range_arms_b!(Set1DR, &arg, u16,  "u16"))
+  .or_else(|_| impl_set_range_arms_b!(Set1DR, &arg, u32,  "u32"))
+  .or_else(|_| impl_set_range_arms_b!(Set1DR, &arg, u64,  "u64"))
+  .or_else(|_| impl_set_range_arms_b!(Set1DR, &arg, u128, "u128"))
+  .or_else(|_| impl_set_range_arms_b!(Set1DR, &arg, i8,   "i8"))
+  .or_else(|_| impl_set_range_arms_b!(Set1DR, &arg, i16,  "i16"))
+  .or_else(|_| impl_set_range_arms_b!(Set1DR, &arg, i32,  "i32"))
+  .or_else(|_| impl_set_range_arms_b!(Set1DR, &arg, i64,  "i64"))
+  .or_else(|_| impl_set_range_arms_b!(Set1DR, &arg, i128, "i128"))
+  .or_else(|_| impl_set_range_arms_b!(Set1DR, &arg, F32,  "f32"))
+  .or_else(|_| impl_set_range_arms_b!(Set1DR, &arg, F64,  "f64"))
+  .or_else(|_| impl_set_range_arms_b!(Set1DR, &arg, R64,  "rational"))
+  .or_else(|_| impl_set_range_arms_b!(Set1DR, &arg, C64,  "complex"))
+  .or_else(|_| impl_set_range_arms_b!(Set1DR, &arg, bool, "bool"))
+  .or_else(|_| impl_set_range_arms_b!(Set1DR, &arg, String, "string"))
+  .map_err(|_| MechError { file: file!().to_string(), tokens: vec![], msg: format!("Unsupported argument: {:?}", &arg), id: line!(), kind: MechErrorKind::UnhandledFunctionArgumentKind})
 }
-
-matrix_assign_range_fxn!(impl_assign_range_fxn, Set1DR);
 
 pub struct MatrixSetRange {}
 impl NativeFunctionCompiler for MatrixSetRange {
