df87fa33eb2ac7171477ca7c9026f608ec0251d4
cmontella <cmontella@live.com>
2025-10-01 01:51:40 -0400

    Refactor access function implementations to register function descriptors for matrix and vector types

--- src/interpreter/src/stdlib/access/matrix.rs
+++ src/interpreter/src/stdlib/access/matrix.rs
@@ -595,43 +595,79 @@ macro_rules! impl_access_scalar_match_arms {
           $(
             #[cfg(all(feature = $value_string, feature = "row_vector4"))]              
             (Value::$matrix_kind(Matrix::RowVector4(input)), [Value::Index(ix)]) => {
-              //register_assign_s1!([<$fxn_name R4>], $target_type, $value_string, RowVector4);
+              register_fxn_descriptor_inner!([<$fxn_name R4>], $target_type, $value_string);
               Ok(Box::new([<$fxn_name R4>]  {source: input.clone(), ixes: ix.clone(), out: Ref::new($default) }))
-            }
+            },
             #[cfg(all(feature = $value_string, feature = "row_vector3"))]              
-            (Value::$matrix_kind(Matrix::RowVector3(input)), [Value::Index(ix)]) => Ok(Box::new([<$fxn_name R3>]  {source: input.clone(), ixes: ix.clone(), out: Ref::new($default) })),
+            (Value::$matrix_kind(Matrix::RowVector3(input)), [Value::Index(ix)]) => {
+              register_fxn_descriptor_inner!([<$fxn_name R3>], $target_type, $value_string);
+              Ok(Box::new([<$fxn_name R3>]  {source: input.clone(), ixes: ix.clone(), out: Ref::new($default) }))
+            },
             #[cfg(all(feature = $value_string, feature = "row_vector2"))]              
             (Value::$matrix_kind(Matrix::RowVector2(input)), [Value::Index(ix)]) => {
-              //register_assign_s1!([<$fxn_name R2>], $target_type, $value_string, RowVector2);
+              register_fxn_descriptor_inner!([<$fxn_name R2>], $target_type, $value_string);
               Ok(Box::new([<$fxn_name R2>]  {source: input.clone(), ixes: ix.clone(), out: Ref::new($default) }))
             },
             #[cfg(all(feature = $value_string, feature = "vector4"))]              
-            (Value::$matrix_kind(Matrix::Vector4(input)),    [Value::Index(ix)]) => Ok(Box::new([<$fxn_name V4>]  {source: input.clone(), ixes: ix.clone(), out: Ref::new($default) })),
+            (Value::$matrix_kind(Matrix::Vector4(input)),    [Value::Index(ix)]) => {
+              register_fxn_descriptor_inner!([<$fxn_name V4>], $target_type, $value_string);
+              Ok(Box::new([<$fxn_name V4>]  {source: input.clone(), ixes: ix.clone(), out: Ref::new($default) }))
+            },
             #[cfg(all(feature = $value_string, feature = "vector3"))]              
-            (Value::$matrix_kind(Matrix::Vector3(input)),    [Value::Index(ix)]) => Ok(Box::new([<$fxn_name V3>]  {source: input.clone(), ixes: ix.clone(), out: Ref::new($default) })),
+            (Value::$matrix_kind(Matrix::Vector3(input)),    [Value::Index(ix)]) => {
+              register_fxn_descriptor_inner!([<$fxn_name V3>], $target_type, $value_string);
+              Ok(Box::new([<$fxn_name V3>]  {source: input.clone(), ixes: ix.clone(), out: Ref::new($default) }))
+            },
             #[cfg(all(feature = $value_string, feature = "vector2"))]              
-            (Value::$matrix_kind(Matrix::Vector2(input)),    [Value::Index(ix)]) => Ok(Box::new([<$fxn_name V2>]  {source: input.clone(), ixes: ix.clone(), out: Ref::new($default) })),
+            (Value::$matrix_kind(Matrix::Vector2(input)),    [Value::Index(ix)]) => {
+              register_fxn_descriptor_inner!([<$fxn_name V2>], $target_type, $value_string);
+              Ok(Box::new([<$fxn_name V2>]  {source: input.clone(), ixes: ix.clone(), out: Ref::new($default) }))
+            },
             #[cfg(all(feature = $value_string, feature = "matrix4"))]              
-            (Value::$matrix_kind(Matrix::Matrix4(input)),    [Value::Index(ix)]) => Ok(Box::new([<$fxn_name M4>]  {source: input.clone(), ixes: ix.clone(), out: Ref::new($default) })),
+            (Value::$matrix_kind(Matrix::Matrix4(input)),    [Value::Index(ix)]) => {
+              register_fxn_descriptor_inner!([<$fxn_name M4>], $target_type, $value_string);
+              Ok(Box::new([<$fxn_name M4>]  {source: input.clone(), ixes: ix.clone(), out: Ref::new($default) }))
+            },
             #[cfg(all(feature = $value_string, feature = "matrix3"))]              
-            (Value::$matrix_kind(Matrix::Matrix3(input)),    [Value::Index(ix)]) => Ok(Box::new([<$fxn_name M3>]  {source: input.clone(), ixes: ix.clone(), out: Ref::new($default) })),
+            (Value::$matrix_kind(Matrix::Matrix3(input)),    [Value::Index(ix)]) => {
+              register_fxn_descriptor_inner!([<$fxn_name M3>], $target_type, $value_string);
+              Ok(Box::new([<$fxn_name M3>]  {source: input.clone(), ixes: ix.clone(), out: Ref::new($default) }))
+            },
             #[cfg(all(feature = $value_string, feature = "matrix2"))]              
-            (Value::$matrix_kind(Matrix::Matrix2(input)),    [Value::Index(ix)]) => Ok(Box::new([<$fxn_name M2>]  {source: input.clone(), ixes: ix.clone(), out: Ref::new($default) })),
+            (Value::$matrix_kind(Matrix::Matrix2(input)),    [Value::Index(ix)]) => {
+              register_fxn_descriptor_inner!([<$fxn_name M2>], $target_type, $value_string);
+              Ok(Box::new([<$fxn_name M2>]  {source: input.clone(), ixes: ix.clone(), out: Ref::new($default) }))
+            },
             #[cfg(all(feature = $value_string, feature = "matrix1"))]              
-            (Value::$matrix_kind(Matrix::Matrix1(input)),    [Value::Index(ix)]) => Ok(Box::new([<$fxn_name M1>]  {source: input.clone(), ixes: ix.clone(), out: Ref::new($default) })),
+            (Value::$matrix_kind(Matrix::Matrix1(input)),    [Value::Index(ix)]) => {
+              register_fxn_descriptor_inner!([<$fxn_name M1>], $target_type, $value_string);
+              Ok(Box::new([<$fxn_name M1>]  {source: input.clone(), ixes: ix.clone(), out: Ref::new($default) }))
+            },
             #[cfg(all(feature = $value_string, feature = "matrix2x3"))]              
-            (Value::$matrix_kind(Matrix::Matrix2x3(input)),  [Value::Index(ix)]) => Ok(Box::new([<$fxn_name M2x3>]{source: input.clone(), ixes: ix.clone(), out: Ref::new($default) })),
+            (Value::$matrix_kind(Matrix::Matrix2x3(input)),  [Value::Index(ix)]) => {
+              register_fxn_descriptor_inner!([<$fxn_name M2x3>], $target_type, $value_string);
+              Ok(Box::new([<$fxn_name M2x3>]  {source: input.clone(), ixes: ix.clone(), out: Ref::new($default) }))
+            },
             #[cfg(all(feature = $value_string, feature = "matrix3x2"))]              
-            (Value::$matrix_kind(Matrix::Matrix3x2(input)),  [Value::Index(ix)]) => Ok(Box::new([<$fxn_name M3x2>]{source: input.clone(), ixes: ix.clone(), out: Ref::new($default) })),
+            (Value::$matrix_kind(Matrix::Matrix3x2(input)),  [Value::Index(ix)]) => {
+              register_fxn_descriptor_inner!([<$fxn_name M3x2>], $target_type, $value_string);
+              Ok(Box::new([<$fxn_name M3x2>]  {source: input.clone(), ixes: ix.clone(), out: Ref::new($default) }))
+            },
             #[cfg(all(feature = $value_string, feature = "row_vectord"))]              
             (Value::$matrix_kind(Matrix::RowDVector(input)), [Value::Index(ix)]) => {
               register_fxn_descriptor_inner!([<$fxn_name RD>], $target_type, $value_string);
               Ok(Box::new([<$fxn_name RD>]  {source: input.clone(), ixes: ix.clone(), out: Ref::new($default) }))
             },
             #[cfg(all(feature = $value_string, feature = "vectord"))]              
-            (Value::$matrix_kind(Matrix::DVector(input)),    [Value::Index(ix)]) => Ok(Box::new([<$fxn_name VD>]  {source: input.clone(), ixes: ix.clone(), out: Ref::new($default) })),
+            (Value::$matrix_kind(Matrix::DVector(input)),    [Value::Index(ix)]) => {
+              register_fxn_descriptor_inner!([<$fxn_name VD>], $target_type, $value_string);
+              Ok(Box::new([<$fxn_name VD>]  {source: input.clone(), ixes: ix.clone(), out: Ref::new($default) }))
+            },
             #[cfg(all(feature = $value_string, feature = "matrixd"))]              
-            (Value::$matrix_kind(Matrix::DMatrix(input)),    [Value::Index(ix)]) => Ok(Box::new([<$fxn_name MD>]  {source: input.clone(), ixes: ix.clone(), out: Ref::new($default) })),
+            (Value::$matrix_kind(Matrix::DMatrix(input)),    [Value::Index(ix)]) => {
+              register_fxn_descriptor_inner!([<$fxn_name MD>], $target_type, $value_string);
+              Ok(Box::new([<$fxn_name MD>]  {source: input.clone(), ixes: ix.clone(), out: Ref::new($default) }))
+            },
           )+
         )+
         x => Err(MechError{file: file!().to_string(),  tokens: vec![], msg: format!("{:?}",x), id: line!(), kind: MechErrorKind::UnhandledFunctionArgumentKind }),
