12b1829b9ba040b1b2b2e3bf7a2f8306c74b3311
cmontella <cmontella@live.com>
2025-10-06 15:01:32 -0400

    Add unsigned absolute value operations and extend MathAbs implementations

--- machines/math/src/arithmetic/abs.rs
+++ machines/math/src/arithmetic/abs.rs
@@ -8,6 +8,17 @@ use mech_core::matrix::Matrix;
 
 use libm::{fabs,fabsf};
 
+macro_rules! uabs_op {
+  ($arg:expr, $out:expr) => {
+    unsafe{(*$out) = (*$arg).clone();}
+  };}
+
+macro_rules! uabs_vec_op {
+  ($arg:expr, $out:expr) => {
+    unsafe {
+      for i in 0..(*$arg).len() {
+        (&mut (*$out))[i] =  (&(*$arg))[i].clone();
+      }}};}
 
 macro_rules! abs_op {
   ($arg:expr, $out:expr) => {
@@ -45,20 +56,57 @@ macro_rules! fabsf_vec_op {
         ((&mut (*$out))[i]).0 = fabsf(((&(*$arg))[i]).0);
       }}};}
 
+#[cfg(feature = "u8")]
+impl_math_unop!(MathAbs, u8, uabs, FeatureFlag::Custom(hash_str("math/abs")));
+#[cfg(feature = "u16")]
+impl_math_unop!(MathAbs, u16, uabs, FeatureFlag::Custom(hash_str("math/abs")));
+#[cfg(feature = "u32")]
+impl_math_unop!(MathAbs, u32, uabs, FeatureFlag::Custom(hash_str("math/abs")));
+#[cfg(feature = "u64")]
+impl_math_unop!(MathAbs, u64, uabs, FeatureFlag::Custom(hash_str("math/abs")));
+#[cfg(feature = "u128")]
+impl_math_unop!(MathAbs, u128, uabs, FeatureFlag::Custom(hash_str("math/abs")));
+
+#[cfg(feature = "i8")]
+impl_math_unop!(MathAbs, i8, abs, FeatureFlag::Custom(hash_str("math/abs")));
+#[cfg(feature = "i16")]
+impl_math_unop!(MathAbs, i16, abs, FeatureFlag::Custom(hash_str("math/abs")));
+#[cfg(feature = "i32")]
+impl_math_unop!(MathAbs, i32, abs, FeatureFlag::Custom(hash_str("math/abs")));
 #[cfg(feature = "i64")]
 impl_math_unop!(MathAbs, i64, abs, FeatureFlag::Custom(hash_str("math/abs")));
+#[cfg(feature = "i128")]
+impl_math_unop!(MathAbs, i128, abs, FeatureFlag::Custom(hash_str("math/abs")));
+
 #[cfg(feature = "f32")]
 impl_math_unop!(MathAbs, F32, fabsf, FeatureFlag::Custom(hash_str("math/abs")));
 #[cfg(feature = "f64")]
 impl_math_unop!(MathAbs, F64, fabs, FeatureFlag::Custom(hash_str("math/abs")));
 
+#[cfg(feature = "c64")]
+impl_math_unop!(MathAbs, C64, abs, FeatureFlag::Custom(hash_str("math/abs")));
+
+#[cfg(feature = "r64")]
+impl_math_unop!(MathAbs, R64, abs, FeatureFlag::Custom(hash_str("math/abs")));
+
 fn impl_abs_fxn(lhs_value: Value) -> Result<Box<dyn MechFunction>, MechError> {
   impl_urnop_match_arms2!(
     MathAbs,
     (lhs_value),
+    U8 => MatrixU8, u8, u8::zero(), "u8";
+    U16 => MatrixU16, u16, u16::zero(), "u16";
+    U32 => MatrixU32, u32, u32::zero(), "u32";
+    U64 => MatrixU64, u64, u64::zero(), "u64";
+    U128 => MatrixU128, u128, u128::zero(), "u128";
+    I8 => MatrixI8, i8, i8::zero(), "i8";
+    I16 => MatrixI16, i16, i16::zero(), "i16";
+    I32 => MatrixI32, i32, i32::zero(), "i32";
     I64 => MatrixI64, i64, i64::zero(), "i64";
+    I128 => MatrixI128, i128, i128::zero(), "i128";
     F32 => MatrixF32, F32, F32::zero(), "f32";
     F64 => MatrixF64, F64, F64::zero(), "f64";
+    C64 => MatrixC64, C64, C64::default(), "c64";
+    R64 => MatrixR64, R64, R64::zero(), "r64";
   )
 }
 
