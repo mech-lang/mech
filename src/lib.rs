f7e42189103f88d7c3dfcca73d8a387cf8ac5609
cmontella <cmontella@live.com>
2025-10-17 16:57:45 -0400

    Implement set/union

--- src/lib.rs
+++ src/lib.rs
@@ -11,10 +11,26 @@ use paste::paste;
 use std::fmt::{Display, Debug};
 use std::marker::PhantomData;
 
+#[cfg(feature = "union")]
+pub mod union;
+//#[cfg(feature = "complement")]
+//pub mod complement;
+//#[cfg(feature = "difference")]
+//pub mod difference;
+//#[cfg(feature = "intersection")]
+//pub mod intersection;
+//
+#[cfg(feature = "union")]
+pub use self::union::*;
+//#[cfg(feature = "complement")]
+//pub use self::complement::*;
+//#[cfg(feature = "difference")]
+//pub use self::difference::*;
+//#[cfg(feature = "intersection")]
+//pub use self::intersection::*;
+
 //#[cfg(feature = "join")]
 //pub mod join;
-#[cfg(feature = "ops")]
-pub mod ops;
 //#[cfg(feature = "powerset")]
 //pub mod powerset;
 //
