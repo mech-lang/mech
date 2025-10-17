08c3ecfa39febdacb262f56db2cc4d621b896d54
cmontella <cmontella@live.com>
2025-10-17 16:15:50 -0400

    Implement set/union

--- machines/set/src/ops/mod.rs
+++ machines/set/src/ops/mod.rs
@@ -3,18 +3,18 @@ pub use crate::*;
 
 #[cfg(feature = "union")]
 pub mod union;
-#[cfg(feature = "complement")]
-pub mod complement;
-#[cfg(feature = "difference")]
-pub mod difference;
-#[cfg(feature = "intersection")]
-pub mod intersection;
-
+//#[cfg(feature = "complement")]
+//pub mod complement;
+//#[cfg(feature = "difference")]
+//pub mod difference;
+//#[cfg(feature = "intersection")]
+//pub mod intersection;
+//
 #[cfg(feature = "union")]
 pub use self::union::*;
-#[cfg(feature = "complement")]
-pub use self::complement::*;
-#[cfg(feature = "difference")]
-pub use self::difference::*;
-#[cfg(feature = "intersection")]
-pub use self::intersection::*;
+//#[cfg(feature = "complement")]
+//pub use self::complement::*;
+//#[cfg(feature = "difference")]
+//pub use self::difference::*;
+//#[cfg(feature = "intersection")]
+//pub use self::intersection::*;
