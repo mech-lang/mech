03c68dccc0a24583bd0c15f80c5ffc9100a477cb
Liam Messinger <liam.messinger@gmail.com>
2025-10-28 12:37:09 -0400

    Add element_of, not_element_of, proper_subset, and proper_superset

--- src/relations/mod.rs
+++ src/relations/mod.rs
@@ -1,6 +1,20 @@
 // relations module (subset/superset etc.)
+#[macro_use]
 
+#[cfg(feature = "subset")]
 pub mod subset;
+#[cfg(feature = "proper_subset")]
 pub mod proper_subset;
+#[cfg(feature = "superset")]
 pub mod superset;
+#[cfg(feature = "proper_superset")]
 pub mod proper_superset;
+
+#[cfg(feature = "subset")]
+pub use self::subset::*;
+#[cfg(feature = "proper_subset")]
+pub use self::proper_subset::*;
+#[cfg(feature = "superset")]
+pub use self::superset::*;
+#[cfg(feature = "proper_superset")]
+pub use self::proper_superset::*;
