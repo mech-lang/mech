c7c5b128d979bd7375a4439e0e1f4ffbab41bee0
cmontella <cmontella@live.com>
2025-11-15 23:56:39 -0500

    Fix indentation in error handling for incorrect number of arguments

--- machines/compare/src/lib.rs
+++ machines/compare/src/lib.rs
@@ -95,7 +95,7 @@ macro_rules! impl_compare_binop {
             let out: Ref<$out_type> = unsafe { out.as_unchecked() }.clone();
             Ok(Box::new(Self {lhs, rhs, out }))
           },
-        _ => Err(MechError2::new(
+          _ => Err(MechError2::new(
               IncorrectNumberOfArguments { expected: 2, found: args.len() }, 
               None
             ).with_compiler_loc()
