36bb86cfeb32eb7930093087e005aea8c0c9e317
cmontella <cmontella@live.com>
2025-12-04 20:51:58 -0500

    Remove unused hashbrown crate dependency from syntax module

--- src/syntax/src/lib.rs
+++ src/syntax/src/lib.rs
@@ -11,7 +11,6 @@
 extern crate mech_core;
 #[cfg(feature="no-std")] #[macro_use] extern crate alloc;
 #[cfg(not(feature = "no-std"))] extern crate core;
-extern crate hashbrown;
 extern crate nom;
 extern crate nom_unicode;
 extern crate tabled;
