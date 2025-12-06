718b44e1db79b4c389032a19f632c4c532b121d2
cmontella <cmontella@live.com>
2025-12-05 22:33:48 -0500

    Rename 'exponential' module to 'exp' for consistency and clarity

--- machines/math/src/exponential/mod.rs
+++ machines/math/src/exponential/mod.rs
@@ -1,7 +1,7 @@
 #[macro_use]
 pub use crate::*;
-#[cfg(feature = "exponential")]
-pub mod exponential;
+#[cfg(feature = "exp")]
+pub mod exp;
 #[cfg(feature = "exp2")]
 pub mod exp2;
 #[cfg(feature = "exp10")]
@@ -9,8 +9,8 @@ pub mod exp10;
 #[cfg(feature = "expm1")]
 pub mod expm1;
 
-#[cfg(feature = "exponential")]
-pub use self::exponential::*;
+#[cfg(feature = "exp")]
+pub use self::exp::*;
 #[cfg(feature = "exp2")]
 pub use self::exp2::*;
 #[cfg(feature = "exp10")]
