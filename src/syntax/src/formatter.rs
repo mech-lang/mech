bd63787ca76a8d3656d443f3518dc23ae2807228
cmontella <cmontella@live.com>
2025-12-15 22:22:46 -0500

    Add block ID to fenced mech code for improved referencing

--- src/syntax/src/formatter.rs
+++ src/syntax/src/formatter.rs
@@ -423,6 +423,7 @@ impl Formatter {
 
   pub fn fenced_mech_code(&mut self, block: &FencedMechCode) -> String {
     self.interpreter_id = block.config.namespace;
+    let block_id = hash_str(&format!("{:?}",block));
     let namespace_str = &block.config.namespace_str;
     let mut src = String::new();
     for (code,cmmnt) in &block.code {
@@ -478,13 +479,13 @@ impl Formatter {
         let namespace_str = if namespace_str.is_empty() {
           "".to_string()
         } else {
-          format!("<div class=\"mech-code-block-namespace\">{}</div>", namespace_str)
+          format!("<div class=\"mech-code-block-namespace\"><a href=\"#{}\">{}</a></div>", block_id, namespace_str)
         };
-        format!("<div class=\"mech-fenced-mech-block\"{}>
+        format!("<div id=\"{}\" class=\"mech-fenced-mech-block\"{}>
           {}
           <div class=\"mech-code-block\">{}</div>
           <div class=\"mech-block-output\" id=\"{}:{}\"></div>
-        </div>", style_attr, namespace_str, src, output_id, intrp_id)
+        </div>", block_id, style_attr, namespace_str, src, output_id, intrp_id)
       }
     } else {
       format!("```mech{}\n{}\n```", src, format!(":{}", disabled_tag))
