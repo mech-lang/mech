c93084393851e43b3a3835fba42bc7a671cb7831
cmontella <cmontella@live.com>
2025-12-18 23:26:33 -0500

    Refactor PrettyPrint implementation for MechMap to improve formatting and readability

--- src/core/src/structures/map.rs
+++ src/core/src/structures/map.rs
@@ -54,18 +54,17 @@ impl MechMap {
 #[cfg(feature = "pretty_print")]
 impl PrettyPrint for MechMap {
   fn pretty_print(&self) -> String {
-    let mut builder = Builder::default();
-    let mut element_strings = vec![];
-    let mut key_strings = vec![];
-    for (k,v) in self.map.iter() {
-      element_strings.push(v.pretty_print());
-      key_strings.push(k.pretty_print());
-    }    
-    builder.push_record(key_strings);
-    builder.push_record(element_strings);
-    let mut table = builder.build();
-    table.with(Style::modern_rounded());
-    format!("{table}")
+    let mut lines = Vec::new();
+
+    for (k, v) in &self.map {
+      lines.push(format!(
+        "  {}: {}",
+        k.pretty_print(),
+        v.pretty_print()
+      ));
+    }
+
+    format!("{{\n{}\n}}", lines.join("\n"))
   }
 }
 
