7330e8a6d688bfb11721cf96fb8298da27f43867
cmontella <cmontella@live.com>
2025-12-26 18:18:18 -0500

    Refactor lib.rs to remove unused modules and streamline extern crate declarations

--- machines/string/src/lib.rs
+++ machines/string/src/lib.rs
@@ -1,17 +1,8 @@
-extern crate mech_core;
-extern crate mech_utilities;
+#![no_main]
+#![allow(warnings)]
 #[macro_use]
-extern crate lazy_static;
-use mech_core::*;
+extern crate mech_core;
+extern crate paste;
 
-lazy_static! {
-  static ref SEPARATOR: u64 = hash_str("separator");
-  static ref STRING: u64 = hash_str("string");
-  static ref TABLE: u64 = hash_str("table");
-  static ref ROW: u64 = hash_str("row");
-  static ref COLUMN: u64 = hash_str("column");
-}
-
-pub mod join;
-pub mod length;
-pub mod split;
+#[cfg(feature = "concat")]
+pub mod concat;
