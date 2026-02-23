f8e64dadc2046fae64bc4482221c0299874642d2
Corey Montella <cmontella@live.com>
2026-02-23 00:02:53 -0500

    Add Mika feature support to Cargo.toml and section element handling in mechdown.rs

--- src/interpreter/src/mechdown.rs
+++ src/interpreter/src/mechdown.rs
@@ -132,6 +132,7 @@ pub fn section_element(element: &SectionElement, p: &Interpreter) -> MResult<Val
     SectionElement::WarningBlock(x) => x.hash(&mut hasher),
     SectionElement::InfoBlock(x) => x.hash(&mut hasher),
     SectionElement::IdeaBlock(x) => x.hash(&mut hasher),
+    SectionElement::Mika(x) => x.hash(&mut hasher),
     x => {return Err(MechError2::new(
         FeatureNotEnabledError,
         Some(format!("Feature not enabled for section element: {:?}", x)),
