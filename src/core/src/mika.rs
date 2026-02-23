e17cc1a2ea0de592ea5189f265a3b80c90714be5
Corey Montella <cmontella@live.com>
2026-02-22 20:47:31 -0500

    Enhance Mika's structure with optional sections and refine arm and nose enums for better expression handling

--- src/core/src/mika.rs
+++ src/core/src/mika.rs
@@ -3,23 +3,144 @@ use crate::*;
 // Mika
 // ============================================================================
 
-// Inline Mika
-// -----------------------------------------------------------------------------
+// Inline Mika lives in the terminal. She greets users when they start Mech, and provides a friendly face to interact with. She can be depicted in a variety of expressions, sizes, colors, and poses. 
+
+#[derive(Clone, Debug, PartialEq)]
+pub struct MikaSection {
+  pub elements: Vec<SectionElement>,
+}
 
 #[derive(Clone, Copy, Debug, PartialEq, Eq, Hash, Serialize, Deserialize)]
 pub enum Mika {
-  Mini(MiniMika),
-  Micro(MicroMika)
+  Mini(MiniMika, Option<MikaSection>),
+  Micro(MicroMika, Option<MikaSection>),
 }
 
 #[derive(Clone, Copy, Debug, PartialEq, Eq, Hash, Serialize, Deserialize)]
 pub struct MiniMika {
   pub expression: MikaExpression,
-  pub left_arm: MikaArm,
-  pub right_arm: MikaArm,
+  pub left_arm: Option<MikaArm>,
+  pub right_arm: Option<MikaArm>,
 }
 
-// Inline Mika lives in the terminal. She greets users when they start Mech, and provides a friendly face to interact with. She can display a variety of expressions and poses, and can be used to add personality and fun to the Mech experience. Users can customize Mika's appearance and expressions, and she can be used to provide feedback, celebrate achievements, or just add a bit of whimsy to the coding process.
+// Parts
+// ---------------------------------------------------------------------------
+
+#[derive(Clone, Copy, Debug, PartialEq, Eq, Hash, Serialize, Deserialize)]
+pub enum MikaNose {
+  Normal,      // ⦿
+  Open,        // ◯
+  Back,        // ⊕
+  Stage1,      // ⊙
+  Stage2,      // ⊚
+  Stage3,      // ⦾
+  Blink,       // ⊖
+  Wide,        // ⦵
+  Error,       // ⊗
+  Filled,      // ⏺
+  FlatMouth,   // ⍜
+  Hexagon,     // ⬢
+  Pentagon,    // ⬟
+  Hexagon2,    // ⬣
+  HexagonOpen, // ⎔
+}
+
+impl MikaNose {
+  pub fn symbol(&self) -> &'static str {
+    match self {
+      MikaNose::Normal => "⦿",
+      MikaNose::Open => "◯",
+      MikaNose::Back => "⊕",
+      MikaNose::Stage1 => "∘",
+      MikaNose::Stage2 => "⦾",
+      MikaNose::Stage3 => "⦾",
+      MikaNose::Blink => "⊖",
+      MikaNose::Wide => "⦵",
+      MikaNose::Error => "⊗",
+      MikaNose::Filled => "⏺",
+      MikaNose::FlatMouth => "⍜",
+      MikaNose::Hexagon => "⬢",
+      MikaNose::Pentagon => "⬟",
+      MikaNose::Hexagon2 => "⬣",
+      MikaNose::HexagonOpen => "⎔",
+    }
+  }
+}
+
+
+#[derive(Clone, Copy, Debug, PartialEq, Eq, Hash, Serialize, Deserialize)]
+pub enum MikaArm {
+  BatWing,           // ᗑ
+  BigGripperLeft,    // Ɔ∞
+  BigGripperRight,   // ∞C
+  CurlLeft,          // ᕦ
+  CurlRight,         // ᕤ
+  Dance,             // ~
+  GestureLeft,       // ›⌣
+  GestureRight,      // ⌣‹
+  GripperLeft,       // ›─
+  GripperRight,      // ─‹
+  Left,              // ╭
+  RaisedLeft,        // ⸌
+  RaisedRight,       // ⸍
+  Right,             // ╮
+  Shield,            // ᗢ
+  ShootLeft,         // ·¬
+  ShootRight,        // ⌐·
+  ShrugLeft,         // -◡
+  ShrugRight,        // ◡-
+  Sword,             // ⸸
+  Point,             // ─
+  PunchLeft,         // ᓂ
+  PunchRight,        // ᓀ
+  PunchLowLeft,      // ᓇ
+  PunchLowRight,     // ᓄ
+  UpLeft,            // ╰
+  UpRight,           // ╯
+}
+
+impl MikaArm {
+  pub fn symbol(&self) -> &'static str {
+    match self {
+      MikaArm::BatWing => "ᗑ",
+      MikaArm::BigGripperLeft => "Ɔ∞",
+      MikaArm::BigGripperRight => "∞C",
+      MikaArm::CurlLeft => "ᕦ",
+      MikaArm::CurlRight => "ᕤ",
+      MikaArm::Dance => "~",
+      MikaArm::GestureLeft => "⌣",
+      MikaArm::GestureRight => "⌣",
+      MikaArm::GripperLeft => "›─",
+      MikaArm::GripperRight => "─‹",
+      MikaArm::Left => "╭",
+      MikaArm::RaisedLeft => "⸌",
+      MikaArm::RaisedRight => "⸍",
+      MikaArm::Right => "╮",
+      MikaArm::Shield => "ᗢ",
+      MikaArm::ShootLeft => "·¬",
+      MikaArm::ShootRight => "⌐·",
+      MikaArm::ShrugLeft => "-◡",
+      MikaArm::ShrugRight => "◡-",
+      MikaArm::Sword => "⸸",
+      MikaArm::Point => "─",
+      MikaArm::PunchLeft => "ᓂ",
+      MikaArm::PunchRight => "ᓀ",
+      MikaArm::PunchLowLeft => "ᓇ",
+      MikaArm::PunchLowRight => "ᓄ",
+      MikaArm::UpLeft => "╰",
+      MikaArm::UpRight => "╯",
+    }
+  }
+
+  pub fn is_left(&self) -> bool {
+    matches!(self, MikaArm::UpLeft | MikaArm::BatWing | MikaArm::BigGripperLeft | MikaArm::CurlLeft | MikaArm::GestureLeft | MikaArm::GripperLeft | MikaArm::Left | MikaArm::ShootLeft | MikaArm::ShrugLeft | MikaArm::Dance)
+  }
+
+  pub fn is_right(&self) -> bool {
+    matches!(self, MikaArm::UpRight | MikaArm::BatWing | MikaArm::BigGripperRight | MikaArm::CurlRight | MikaArm::GestureRight | MikaArm::GripperRight | MikaArm::Right | MikaArm::ShootRight | MikaArm::ShrugRight | MikaArm::Dance)
+  }
+
+}
 
 #[derive(Clone, Copy, Debug, PartialEq, Eq, Hash, Serialize, Deserialize)]
 pub enum MikaEyeLeft {
@@ -140,6 +261,9 @@ impl MikaExpression {
   }
 }
 
+// MicroMika
+// ---------------------------------------------------------------------------
+
 #[derive(Clone, Copy, Debug, PartialEq, Eq, Hash, Serialize, Deserialize)]
 pub enum MicroMika {
   Bat,            // ᗑ⦿ᗑ
@@ -197,122 +321,9 @@ impl MicroMika {
   }
 }
 
-#[derive(Clone, Copy, Debug, PartialEq, Eq, Hash, Serialize, Deserialize)]
-pub enum MikaArm {
-  BatWing,           // ᗑ
-  BigGripperLeft,    // Ɔ∞
-  BigGripperRight,   // ∞C
-  CurlLeft,          // ᕦ
-  CurlRight,         // ᕤ
-  Dance,             // ~
-  GestureLeft,       // ›⌣
-  GestureRight,      // ⌣‹
-  GripperLeft,       // ›─
-  GripperRight,      // ─‹
-  Left,              // ╭
-  RaisedLeft,        // ⸌
-  RaisedRight,       // ⸍
-  Right,             // ╮
-  Shield,            // ᗢ
-  ShootLeft,         // ·¬
-  ShootRight,        // ⌐·
-  ShrugLeft,         // -◡
-  ShrugRight,        // ◡-
-  Sword,             // ⸸
-  Point,             // ─
-  PunchLeft,         // ᓂ
-  PunchRight,        // ᓀ
-  PunchLowLeft,      // ᓇ
-  PunchLowRight,     // ᓄ
-  UpLeft,            // ╰
-  UpRight,           // ╯
-}
-
-impl MikaArm {
-  pub fn symbol(&self) -> &'static str {
-    match self {
-      MikaArm::BatWing => "ᗑ",
-      MikaArm::BigGripperLeft => "Ɔ∞",
-      MikaArm::BigGripperRight => "∞C",
-      MikaArm::CurlLeft => "ᕦ",
-      MikaArm::CurlRight => "ᕤ",
-      MikaArm::Dance => "~",
-      MikaArm::GestureLeft => "⌣",
-      MikaArm::GestureRight => "⌣",
-      MikaArm::GripperLeft => "›─",
-      MikaArm::GripperRight => "─‹",
-      MikaArm::Left => "╭",
-      MikaArm::RaisedLeft => "⸌",
-      MikaArm::RaisedRight => "⸍",
-      MikaArm::Right => "╮",
-      MikaArm::Shield => "ᗢ",
-      MikaArm::ShootLeft => "·¬",
-      MikaArm::ShootRight => "⌐·",
-      MikaArm::ShrugLeft => "-◡",
-      MikaArm::ShrugRight => "◡-",
-      MikaArm::Sword => "⸸",
-      MikaArm::Point => "─",
-      MikaArm::PunchLeft => "ᓂ",
-      MikaArm::PunchRight => "ᓀ",
-      MikaArm::PunchLowLeft => "ᓇ",
-      MikaArm::PunchLowRight => "ᓄ",
-      MikaArm::UpLeft => "╰",
-      MikaArm::UpRight => "╯",
-    }
-  }
-
-  pub fn is_left(&self) -> bool {
-    matches!(self, MikaArm::UpLeft | MikaArm::BatWing | MikaArm::BigGripperLeft | MikaArm::CurlLeft | MikaArm::GestureLeft | MikaArm::GripperLeft | MikaArm::Left | MikaArm::ShootLeft | MikaArm::ShrugLeft | MikaArm::Dance)
-  }
-
-  pub fn is_right(&self) -> bool {
-    matches!(self, MikaArm::UpRight | MikaArm::BatWing | MikaArm::BigGripperRight | MikaArm::CurlRight | MikaArm::GestureRight | MikaArm::GripperRight | MikaArm::Right | MikaArm::ShootRight | MikaArm::ShrugRight | MikaArm::Dance)
-  }
-
-}
-
-#[derive(Clone, Copy, Debug, PartialEq, Eq, Hash, Serialize, Deserialize)]
-pub enum MikaNose {
-  Normal,      // ⦿
-  Open,        // ◯
-  Back,        // ⊕
-  Stage1,      // ⊙
-  Stage2,      // ⊚
-  Stage3,      // ⦾
-  Blink,       // ⊖
-  Wide,        // ⦵
-  Error,       // ⊗
-  Filled,      // ⏺
-  FlatMouth,   // ⍜
-  Hexagon,     // ⬢
-  Pentagon,    // ⬟
-  Hexagon2,    // ⬣
-  HexagonOpen, // ⎔
-}
-
-impl MikaNose {
-  pub fn symbol(&self) -> &'static str {
-    match self {
-      MikaNose::Normal => "⦿",
-      MikaNose::Open => "◯",
-      MikaNose::Back => "⊕",
-      MikaNose::Stage1 => "∘",
-      MikaNose::Stage2 => "⦾",
-      MikaNose::Stage3 => "⦾",
-      MikaNose::Blink => "⊖",
-      MikaNose::Wide => "⦵",
-      MikaNose::Error => "⊗",
-      MikaNose::Filled => "⏺",
-      MikaNose::FlatMouth => "⍜",
-      MikaNose::Hexagon => "⬢",
-      MikaNose::Pentagon => "⬟",
-      MikaNose::Hexagon2 => "⬣",
-      MikaNose::HexagonOpen => "⎔",
-    }
-  }
-}
 
 // Animations
+// ---------------------------------------------------------------------------
 
 // Sleep
 static MIRCOMIKA_POWEROFF: &[&str] = &["╭⦿╮","╭⦾╮","╭⊚╮","╭⊙╮","╭◯╮"];
@@ -325,6 +336,9 @@ static MICROMIKA_LOWER_ARMS: &[&str] = &["╰⦿╯","─⦿─","╭⦿╮"];
 static MICROMIKA_FLAPPING: &[&str] = &["─⦿─","╰⦿╯"];
 static MICROMIKA_GRIPPING_RIGHT: &[&str] = &["╭⦿─‹ -> ╭⦿─-"];
 static MICROMIKA_GRIPPING_LEFT: &[&str] = &["›─⦿╮ -> -─⦿╮"];
+
+// Mylo
+// ---------------------------------------------------------------------------
 
 // Mylo is a secondary character, he's under development right now on the basis of these faces. Maybe he's a villain? Maybe he's Mika's siblng? I don't know.
 
