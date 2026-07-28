103a631f4ee9cad446c4736afdc4eccb63e5bcd0
cmontella <cmontella@live.com>
2026-07-28 01:10:22 -0400

    refactor(interpreter): split expression registration and access tests

---
+++ src/interpreter/src/expressions/tests/mod.rs
@@ -0,0 +1,23 @@
+#[cfg(all(feature = "functions", feature = "f64"))]
+mod registration;
+
+#[cfg(all(
+  feature = "functions",
+  feature = "record",
+  feature = "tuple",
+  feature = "f64",
+  feature = "program",
+  feature = "compiler"
+))]
+mod structural_access;
+
+#[cfg(all(
+  feature = "functions",
+  feature = "f64",
+  feature = "u64",
+  feature = "convert",
+  feature = "kind_annotation",
+  feature = "variable_define",
+  feature = "variables"
+))]
+mod variables;
